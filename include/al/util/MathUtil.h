2912d5fc93867c0a5eccc5af3f9d6556aedb8ecb
GRAnimated <devin@nhscan.com>
2020-07-24 03:12:30 -0400

    Add several more math utilities, read description
    powerIn and powerOut are currently using powf instead of Odyssey's powf_0.

--- include/al/util/MathUtil.h
+++ include/al/util/MathUtil.h
@@ -2,5 +2,19 @@
 
 namespace al
 {
+    float easeIn(float);
+    float easeOut(float);
+    float easeInOut(float);
+
+    float sign(float);
+    bool sign(int);
+
+    float squareIn(float);
+    float squareOut(float);
+
+    float powerIn(float, float);
+    float powerOut(float, float);
+
+    float normalize(float, float, float);
     float normalize(signed int, signed int, signed int);
 };
--- source/al/util/MathUtil.cpp
+++ source/al/util/MathUtil.cpp
@@ -1,7 +1,40 @@
 #include "al/util/MathUtil.h"
+#include <cmath>
 
 namespace al
 {
+    float normalize(float var, float min, float max)
+    {
+    float v4;
+    bool v5;
+    float result;
+    float v7;
+
+    v4 = -(max - min);
+    if ( (max - min) > 0.0f )
+        v4 = max - min;
+    if ( v4 >= 0.001f )
+    {
+        v7 = min;
+        if ( var >= min )
+        {
+        v7 = var;
+        if ( var > max )
+            v7 = max;
+        }
+        result = (v7 - min) / (max - min);
+    }
+    else
+    {
+        v5 = var < min;
+        result = 1.0f;
+        if ( v5 )
+        result = 0.0f;
+    }
+    return result;
+    }
+
+
     float normalize(signed int var, signed int min, signed int max)
     {
         signed int v3;
@@ -41,5 +74,29 @@ namespace al
         }
 
         return result;
+    }
+
+    float easeIn(float var) {
+        return (((var * -0.5f) + 1.5f) * var) * var;
+    }
+    float easeOut(float var) {
+        return (((var * -0.5f) * var) + 1.5f) * var;
+    }
+    float easeInOut(float var) {
+        return (((var * -2.0f) + 3.0f) * var) * var;
+    }
+
+    float squareIn(float var) {
+        return var * var;
+    }
+    float squareOut(float var) {
+        return (2.0f - var) * var;
+    }
+
+    float powerIn(float x, float y) {
+        return powf(x, y);
+    }
+    float powerOut(float x, float y) {
+        return powf(x, 1.0 / y);
     }
 };
