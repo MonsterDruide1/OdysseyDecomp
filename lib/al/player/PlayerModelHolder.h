a21eab10f66ba8ecc9248dc3955ebbd6285bc13f
MonsterDruide1 <5958456@gmail.com>
2021-09-02 16:54:01 +0200

    player: Add PlayerModelHolder

---
+++ lib/al/player/PlayerModelHolder.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <container/seadPtrArray.h>
+#include <prim/seadSafeString.h>
+
+namespace al {
+    class LiveActor;
+}
+
+class PlayerModelHolder {
+
+    struct Entry {
+        sead::FixedSafeString<128> mName;
+        al::LiveActor* mLiveActor;
+    };
+
+    PlayerModelHolder(unsigned int);
+    void registerModel(al::LiveActor*, char const*);
+    void changeModel(char const*);
+    al::LiveActor* findModelActor(char const*) const;
+    al::LiveActor* tryFindModelActor(char const*) const;
+    bool isCurrentModelLabel(char const*) const;
+    bool isCurrentModelLabelSubString(char const*) const;
+
+private:
+    sead::PtrArray<Entry> mBuffer;
+    Entry *currentModel = nullptr;
+    sead::FixedSafeString<128> _10 = sead::FixedSafeString<128>("");
+};
+
---
+++ src/al/player/PlayerModelHolder.cpp
@@ -0,0 +1,46 @@
+#include "al/player/PlayerModelHolder.h"
+
+#include "al/util/StringUtil.h"
+
+PlayerModelHolder::PlayerModelHolder(unsigned int bufferSize){
+    mBuffer.allocBuffer(bufferSize, nullptr);
+}
+
+void PlayerModelHolder::registerModel(al::LiveActor* liveActor, const char* name){
+    mBuffer.pushBack(new Entry{sead::FixedSafeString<128>(name), liveActor});
+}
+
+void PlayerModelHolder::changeModel(const char* name){
+    for (auto it = mBuffer.begin(), end = mBuffer.end(); it != end; ++it){
+        if(al::isEqualString(it->mName, sead::SafeString(name))){
+            currentModel = &*it;
+            return;
+        }
+    }
+}
+
+al::LiveActor* PlayerModelHolder::findModelActor(const char* name) const {
+    for (auto it = mBuffer.begin(), end = mBuffer.end(); it != end; ++it){
+        if(al::isEqualString(it->mName, sead::SafeString(name))){
+            return it->mLiveActor;
+        }
+    }
+    return currentModel->mLiveActor;
+}
+
+al::LiveActor* PlayerModelHolder::tryFindModelActor(const char* name) const {
+    for (auto it = mBuffer.begin(), end = mBuffer.end(); it != end; ++it){
+        if(al::isEqualString(it->mName, sead::SafeString(name))){
+            return it->mLiveActor;
+        }
+    }
+    return nullptr;
+}
+
+bool PlayerModelHolder::isCurrentModelLabel(const char* name) const {
+    return al::isEqualString(currentModel->mName.cstr(), name);
+}
+
+bool PlayerModelHolder::isCurrentModelLabelSubString(const char* name) const {
+    return al::isEqualSubString(currentModel->mName.cstr(), name);
+}
