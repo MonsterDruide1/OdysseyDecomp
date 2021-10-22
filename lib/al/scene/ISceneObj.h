683c3111f8ff44b8ba3696f70f61bde5ef109752
MonsterDruide1 <5958456@gmail.com>
2021-10-22 13:39:44 +0200

    scene: Add ISceneObj

---
+++ lib/al/scene/ISceneObj.h
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace al {
+class ActorInitInfo;
+
+class ISceneObj {
+public:
+    virtual ~ISceneObj();
+    virtual const char* getSceneObjName() const;
+    virtual void initAfterPlacementSceneObj(const ActorInitInfo&);
+    virtual void initSceneObj();
+};
+
+}
