e65ffec4356c3538c60dd86400affa3cc00a634c
JoshuaBradbury <joshua.bradbury@protonmail.com>
2021-12-04 14:24:50 +0000

    Fix issues with compilation and some mismatches

--- lib/al/util/StringUtil.h
+++ lib/al/util/StringUtil.h
@@ -21,6 +21,6 @@ bool isEqualSubString(const char*, const char*);
 bool isEqualSubString(const sead::SafeStringBase<char>&, const sead::SafeStringBase<char>&);
 bool isStartWithString(const char*, const char*);
 bool isEndWithString(const char*, const char*);
-bool isEqualString(const char*, const char*);
+bool isEqualString(const char*, const char*) __attribute__((noinline));
 bool isEqualString(const sead::SafeString&, const sead::SafeString&);
 };  // namespace al
