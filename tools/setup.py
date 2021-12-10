39888552e49838c5069703301eff1edce363e573
MonsterDruide1 <5958456@gmail.com>
2021-12-10 13:25:56 +0100

    dump changes

--- tools/setup.py
+++ tools/setup.py
@@ -23,39 +23,45 @@ CACHE_REPO_RELEASE_URL = "https://github.com/MonsterDruide1/OdysseyDecompToolsCa
 LIBCXX_SRC_URL = "https://releases.llvm.org/3.9.1/libcxx-3.9.1.src.tar.xz"
 
 class Version(Enum):
-    VER_100 = "1.0"
-    VER_101 = "1.0.1"
-    VER_110 = "1.1"
-    VER_120 = "1.2"
-    VER_130 = "1.3"
+    #VER_NAME = VER_ID, COMPRESSED_HASH, UNCOMPRESSED_HASH
+    VER_100 = ["1.0",   "e21692d90f8fd2def2d2d22d983d62ac81df3b8b3c762d1f2dca9d9ab7b3053a", "18ece865061704d551fe456e0600c604c26345ecb38dcbe328a24d5734b3b4eb"]
+    VER_101 = ["1.0.1", "c8531a7a14b19bc7df4738b230a7cc85d8b270264cb22bfd0dfcf3bef6fde2b6", "e4f73a2721288a93bf6038094002ea00075068276bdd814d628650227fd7d5c1"]
+    VER_110 = ["1.1",   "d3afba4ee684672fac264444bfe26e908d3b5d41059d362343116792fe69a7f6", "d2f664ef1a6734803d239c56549e00b74aa534c950cdcacb605939372b7e01f3"]
+    VER_120 = ["1.2",   "6d22647134b241641bf4df5e596bde48fc0ad81edf1856ffbbdf3f49b71d1219", "00c7275f67183d0a7c46c6d34349ac8988f941d8ed4162c9ce156469b3d6e4de"]
+    VER_130 = ["1.3",   "47ac74a66234bc1eb717b18e01fe7c880d2dd50cf1115fd046f5e5878be5f85f", "a991fcfe32506ad42f1e425f5d3b4176bf4f95bf2a1cccec70eb933ccfe1f416"]
 
-def prepare_executable(original_nso: Optional[Path]):
-    COMPRESSED_V10_HASH = "e21692d90f8fd2def2d2d22d983d62ac81df3b8b3c762d1f2dca9d9ab7b3053a"
-    UNCOMPRESSED_V10_HASH = "18ece865061704d551fe456e0600c604c26345ecb38dcbe328a24d5734b3b4eb"
+def check_executable(version: Version):
+    path = setup.ROOT / "data" / version.value[0]
+    nso = path / "main.nso"
+    elf = path / "main.elf"
+    return nso.is_file() and hashlib.sha256(nso.read_bytes()).hexdigest() == version.value[2] and elf.is_file()
 
-    # The uncompressed v1.0 main NSO.
-    TARGET_HASH = UNCOMPRESSED_V10_HASH
+def download_patch(v0: Version, v1: Version, temp_dir:Path) -> Path:
+    print(f">>>> downloading patch from version {v0.name} to {v1.name}...")
+    patch_name = v0.value[0]+"-"+v1.value[0]+".patch"
+    path = temp_dir / patch_name
+    urllib.request.urlretrieve("https://raw.githubusercontent.com/wiki/MonsterDruide1/OdysseyDecomp/patches/"+patch_name, path)
+    return path
 
-    if TARGET_PATH.is_file() and hashlib.sha256(TARGET_PATH.read_bytes()).hexdigest() == TARGET_HASH and TARGET_ELF_PATH.is_file():
-        print(">>> NSO is already set up")
-        return
 
-    if not original_nso.is_file():
-        setup.fail(f"{original_nso} is not a file")
+def prepare_single_executable(comp_v100_nso: Path, version: Version, temp_dir: tempfile.TemporaryDirectory):
+    TARGET_HASH = version.value[2]
+    data_path = setup.ROOT / "data" / version.value[0]
+    data_path.mkdir(parents=True, exist_ok=True)
+    TARGET_PATH = data_path / "main.nso"
+    TARGET_ELF_PATH = data_path / "main.elf"
 
-    nso_data = original_nso.read_bytes()
-    nso_hash = hashlib.sha256(nso_data).hexdigest()
+    if hashlib.sha256(comp_v100_nso.read_bytes()).hexdigest() != Version.VER_100.value[1]:
+        setup.fail("internal error while preparing executable (not v1.0); please report")
 
-    if nso_hash == UNCOMPRESSED_V10_HASH:
-        print(">>> found uncompressed 1.0 NSO")
-        TARGET_PATH.write_bytes(nso_data)
-
-    elif nso_hash == COMPRESSED_V10_HASH:
-        print(">>> found compressed 1.0 NSO")
-        setup._decompress_nso(original_nso, TARGET_PATH)
-
+    comp_nso = temp_dir / (version.name+"_comp.nso")
+    if version != Version.VER_100:
+        patch_path = download_patch(Version.VER_100, version, temp_dir)
+        setup._apply_xdelta3_patch(comp_v100_nso, patch_path, comp_nso)
     else:
-        setup.fail(f"unknown executable: {nso_hash}")
+        comp_nso = comp_v100_nso
+    
+    setup._decompress_nso(comp_nso, TARGET_PATH)
 
     if not TARGET_PATH.is_file():
         setup.fail("internal error while preparing executable (missing NSO); please report")
@@ -66,6 +72,39 @@ def prepare_executable(original_nso: Optional[Path]):
 
     if not TARGET_ELF_PATH.is_file():
         setup.fail("internal error while preparing executable (missing ELF); please report")
+    
+
+def prepare_executable(original_nso: Optional[Path]):
+    temp_dir_ = tempfile.TemporaryDirectory()
+    temp_dir = Path(temp_dir_.name)
+
+    if(original_nso is not None):
+        orig_hash = hashlib.sha256(original_nso.read_bytes()).hexdigest()
+        v1_nso = temp_dir / "1.0.nso"
+        if(orig_hash == Version.VER_130.value[1]):
+            patch_path = download_patch(Version.VER_130, Version.VER_100, temp_dir)
+            setup._apply_xdelta3_patch(original_nso, patch_path, v1_nso)
+        elif(orig_hash == Version.VER_100.value[1]):
+            v1_nso = original_nso
+        else:
+            setup.fail(f"unknown executable: {orig_hash}")
+
+
+    for ver in Version:
+        if(check_executable(ver)):
+            print(f">>> {ver} is already set up")
+            continue
+
+        if original_nso is None:
+            setup.fail("please pass a path to the NSO (refer to the readme for more details)")
+        if not original_nso.is_file():
+            setup.fail(f"{original_nso} is not a file")
+        if not v1_nso.is_file():
+            setup.fail(f"Failed to create 1.0-nso: {v1_nso} is not a file")
+        
+        prepare_single_executable(v1_nso, ver, temp_dir)
+
+    temp_dir_.cleanup()
 
 def get_build_dir():
     return setup.ROOT / "build"
@@ -155,8 +194,7 @@ def setup_project_tools(tools_from_source):
                 build_tools_from_source(tmpdir)
 
 def create_build_dir(ver, cmake_backend):
-    if(ver != Version.VER_100): return # TODO: remove this when multiple versions should be built
-    build_dir = get_build_dir()
+    build_dir = setup.ROOT / "build" / ver.value[0]
     if build_dir.is_dir():
         print(">>> build directory already exists: nothing to do")
         return
