73fbd0b34c49955c993305d157bee295185c1b1e
MonsterDruide1 <5958456@gmail.com>
2023-09-25 20:34:02 +0200

    Add headers workflow

---
+++ .github/scripts/copy-headers.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+# bash boilerplate
+set -euo pipefail # strict mode
+readonly SCRIPT_NAME="$(basename "$0")"
+readonly SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+function l { # Log a message to the terminal.
+    echo
+    echo -e "[$SCRIPT_NAME] ${1:-}"
+}
+
+# Delete everything within the headers repo to get a fresh start
+rm -r $DESTINATION_PATH/*
+
+# Copy libraries
+mkdir $DESTINATION_PATH/NintendoSDK $DESTINATION_PATH/agl $DESTINATION_PATH/al $DESTINATION_PATH/eui $DESTINATION_PATH/sead
+cp -r ./lib/NintendoSDK/include/* $DESTINATION_PATH/NintendoSDK
+cp -r ./lib/aarch64 $DESTINATION_PATH/
+cp -r ./lib/agl/include/* $DESTINATION_PATH/agl
+cp -r ./lib/al/include/* $DESTINATION_PATH/al
+cp -r ./lib/eui/include/* $DESTINATION_PATH/eui
+cp -r ./lib/sead/include/* $DESTINATION_PATH/sead
+
+# Copy headers of game
+mkdir $DESTINATION_PATH/game
+rsync -a --prune-empty-dirs --include '*/' --include '*.h' --exclude '*' src/ $DESTINATION_PATH/game
+
+# Make all contents of every class public
+grep -rli 'private:' * | xargs -i@ sed -i 's/private:/public:/g' @
+grep -rli 'protected:' * | xargs -i@ sed -i 's/protected:/public:/g' @
---
+++ .github/scripts/push-headers.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# bash boilerplate
+readonly SCRIPT_NAME="$(basename "$0")"
+readonly SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+function l { # Log a message to the terminal.
+    echo
+    echo -e "[$SCRIPT_NAME] ${1:-}"
+}
+
+# move to the root the OdysseyHeaders repo
+cd "./OdysseyHeaders"
+echo "Open root of OdysseyHeaders repo"
+
+git add -A .
+git config user.name $COMMIT_AUTHOR_NAME
+git config user.email $COMMIT_AUTHOR_MAIL
+git commit -am "$COMMIT_MESSAGE"
+git push -f -u origin $HEADER_BRANCH
+
+echo "Updated headers successfully pushed to OdysseyHeaders repo"
