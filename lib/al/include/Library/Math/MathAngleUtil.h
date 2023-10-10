ed167b390b05d70ce8c73b1c9486de46d2787a97
MonsterDruide1 <5958456@gmail.com>
2023-10-10 16:07:45 +0200

    clang-format

--- lib/al/include/Library/Math/MathAngleUtil.h
+++ lib/al/include/Library/Math/MathAngleUtil.h
@@ -10,11 +10,13 @@ f32 calcAngleDegree(const sead::Vector3f&, const sead::Vector3f&);
 f32 calcAngleDegree(const sead::Vector2f&, const sead::Vector2f&);
 f32 calcAngleOnPlaneRadian(const sead::Vector3f&, const sead::Vector3f&, const sead::Vector3f&);
 f32 calcAngleOnPlaneDegree(const sead::Vector3f&, const sead::Vector3f&, const sead::Vector3f&);
-f32 calcAngleOnPlaneDegreeOrZero(const sead::Vector3f&, const sead::Vector3f&, const sead::Vector3f&);
+f32 calcAngleOnPlaneDegreeOrZero(const sead::Vector3f&, const sead::Vector3f&,
+                                 const sead::Vector3f&);
 s32 calcAngleSignOnPlane(const sead::Vector3f&, const sead::Vector3f&, const sead::Vector3f&);
 
 bool tryCalcAngleDegree(f32*, const sead::Vector3f&, const sead::Vector3f&);
-bool tryCalcAngleOnPlaneDegree(f32*, const sead::Vector3f&, const sead::Vector3f&, const sead::Vector3f&);
+bool tryCalcAngleOnPlaneDegree(f32*, const sead::Vector3f&, const sead::Vector3f&,
+                               const sead::Vector3f&);
 
 bool isNearZero(const sead::Vector2f& vec, f32 tolerance);
 bool isNearZero(const sead::Vector3f& vec, f32 tolerance);
@@ -23,9 +25,12 @@ bool isNearAngleRadian(const sead::Vector2f&, const sead::Vector2f&, f32);
 bool isNearAngleRadian(const sead::Vector3f&, const sead::Vector3f&, f32);
 bool isNearAngleDegree(const sead::Vector2f&, const sead::Vector2f&, f32);
 bool isNearAngleDegree(const sead::Vector3f&, const sead::Vector3f&, f32);
-bool isNearAngleRadianHV(const sead::Vector3f&, const sead::Vector3f&, const sead::Vector3f&, f32, f32);
-bool isNearAngleDegreeHV(const sead::Vector3f&, const sead::Vector3f&, const sead::Vector3f&, f32, f32);
-bool isInAngleOnPlaneDegreeHV(const sead::Vector3f&, const sead::Vector3f&, const sead::Vector3f&, f32, f32, f32, f32);
+bool isNearAngleRadianHV(const sead::Vector3f&, const sead::Vector3f&, const sead::Vector3f&, f32,
+                         f32);
+bool isNearAngleDegreeHV(const sead::Vector3f&, const sead::Vector3f&, const sead::Vector3f&, f32,
+                         f32);
+bool isInAngleOnPlaneDegreeHV(const sead::Vector3f&, const sead::Vector3f&, const sead::Vector3f&,
+                              f32, f32, f32, f32);
 
 void normalize(sead::Vector2f*, const sead::Vector2f&);
 void normalize(sead::Vector3f*, const sead::Vector3f&);
