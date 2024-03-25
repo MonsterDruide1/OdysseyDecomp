328cbf1adf92064236b2d6b66d04da8092e02217
vakhdmyt06 <vakhdmyt06@gmail.com>
2024-03-25 21:45:00 +0100

    Update ActionEffectCtrl

--- lib/al/include/Project/Action/ActionEffectCtrl.h
+++ lib/al/include/Project/Action/ActionEffectCtrl.h
@@ -1,18 +1,28 @@
 #pragma once
 
 #include <basis/seadTypes.h>
-
 #include "Library/Effect/IUseEffectKeeper.h"
 
 namespace al {
-
-struct ActionEffectCtrlInfo {};
-
-class ActionEffectCtrl {
-private:
-    IUseEffectKeeper* mIEffectKeeper;
-    s32 mInfoMax;
-    ActionEffectCtrlInfo* mInfoArr;
+struct ActionEffectCtrlInfo {
+    const char* mName;
+    const char* mMaterialName;
+    bool mHasEmittedEffect;
 };
 
+class ActionEffectCtrl : public IUseEffectKeeper {
+public:
+    static ActionEffectCtrl* tryCreate(al::IUseEffectKeeper);
+
+    ActionEffectCtrl(al::IUseEffectKeeper*);
+
+    void startAction(const char*);
+    void update(float, float, float, bool);
+    bool isKeepSameEffectNext(const al::ActionEffectCtrlInfo*, const char*);
+
+private:
+    EffectKeeper* mEffectKeeper;
+    s32 mEffectCount;
+    ActionEffectCtrlInfo mEffectStack;
+};
 }  // namespace al
