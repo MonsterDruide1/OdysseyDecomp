57754d97ead5f202b81463750808ec15cf91d7da
Fuzzy2319 <alexandre.verbrigghe@orange.fr>
2024-04-08 11:18:57 +0200

    add: AccountHolder header

---
+++ lib/al/include/Library/Network/AccountHolder.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <nn/account.h>
+
+#include "Library/HostIO/HioNode.h"
+
+namespace al {
+class AccountHolder: public HioNode {
+public:
+    AccountHolder();
+	~AccountHolder();
+	
+	bool isValid() const;	
+private:
+    UserHandle *mUserHandle;
+	Uid mUid;
+	bool mIsValid;
+};
+
+static_assert(sizeof(AccountHolder) == 0x28);
+}  // namespace al
