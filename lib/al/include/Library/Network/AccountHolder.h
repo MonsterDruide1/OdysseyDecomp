d59fac316f33cd80bc5c955fe37ffb20dff361d5
Fuzzy2319 <alexandre.verbrigghe@orange.fr>
2024-04-08 11:21:23 +0200

    fix:indent

--- lib/al/include/Library/Network/AccountHolder.h
+++ lib/al/include/Library/Network/AccountHolder.h
@@ -8,13 +8,13 @@ namespace al {
 class AccountHolder: public HioNode {
 public:
     AccountHolder();
-	~AccountHolder();
-	
-	bool isValid() const;	
+    ~AccountHolder();
+
+    bool isValid() const;	
 private:
     UserHandle *mUserHandle;
-	Uid mUid;
-	bool mIsValid;
+    Uid mUid;
+    bool mIsValid;
 };
 
 static_assert(sizeof(AccountHolder) == 0x28);
