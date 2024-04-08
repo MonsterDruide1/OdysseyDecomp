c1b1fed19e97a39aac00fbc466e260d23ee1dd34
Fuzzy2319 <alexandre.verbrigghe@orange.fr>
2024-04-08 20:30:31 +0200

    add:AccountHolder implementation

--- lib/al/include/Library/Network/AccountHolder.h
+++ lib/al/include/Library/Network/AccountHolder.h
@@ -5,17 +5,18 @@
 #include "Library/HostIO/HioNode.h"
 
 namespace al {
-class AccountHolder: public HioNode {
+class AccountHolder : public HioNode {
 public:
     AccountHolder();
     ~AccountHolder();
 
-    bool isValid() const;	
+    bool isValid() const;
+
 private:
-    UserHandle *mUserHandle;
-    Uid mUid;
-    bool mIsValid;
+    nn::account::UserHandle* mUserHandle = nullptr;
+    nn::account::Uid mUid{};
+    bool mIsValid = false;
 };
 
-static_assert(sizeof(AccountHolder) == 0x28);
+static_assert(sizeof(al::AccountHolder) == 0x28);
 }  // namespace al
