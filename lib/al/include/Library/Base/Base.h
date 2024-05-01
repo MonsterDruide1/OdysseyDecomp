7325f50e48614ed9b5bf735324a9831aabb1b9c8
Fuzzy2319 <alexandre.verbrigghe@orange.fr>
2024-05-01 17:17:37 +0200

    add:hash code functions && update:getBaseName

--- lib/al/include/Library/Base/Base.h
+++ lib/al/include/Library/Base/Base.h
@@ -1,5 +1,15 @@
 #pragma once
 
+#include <basis/seadTypes.h>
+#include <cctype>
+#include <cstdarg>
+#include <cstdio>
+#include <cstring>
+
 namespace al {
-const char* getBaseName(const char*);
-}
+s32 calcHashCode(char const* hash);
+s32 calcHashCodeLower(char const* hash);
+s32 calcHashCodeFmt(char const* format, std::va_list argv);
+s32 calcHashCodeFmt(char const* format, ...);
+const char* getBaseName(char const* name);
+}  // namespace al
