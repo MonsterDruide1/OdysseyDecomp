4020af125506224741928b578ebc6742b299a780
MonsterDruide1 <5958456@gmail.com>
2024-05-06 15:16:32 +0200

    Library/HitSensor: Complete HitSensorKeeper.h
    Co-authored-by: Aubrey Taylor <aubrey@hall.ly>

--- lib/al/Library/HitSensor/HitSensorKeeper.h
+++ lib/al/Library/HitSensor/HitSensorKeeper.h
@@ -24,8 +24,18 @@ public:
     void invalidateBySystem();
     HitSensor* getSensor(const char*) const;
 
+    HitSensor* addSensor(LiveActor*, const char*, u32, f32, u16, const sead::Vector3f*,
+                         const sead::Matrix34f*, const sead::Vector3f&);
     void update();
+    s32 getSensorNum() const;
+    HitSensor* getSensor(s32) const;
     void attackSensor();
+    void clear();
+    void validate();
+    void invalidate();
+    void validateBySystem();
+    void invalidateBySystem();
+    HitSensor* getSensor(const char*) const;
 
 private:
     s32 mSensorCount;
