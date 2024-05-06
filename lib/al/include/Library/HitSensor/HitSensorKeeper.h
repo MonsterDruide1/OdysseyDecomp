db6b8722b993c0a287878f6d8c8d447f92e46ce9
MonsterDruide1 <5958456@gmail.com>
2024-05-06 15:16:32 +0200

    Library/HitSensor: Complete HitSensorKeeper.h
    Co-authored-by: Aubrey Taylor <aubrey@hall.ly>

--- lib/al/include/Library/HitSensor/HitSensorKeeper.h
+++ lib/al/include/Library/HitSensor/HitSensorKeeper.h
@@ -1,18 +1,33 @@
 #pragma once
 
+#include <math/seadMatrix.h>
+#include <math/seadVector.h>
+
 namespace al {
 class HitSensor;
+class LiveActor;
 
 class HitSensorKeeper {
 public:
-    HitSensorKeeper(u32);
+    HitSensorKeeper(s32);
 
+    HitSensor* addSensor(LiveActor*, const char*, u32, f32, u16, const sead::Vector3f*,
+                         const sead::Matrix34f*, const sead::Vector3f&);
+    void update();
+    s32 getSensorNum() const;
+    HitSensor* getSensor(s32) const;
+    void attackSensor();
+    void clear();
+    void validate();
+    void invalidate();
+    void validateBySystem();
+    void invalidateBySystem();
     HitSensor* getSensor(const char*) const;
-    HitSensor* getSensor(u32) const;
-    u32 getSensorNum();
 
 private:
-    u32 mSensorCount;
+    s32 mSensorCount;
     HitSensor** mSensors;
 };
+static_assert(sizeof(HitSensorKeeper) == 0x10);
+
 }  // namespace al
