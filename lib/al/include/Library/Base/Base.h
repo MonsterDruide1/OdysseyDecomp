2b0f71d819fc0bf040fb85f2ce8e006fb444abd0
Fuzzy2319 <61144247+Fuzzy2319@users.noreply.github.com>
2024-05-09 19:30:16 +0200

    Library/Base: Add hash code functions (#35)

--- lib/al/include/Library/Base/Base.h
+++ lib/al/include/Library/Base/Base.h
@@ -1,5 +1,12 @@
 #pragma once
 
+#include <basis/seadTypes.h>
+#include <cstdarg>
+
 namespace al {
-const char* getBaseName(const char*);
-}
+s32 calcHashCode(char const* hash);
+s32 calcHashCodeLower(char const* hash);
+s32 calcHashCodeFmt(char const* format, std::va_list argv);
+s32 calcHashCodeFmt(char const* format, ...);
+const char* getBaseName(char const* name);
+}  // namespace al
--- lib/al/src/Library/Base/Base.cpp
+++ lib/al/src/Library/Base/Base.cpp
@@ -1,13 +1,52 @@
 #include "Library/Base/Base.h"
 
+#include <cctype>
+#include <cstdio>
 #include <cstring>
 
 namespace al {
-const char* getBaseName(const char* string) {
-    const char* result = strrchr(string, '/');
-    if (result)
-        return result + 1;
-    else
-        return string;
+s32 calcHashCode(char const* hash) {
+    if (hash[0] == '\0') {
+        return 0;
+    }
+
+    s32 hashCode = 0;
+    for (s32 i = 0; hash[i] != '\0'; i++) {
+        hashCode = (hashCode * 0x1f) + hash[i];
+    }
+
+    return hashCode;
+}
+
+s32 calcHashCodeLower(char const* hash) {
+    s32 hashCode = 0;
+    for (s32 i = 0; hash[i] != '\0'; i++) {
+        hashCode = (hashCode * 0x1f) + tolower(hash[i]);
+    }
+
+    return hashCode;
+}
+
+s32 calcHashCodeFmt(char const* format, std::va_list argv) {
+    char buf[0x100];
+    vsnprintf(buf, 0x100, format, argv);
+
+    return calcHashCode(buf);
+}
+
+s32 calcHashCodeFmt(char const* format, ...) {
+    std::va_list argv;
+
+    va_start(argv, format);
+    s32 result = calcHashCodeFmt(format, argv);
+    va_end(argv);
+
+    return result;
+}
+
+const char* getBaseName(char const* name) {
+    const char* baseName = strrchr(name, '/');
+
+    return baseName != nullptr ? baseName + 1 : name;
 }
 }  // namespace al
