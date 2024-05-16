58969f3aa487db3074b7cc2aeeb36af94d59cebe
Fuzzy2319 <alexandre.verbrigghe@orange.fr>
2024-05-16 20:46:02 +0200

    add:AllDeadWatch header

---
+++ lib/al/include/Library/Obj/AllDeadWatcher.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "Library/LiveActor/LiveActor.h"
+#include "Library/Nerve/NerveSetupUtil.h"
+
+namespace al {
+class AllDeadWatcher : public LiveActor {
+public:
+    AllDeadWatcher(const char* name);
+
+    void appear() override;
+    void init(const al::ActorInitInfo& info) override;
+    void kill() override;
+
+    void exeWait();
+    void exeWatch();
+
+private:
+    LiveActor** mTargets = nullptr;
+    s32 mTargetCount = 0;
+    s32 mSwitchOnDelayStep = 10;
+    bool mIsNotControlActorAppear = false;
+};
+
+static_assert(sizeof(AllDeadWatcher) == 0x120);
+
+namespace {
+NERVE_IMPL(AllDeadWatcher, Watch);
+NERVE_IMPL(AllDeadWatcher, Wait);
+
+struct {
+    NERVE_MAKE(AllDeadWatcher, Watch);
+    NERVE_MAKE(AllDeadWatcher, Wait);
+} NrvAllDeadWatcher;
+}  // namespace
+}  // namespace al
