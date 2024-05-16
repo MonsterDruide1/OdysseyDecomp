891448404337957d09dedde163a3acc0a02b830d
MonsterDruide1 <5958456@gmail.com>
2024-05-16 23:59:30 +0200

    Add CollisionShapeInfoSphere

--- src/Player/CollisionShapeInfo.cpp
+++ src/Player/CollisionShapeInfo.cpp
@@ -1,4 +1,5 @@
 #include "Player/CollisionShapeInfo.h"
+#include "math/seadMatrix.h"
 
 CollisionShapeInfoBase::CollisionShapeInfoBase(CollisionShapeId id, const char* name)
     : mId(id), mName(name) {}
@@ -24,3 +25,39 @@ f32 CollisionShapeInfoBase::getCheckStepRangeWorld() const {
 void CollisionShapeInfoBase::updateShapeOffset(const sead::Vector3f& offset) {}
 void CollisionShapeInfoBase::calcWorldShapeInfo(const sead::Matrix34f& matrix, f32) {}
 void CollisionShapeInfoBase::calcRelativeShapeInfo(const sead::Matrix34f& matrix) {}
+
+CollisionShapeInfoSphere::CollisionShapeInfoSphere(char const* name, float boundingRadius,
+                                                   const sead::Vector3f& boundingCenter)
+    : CollisionShapeInfoBase(CollisionShapeId::Sphere, name), mBoundingRadius(boundingRadius), mBoundingCenter(boundingCenter) {
+    calcWorldShapeInfo(sead::Matrix34f::ident, 1.0f);
+}
+const sead::Vector3f& CollisionShapeInfoSphere::getBoundingCenter() const {
+    return mBoundingCenter;
+}
+const sead::Vector3f& CollisionShapeInfoSphere::getBoundingCenterWorld() const {
+    return mBoundingCenterWorld;
+}
+f32 CollisionShapeInfoSphere::getBoundingRadius() const {
+    return mBoundingRadius;
+}
+f32 CollisionShapeInfoSphere::getBoundingRadiusWorld() const {
+    return mBoundingRadiusWorld;
+}
+f32 CollisionShapeInfoSphere::getCheckStepRange() const {
+    return mBoundingRadius;
+}
+f32 CollisionShapeInfoSphere::getCheckStepRangeWorld() const {
+    return mBoundingRadiusWorld;
+}
+void CollisionShapeInfoSphere::updateShapeOffset(const sead::Vector3f& offset) {
+    mBoundingCenter = offset;
+}
+void CollisionShapeInfoSphere::calcWorldShapeInfo(const sead::Matrix34f& mtx, f32 scale) {
+    mBoundingCenterWorld.setMul(mtx, mBoundingCenter * scale);
+    mBoundingRadiusWorld = mBoundingRadius * scale;
+    unk2 = unk1 * scale;
+    mWorldShapeInfo.setRotated(mtx, mUp);
+}
+void CollisionShapeInfoSphere::calcRelativeShapeInfo(const sead::Matrix34f& mtx) {
+    mRelativeShapeInfo.setMul(mtx, mBoundingCenterWorld);
+}
