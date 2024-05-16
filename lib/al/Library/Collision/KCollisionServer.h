28f2e749b03fb5ecccaedf390811bc5ea4400434
MonsterDruide1 <5958456@gmail.com>
2024-05-16 15:46:46 +0200

    Library/Collision: Implement Sphere[Pose]Interpolator (#37)

--- lib/al/Library/Collision/KCollisionServer.h
+++ lib/al/Library/Collision/KCollisionServer.h
@@ -4,6 +4,7 @@
 #include <math/seadVector.h>
 #include <container/seadPtrArray.h>
 #include <container/seadRingBuffer.h>
+#include <math/seadQuat.h>
 #include <math/seadVector.h>
 #include <prim/seadDelegate.h>
 
@@ -168,6 +169,54 @@ private:
     sead::Vector3u mCoordShift = {0,0,0};
     sead::Vector3u unkShiftedByCoordShift = {0,0,0};
     f32 mFarthestVertexDistance = 1.0f;
+};
+
+class SphereInterpolator {
+public:
+    SphereInterpolator() {}
+    void startInterp(const sead::Vector3f& posStart, const sead::Vector3f& posEnd, f32 sizeStart,
+                     f32 sizeEnd, f32 steps);
+    void nextStep();
+    void calcInterpPos(sead::Vector3f* pos) const;
+    void calcInterp(sead::Vector3f* pos, f32* size, sead::Vector3f* remainMoveVec) const;
+    void calcRemainMoveVector(sead::Vector3f* remainMoveVec) const;
+    void getMoveVector(sead::Vector3f* moveVec);
+    void calcStepMoveVector(sead::Vector3f* moveVec) const;
+
+private:
+    sead::Vector3f mPos;
+    sead::Vector3f mMove;
+    f32 mSizeStart;
+    f32 mSizeEnd;
+    f32 mStepSize;
+    f32 mCurrentStep;
+    f32 mPrevStep;
+};
+
+class SpherePoseInterpolator {
+public:
+    SpherePoseInterpolator() {}
+    void startInterp(const sead::Vector3f& posStart, const sead::Vector3f& posEnd, f32 sizeStart,
+                     f32 sizeEnd, const sead::Quatf& quatStart, const sead::Quatf& quatEnd,
+                     f32 steps);
+    void nextStep();
+    void calcInterpPos(sead::Vector3f* pos) const;
+    void calcInterp(sead::Vector3f* pos, f32* size, sead::Quatf* quat,
+                    sead::Vector3f* remainMoveVec) const;
+    void calcRemainMoveVector(sead::Vector3f* remainMoveVec) const;
+    f32 calcRadiusBaseScale(f32 unk) const;
+    void getMoveVector(sead::Vector3f* moveVec);
+
+private:
+    sead::Vector3f mPos;
+    sead::Vector3f mMove;
+    f32 mSizeStart;
+    f32 mSizeEnd;
+    sead::Quatf mQuatStart;
+    sead::Quatf mQuatEnd;
+    f32 mStepSize;
+    f32 mCurrentStep;
+    f32 mPrevStep;
 };
 
 }  // namespace al
