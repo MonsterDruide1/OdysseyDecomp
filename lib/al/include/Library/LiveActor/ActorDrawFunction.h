29a8bafe187033495eef19c9b866d31a4098a9c5
Fuzzy2319 <alexandre.verbrigghe@orange.fr>
2024-05-17 12:52:58 +0200

    fix: missing const in al::isAlive definition

--- lib/al/include/Library/LiveActor/ActorDrawFunction.h
+++ lib/al/include/Library/LiveActor/ActorDrawFunction.h
@@ -3,7 +3,7 @@
 #include "Library/LiveActor/LiveActor.h"
 
 namespace al {
-// bool isAlive(LiveActor* actor);
+// bool isAlive(LiveActor const* actor);
 void initActorSceneInfo(al::LiveActor* actor, al::ActorInitInfo const& info);
 void initStageSwitch(al::LiveActor* actor, al::ActorInitInfo const& info);
 void initExecutorWatchObj(al::LiveActor* actor, al::ActorInitInfo const& info);
