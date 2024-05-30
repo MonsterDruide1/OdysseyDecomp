e6b391d222ef135819791e16d51fe50ce8957d87
MonsterDruide1 <5958456@gmail.com>
2024-05-30 17:40:23 +0200

    Player: Implement PlayerJudgePreInputJump

---
+++ src/Player/PlayerJudgePreInputJump.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <basis/seadTypes.h>
+
+#include "Library/HostIO/HioNode.h"
+
+#include "Player/IJudge.h"
+
+class PlayerConst;
+class PlayerInput;
+
+class PlayerJudgePreInputJump : public al::HioNode, public IJudge {
+public:
+    PlayerJudgePreInputJump(const PlayerConst* pConst, const PlayerInput* input,
+                            IJudge* judgeForceSlopeSlide);
+    void reset() override;
+    void update() override;
+    bool judge() const override;
+
+private:
+    const PlayerConst* mConst;
+    const PlayerInput* mInput;
+    IJudge* mJudgeForceSlopeSlide;
+    s32 mRemainJumpFrame = 0;
+};
