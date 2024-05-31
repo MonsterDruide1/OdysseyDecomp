86a4d4b1ac59ac77318280e7519613e8bf47b270
Fuzzy2319 <61144247+Fuzzy2319@users.noreply.github.com>
2024-05-31 12:00:02 +0200

    Library/Debug: Implement Host (#44)

---
+++ lib/al/include/Library/Debug/Host.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <prim/seadSafeString.h>
+
+#include "Library/Base/String.h"
+
+namespace al {
+bool tryGetComputerName(sead::BufferedSafeString* computerName);
+void getComputerName(sead::BufferedSafeString* computerName);
+void getUserName(sead::BufferedSafeString* userName);
+void makeUniqueTemporaryFilename(sead::BufferedSafeString* out, const char* fileName);
+void expandEnvironmentString(sead::BufferedSafeString* out, const sead::SafeString& envStr);
+void FUN_710086f65c(sead::BufferedSafeString* out,
+                    const sead::SafeString& envStr);  // TODO: Find what is this function
+sead::FixedSafeString<128> makeTmpExpandEnvironmentString(const sead::SafeString& envStr);
+StringTmp<128> makeTmpFileFullPath(const char* fileName);
+const char* getALCommon();
+}  // namespace al
---
+++ lib/al/src/Library/Debug/Host.cpp
@@ -0,0 +1,57 @@
+#include "Library/Debug/Host.h"
+
+#include <devenv/seadEnvUtil.h>
+#include <nn/os.h>
+
+namespace al {
+void getComputerName(sead::BufferedSafeString* computerName) {
+    tryGetComputerName(computerName);
+}
+
+bool tryGetComputerName(sead::BufferedSafeString* computerName) {
+    computerName->format("");
+    computerName->format("");
+
+    return sead::EnvUtil::getEnvironmentVariable(computerName, "COMPUTERNAME") > 0;
+}
+
+void getUserName(sead::BufferedSafeString* userName) {
+    userName->format("");
+    userName->format("");
+
+    sead::EnvUtil::getEnvironmentVariable(userName, "USERNAME");
+}
+
+void makeUniqueTemporaryFilename(sead::BufferedSafeString* out, const char* fileName) {
+    sead::FixedSafeString<128> computerName;
+    getComputerName(&computerName);
+
+    nn::os::Tick time = nn::os::GetSystemTick();
+    out->format("%s_%012lld%s", computerName.cstr(), time, fileName);
+}
+
+void expandEnvironmentString(sead::BufferedSafeString* out, const sead::SafeString& envStr) {
+    out->clear();
+
+    FUN_710086f65c(out, envStr);
+}
+
+// void FUN_710086f65c(sead::BufferedSafeString* out, const sead::SafeString& envStr) {}
+
+sead::FixedSafeString<128> makeTmpExpandEnvironmentString(const sead::SafeString& envStr) {
+    sead::FixedSafeString<128> tmp;
+
+    expandEnvironmentString(&tmp, envStr);
+
+    return tmp;
+}
+
+StringTmp<128> makeTmpFileFullPath(const char* fileName) {
+    return StringTmp<128>("${TEMP}/%012lld%s", nn::os::GetSystemTick(),
+                          fileName != nullptr ? fileName : "");
+}
+
+const char* getALCommon() {
+    return "${AL_TOOL_ROOT}/ALCommon";
+}
+}  // namespace al
