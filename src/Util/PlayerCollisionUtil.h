bba49e8808c77918f0830e198e3a1b4376d78ad5
MonsterDruide1 <5958456@gmail.com>
2024-06-01 12:43:34 +0200

    Player: Implement PlayerJudgeSpeedCheckFall

--- src/Util/PlayerCollisionUtil.h
+++ src/Util/PlayerCollisionUtil.h
@@ -2,10 +2,16 @@
 
 #include <basis/seadTypes.h>
 
+namespace al {
+class LiveActor;
+}
+class IUsePlayerCollision;
 class IUsePlayerHeightCheck;
+class PlayerConst;
 
 namespace rs {
 
 f32 getGroundHeight(const IUsePlayerHeightCheck*);
+bool isCollidedGroundRunAngle(const al::LiveActor*, const IUsePlayerCollision*, const PlayerConst*);
 
-}
+}  // namespace rs
