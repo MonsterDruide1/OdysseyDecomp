44ad801ea0eb894c425f8db3a457a23afce2baec
GRAnimated <devin@nhscan.com>
2024-06-02 11:21:35 -0400

    Fix formatting

--- lib/al/include/Library/Base/HashCodeUtil.h
+++ lib/al/include/Library/Base/HashCodeUtil.h
@@ -4,9 +4,9 @@
 #include <cstdarg>
 
 namespace al {
-s32 calcHashCode(char const* str);
-s32 calcHashCodeLower(char const* str);
-s32 calcHashCodeFmt(char const* format, std::va_list argv);
-s32 calcHashCodeFmt(char const* format, ...);
-const char* getBaseName(char const* name);
+s32 calcHashCode(const char* str);
+s32 calcHashCodeLower(const char* str);
+s32 calcHashCodeFmt(const char* format, std::va_list argv);
+s32 calcHashCodeFmt(const char* format, ...);
+const char* getBaseName(const char* name);
 }  // namespace al
