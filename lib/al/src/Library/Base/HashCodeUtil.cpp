a99d6537e4ee6eead77b3291d983e8766f41de93
GRAnimated <devin@nhscan.com>
2024-06-02 15:12:59 -0400

    Fix formatting

--- lib/al/src/Library/Base/HashCodeUtil.cpp
+++ lib/al/src/Library/Base/HashCodeUtil.cpp
@@ -5,7 +5,7 @@
 #include <cstring>
 
 namespace al {
-s32 calcHashCode(char const* str) {
+s32 calcHashCode(const char* str) {
     if (str[0] == '\0') {
         return 0;
     }
@@ -18,7 +18,7 @@ s32 calcHashCode(char const* str) {
     return hashCode;
 }
 
-s32 calcHashCodeLower(char const* str) {
+s32 calcHashCodeLower(const char* str) {
     s32 hashCode = 0;
     for (s32 i = 0; str[i] != '\0'; i++) {
         hashCode = (hashCode * 0x1f) + tolower(str[i]);
@@ -27,14 +27,14 @@ s32 calcHashCodeLower(char const* str) {
     return hashCode;
 }
 
-s32 calcHashCodeFmt(char const* format, std::va_list argv) {
+s32 calcHashCodeFmt(const char* format, std::va_list argv) {
     char buf[0x100];
     vsnprintf(buf, 0x100, format, argv);
 
     return calcHashCode(buf);
 }
 
-s32 calcHashCodeFmt(char const* format, ...) {
+s32 calcHashCodeFmt(const char* format, ...) {
     std::va_list argv;
 
     va_start(argv, format);
@@ -44,7 +44,7 @@ s32 calcHashCodeFmt(char const* format, ...) {
     return result;
 }
 
-const char* getBaseName(char const* name) {
+const char* getBaseName(const char* name) {
     const char* baseName = strrchr(name, '/');
 
     return baseName != nullptr ? baseName + 1 : name;
