d6296de6408002186b3510be044515d32fd12d59
MonsterDruide1 <5958456@gmail.com>
2024-06-07 22:12:09 +0200

    Player: Implement PlayerWallActionHistory

---
+++ src/Player/PlayerWallActionHistory.cpp
@@ -0,0 +1,32 @@
+#include "Player/PlayerWallActionHistory.h"
+
+#include "Util/PlayerCollisionUtil.h"
+
+PlayerWallActionHistory::PlayerWallActionHistory() {}
+void PlayerWallActionHistory::update(const IUsePlayerCollision* collider) {
+    if (rs::isCollidedGround(collider)) {
+        reset();
+    }
+}
+void PlayerWallActionHistory::reset() {
+    mIsJumpStored = false;
+    mIsLeaveStored = false;
+}
+void PlayerWallActionHistory::recordWallJump(const IUsePlayerCollision* collider,
+                                             const sead::Vector3f& position) {
+    mIsJumpStored = true;
+    mJumpWallPosition = position;
+    mJumpWallNormal = rs::getCollidedWallNormal(collider);
+}
+void PlayerWallActionHistory::recordWallJump(const sead::Vector3f& position,
+                                             const sead::Vector3f& normal) {
+    mIsJumpStored = true;
+    mJumpWallPosition = position;
+    mJumpWallNormal = normal;
+}
+void PlayerWallActionHistory::recordWallLeave(const sead::Vector3f& position,
+                                              const sead::Vector3f& normal) {
+    mIsLeaveStored = true;
+    mLeaveWallPosition = position;
+    mLeaveWallNormal = normal;
+}
--- src/Util/PlayerCollisionUtil.h
+++ src/Util/PlayerCollisionUtil.h
@@ -1,11 +1,14 @@
 #pragma once
 
-#include <basis/seadTypes.h>
+#include <math/seadVector.h>
 
+class IUsePlayerCollision;
 class IUsePlayerHeightCheck;
 
 namespace rs {
 
 f32 getGroundHeight(const IUsePlayerHeightCheck*);
+const sead::Vector3f& getCollidedWallNormal(const IUsePlayerCollision*);
+bool isCollidedGround(const IUsePlayerCollision*);
 
-}
+}  // namespace rs
