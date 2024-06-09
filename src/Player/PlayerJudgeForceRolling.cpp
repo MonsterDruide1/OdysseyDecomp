e43940dc481ed48d579647bdda5d3bd88edf03fb
MonsterDruide1 <5958456@gmail.com>
2024-06-09 12:50:38 +0200

    Player: Implement PlayerJudgeForceRolling

---
+++ src/Player/PlayerJudgeForceRolling.cpp
@@ -0,0 +1,14 @@
+#include "Player/PlayerJudgeForceRolling.h"
+
+#include "Util/PlayerCollisionUtil.h"
+
+PlayerJudgeForceRolling::PlayerJudgeForceRolling(const al::LiveActor* player,
+                                                 const IUsePlayerCollision* collider)
+    : mPlayer(player), mCollider(collider) {}
+
+bool PlayerJudgeForceRolling::judge() const {
+    return rs::isOnGroundForceRollingCode(mPlayer, mCollider);
+}
+
+void PlayerJudgeForceRolling::reset() {}
+void PlayerJudgeForceRolling::update() {}
--- src/Util/PlayerCollisionUtil.h
+++ src/Util/PlayerCollisionUtil.h
@@ -15,5 +15,6 @@ f32 getGroundHeight(const IUsePlayerHeightCheck*);
 const sead::Vector3f& getCollidedWallNormal(const IUsePlayerCollision*);
 bool isCollidedGround(const IUsePlayerCollision*);
 bool isCollidedGroundRunAngle(const al::LiveActor*, const IUsePlayerCollision*, const PlayerConst*);
+bool isOnGroundForceRollingCode(const al::LiveActor*, const IUsePlayerCollision*);
 
 }  // namespace rs
