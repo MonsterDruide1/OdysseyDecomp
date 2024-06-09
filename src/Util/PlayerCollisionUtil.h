e812cfbd4c328cf870acb7f661247d90c50d0b32
MonsterDruide1 <5958456@gmail.com>
2024-06-09 02:07:47 +0200

    Player: Implement PlayerWallActionHistory (#74)

--- src/Util/PlayerCollisionUtil.h
+++ src/Util/PlayerCollisionUtil.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <basis/seadTypes.h>
+#include <math/seadVector.h>
 
 namespace al {
 class LiveActor;
@@ -12,6 +12,8 @@ class PlayerConst;
 namespace rs {
 
 f32 getGroundHeight(const IUsePlayerHeightCheck*);
+const sead::Vector3f& getCollidedWallNormal(const IUsePlayerCollision*);
+bool isCollidedGround(const IUsePlayerCollision*);
 bool isCollidedGroundRunAngle(const al::LiveActor*, const IUsePlayerCollision*, const PlayerConst*);
 
 }  // namespace rs
