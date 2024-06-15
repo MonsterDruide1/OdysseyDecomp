5f5d059b501f1cc31ef6860e4766c87d32c2b341
MonsterDruide1 <5958456@gmail.com>
2024-06-15 15:11:17 +0200

    Player: Implement PlayerJudgeEnableStandUp (#89)

---
+++ src/Player/PlayerJudgeEnableStandUp.cpp
@@ -0,0 +1,16 @@
+#include "Player/PlayerJudgeEnableStandUp.h"
+
+#include "Player/IUsePlayerCeilingCheck.h"
+#include "Util/PlayerCollisionUtil.h"
+
+PlayerJudgeEnableStandUp::PlayerJudgeEnableStandUp(const IUsePlayerCollision* collider,
+                                                   const IUsePlayerCeilingCheck* ceilingChecker)
+    : mCollider(collider), mCeilingChecker(ceilingChecker) {}
+
+bool PlayerJudgeEnableStandUp::judge() const {
+    return mCeilingChecker->isEnableStandUp() || !rs::isCollidedGround(mCollider);
+}
+
+void PlayerJudgeEnableStandUp::reset() {}
+
+void PlayerJudgeEnableStandUp::update() {}
