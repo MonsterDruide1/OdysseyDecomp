26f8fe5dac31f80060ddd72ace82c7e00e1ba4fb
MonsterDruide1 <5958456@gmail.com>
2024-06-19 22:27:38 +0200

    Player: Implement PlayerJudgeStartGroundSpin (#99)

---
+++ src/Player/PlayerJudgeStartGroundSpin.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "Player/IJudge.h"
+
+namespace al {
+class LiveActor;
+}  // namespace al
+class IUsePlayerCollision;
+class PlayerInput;
+
+class PlayerJudgeStartGroundSpin : public IJudge {
+public:
+    PlayerJudgeStartGroundSpin(const al::LiveActor* player, const IUsePlayerCollision* collider,
+                               const PlayerInput* input);
+    void reset() override;
+    void update() override;
+    bool judge() const override;
+
+private:
+    const al::LiveActor* mPlayer;
+    const IUsePlayerCollision* mCollider;
+    const PlayerInput* mInput;
+};
+static_assert(sizeof(PlayerJudgeStartGroundSpin) == 0x20);
