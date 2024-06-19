b18a836668ef9112aed8d9013e34b0c96374793c
MonsterDruide1 <5958456@gmail.com>
2024-06-19 13:56:23 +0200

    Player: Implement PlayerJudgeWallCatch

--- src/Player/PlayerCounterForceRun.h
+++ src/Player/PlayerCounterForceRun.h
@@ -9,6 +9,7 @@ public:
     void setupForceRun(s32 frames, f32 speed);
     void update();
 
+    bool isForceRun() const { return mCounter > 0; }
     s32 getCounter() const { return mCounter; }
     f32 getSpeed() const { return mSpeed; }
 
