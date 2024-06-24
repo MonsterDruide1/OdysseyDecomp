d6422d0adb2aa1cd4ac4f8a5caa33c8d024dcfbe
GRAnimated <devin@nhscan.com>
2024-06-24 02:31:09 -0400

    Gauge Air header

---
+++ src/Layout/GaugeAir.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "Library/Layout/LayoutActor.h"
+
+namespace al {
+class LayoutInitInfo;
+}
+
+class GaugeAir : public al::LayoutActor {
+public:
+    GaugeAir(const char* name, const al::LayoutInitInfo& info);
+    bool isWait() const;
+
+    void start();
+    void updateStateAnim();
+    void endMax();
+    void fastEnd();
+    void setRate(f32 rate);
+    void exeAppear();
+    void exeWait();
+    void exeEnd();
+    void exeFastEnd();
+
+private:
+    bool mIsWait;
+    bool mIsNotFull;
+    float mRate;
+};
