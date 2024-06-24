3cabe18a6b5721dd19b79b707442b2c7b12530c5
GRAnimated <devin@nhscan.com>
2024-06-24 02:15:35 -0400

    Implement FilterFly and FilterScope

---
+++ src/Layout/FilterFly.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <math/seadVector.h>
+
+#include "Library/Layout/LayoutActor.h"
+
+namespace al {
+class LayoutInitInfo;
+}
+
+class FilterFly : public al::LayoutActor {
+public:
+    FilterFly(const char*, const al::LayoutInitInfo&, const char*);
+    void exeWait();
+    void exeWaitEnd();
+    void exeMove();
+    void move(const sead::Vector2f&);
+    bool isWaitEnd() const;
+
+private:
+    sead::Vector2f mTargetPos = sead::Vector2f::zero;
+    sead::Vector2f mTargetPos2 = sead::Vector2f::zero;
+};
