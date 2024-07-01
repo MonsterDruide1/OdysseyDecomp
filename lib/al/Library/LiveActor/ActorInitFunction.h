6f7706299f9b647f2f77a1d1c15c6fddd6ace501
Fuzzy2319 <61144247+Fuzzy2319@users.noreply.github.com>
2024-07-01 12:08:18 +0200

    Library/Obj: Implement `CollisionObj` (#123)

--- lib/al/Library/LiveActor/ActorInitFunction.h
+++ lib/al/Library/LiveActor/ActorInitFunction.h
@@ -1,7 +1,10 @@
 #pragma once
 
+#include <prim/seadSafeString.h>
+
 namespace al {
 class LiveActor;
+class Resource;
 
 LiveActor* createLinksActorFromFactory(const ActorInitInfo& info, const char* linkName,
                                        s32 linkNum);
@@ -9,4 +12,8 @@ bool trySyncStageSwitchAppear(LiveActor* actor);
 void initActorPoseTRSV(LiveActor* actor);
 void initActorSRT(LiveActor* actor, const ActorInitInfo& info);
 bool trySyncStageSwitchAppearAndKill(LiveActor* actor);
+void initActorCollisionWithResource(LiveActor* actor, const Resource* res,
+                                    const sead::SafeString& collisionFileName, HitSensor* hitSensor,
+                                    const sead::Matrix34f* joinMtx, const char* suffix);
+void initExecutorCollisionMapObjDecorationMovement(LiveActor* actor, const ActorInitInfo& info);
 }  // namespace al
--- lib/al/Library/Obj/CollisionObj.h
+++ lib/al/Library/Obj/CollisionObj.h
@@ -1,19 +1,15 @@
 #pragma once
 
 #include "Library/LiveActor/LiveActor.h"
-#include "Library/Resource/Resource.h"
 
 namespace al {
+class Resource;
 
 class CollisionObj : public LiveActor {
 public:
-    CollisionObj(const ActorInitInfo&, Resource*, const char*, HitSensor*, const sead::Matrix34f*,
-                 const char*);
+    CollisionObj(const ActorInitInfo& info, Resource* res, const char* collisionFileName,
+                 HitSensor* hitSensor, const sead::Matrix34f* joinMtx, const char* suffix);
 };
-static_assert(sizeof(CollisionObj) == 0x108, "CollisionObj Size");
 
-CollisionObj* createCollisionObj(const LiveActor*, const ActorInitInfo&, const char*, HitSensor*,
-                                 const char*, const char*);
-CollisionObj* createCollisionObjMtx(const LiveActor*, const ActorInitInfo&, const char*, HitSensor*,
-                                    const sead::Matrix34f*, const char*);
+static_assert(sizeof(CollisionObj) == 0x108, "CollisionObj Size");
 }  // namespace al
