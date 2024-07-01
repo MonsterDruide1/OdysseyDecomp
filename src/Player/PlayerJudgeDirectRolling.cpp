15954247c2d41c822e3f5c76fd5c3a4adfffc43c
MonsterDruide1 <5958456@gmail.com>
2024-07-01 15:10:38 +0200

    Player: Implement `PlayerJudgeDirectRolling` (#124)

---
+++ src/Player/PlayerJudgeDirectRolling.cpp
@@ -0,0 +1,37 @@
+#include "Player/PlayerJudgeDirectRolling.h"
+
+#include "Library/LiveActor/ActorMovementFunction.h"
+#include "Library/LiveActor/ActorPoseKeeper.h"
+#include "Library/Math/MathAngleUtil.h"
+
+#include "Player/IPlayerModelChanger.h"
+#include "Player/PlayerCarryKeeper.h"
+#include "Player/PlayerConst.h"
+#include "Player/PlayerCounterForceRun.h"
+#include "Player/PlayerInput.h"
+#include "Util/PlayerCollisionUtil.h"
+
+PlayerJudgeDirectRolling::PlayerJudgeDirectRolling(
+    const al::LiveActor* player, const PlayerConst* pConst, const PlayerInput* input,
+    const IUsePlayerCollision* collider, const IPlayerModelChanger* modelChanger,
+    const PlayerCarryKeeper* carryKeeper, const PlayerCounterForceRun* counterForceRun)
+    : mPlayer(player), mConst(pConst), mInput(input), mCollider(collider),
+      mModelChanger(modelChanger), mCarryKeeper(carryKeeper), mCounterForceRun(counterForceRun) {}
+
+void PlayerJudgeDirectRolling::reset() {
+    mIsJudge = false;
+}
+
+void PlayerJudgeDirectRolling::update() {
+    mIsJudge = false;
+    if (mCounterForceRun->isForceRun() || mCarryKeeper->isCarry() ||
+        !rs::isOnGround(mPlayer, mCollider) || mModelChanger->is2DModel() || !mInput->isHoldSquat())
+        return;
+    sead::Vector3f horizontalSpeed = {0.0f, 0.0f, 0.0f};
+    al::verticalizeVec(&horizontalSpeed, al::getGravity(mPlayer), al::getVelocity(mPlayer));
+    mIsJudge = horizontalSpeed.length() >= mConst->getDashJudgeSpeed();
+}
+
+bool PlayerJudgeDirectRolling::judge() const {
+    return mIsJudge;
+}
---
+++ src/Player/PlayerJudgeDirectRolling.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "Player/IJudge.h"
+
+namespace al {
+class LiveActor;
+}  // namespace al
+class PlayerConst;
+class PlayerInput;
+class IUsePlayerCollision;
+class IPlayerModelChanger;
+class PlayerCarryKeeper;
+class PlayerCounterForceRun;
+
+class PlayerJudgeDirectRolling : public IJudge {
+public:
+    PlayerJudgeDirectRolling(const al::LiveActor* player, const PlayerConst* pConst,
+                             const PlayerInput* input, const IUsePlayerCollision* collider,
+                             const IPlayerModelChanger* modelChanger,
+                             const PlayerCarryKeeper* carryKeeper,
+                             const PlayerCounterForceRun* counterForceRun);
+
+    void reset() override;
+    void update() override;
+    bool judge() const override;
+
+private:
+    const al::LiveActor* mPlayer;
+    const PlayerConst* mConst;
+    const PlayerInput* mInput;
+    const IUsePlayerCollision* mCollider;
+    const IPlayerModelChanger* mModelChanger;
+    const PlayerCarryKeeper* mCarryKeeper;
+    const PlayerCounterForceRun* mCounterForceRun;
+    bool mIsJudge = false;
+};
