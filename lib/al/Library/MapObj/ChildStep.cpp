d2ae6a40eeb1bf20005b8deb30f4317036896155
Fuzzy2319 <61144247+Fuzzy2319@users.noreply.github.com>
2024-07-25 18:07:05 +0200

    Library/MapObj: Implement `ChildStep` (#136)

---
+++ lib/al/Library/MapObj/ChildStep.cpp
@@ -0,0 +1,65 @@
+#include "Library/MapObj/ChildStep.h"
+
+#include "Library/LiveActor/ActorClippingFunction.h"
+#include "Library/LiveActor/ActorInitFunction.h"
+#include "Library/LiveActor/ActorPoseKeeper.h"
+#include "Library/LiveActor/SubActorFunction.h"
+#include "Library/Nerve/NerveSetupUtil.h"
+#include "Library/Placement/PlacementFunction.h"
+
+namespace {
+using namespace al;
+
+NERVE_IMPL(ChildStep, Wait)
+
+NERVES_MAKE_STRUCT(ChildStep, Wait)
+}  // namespace
+
+namespace al {
+ChildStep::ChildStep(const char* name, LiveActor* parent) : LiveActor(name), mParent(parent) {}
+
+void ChildStep::init(const ActorInitInfo& info) {
+    initMapPartsActor(this, info, nullptr);
+    initNerve(this, &NrvChildStep.Wait, 0);
+
+    multVecInvQuat(&mPos, mParent, getTrans(this));
+
+    makeActorAlive();
+}
+
+bool ChildStep::receiveMsg(const SensorMsg* message, HitSensor* source, HitSensor* target) {
+    return mParent->receiveMsg(message, source, target);
+}
+
+void ChildStep::exeWait() {
+    multVecPose(getTransPtr(this), mParent, mPos);
+}
+
+s32 calcChildStepCount(const ActorInitInfo& info) {
+    return calcLinkChildNum(info, "ChildStep");
+}
+
+void tryInitSubActorKeeperChildStep(LiveActor* actor, const ActorInitInfo& info) {
+    s32 childStepCount = calcChildStepCount(info);
+
+    if (childStepCount <= 0)
+        return;
+
+    initSubActorKeeperNoFile(actor, info, childStepCount);
+}
+
+void createChildStep(const ActorInitInfo& info, LiveActor* parent, bool isSyncClipping) {
+    s32 childStepCount = calcChildStepCount(info);
+
+    for (s32 i = 0; i < childStepCount; i++) {
+        ChildStep* childStep = new ChildStep("子供足場", parent);
+
+        initLinksActor(childStep, info, "ChildStep", i);
+
+        if (isSyncClipping) {
+            invalidateClipping(childStep);
+            registerSubActorSyncClipping(parent, childStep);
+        }
+    }
+}
+}  // namespace al
---
+++ lib/al/Library/MapObj/ChildStep.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "Library/LiveActor/LiveActor.h"
+
+namespace al {
+class ChildStep : public LiveActor {
+public:
+    ChildStep(const char* name, LiveActor* parent);
+
+    void init(const ActorInitInfo& info) override;
+    bool receiveMsg(const SensorMsg* message, HitSensor* source, HitSensor* target) override;
+    void exeWait();
+
+private:
+    LiveActor* mParent;
+    sead::Vector3f mPos = sead::Vector3f::zero;
+};
+
+s32 calcChildStepCount(const ActorInitInfo& info);
+void tryInitSubActorKeeperChildStep(LiveActor* actor, const ActorInitInfo& info);
+void createChildStep(const ActorInitInfo& info, LiveActor* parent, bool isSyncClipping);
+}  // namespace al
