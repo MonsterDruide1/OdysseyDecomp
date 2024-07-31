d45c6f3d8b625e7b12ce022a0fd0b767fec49b7a
Wexos <wexosmk@gmail.com>
2024-07-31 22:59:47 +0200

    Moved some functions from AreaObjUtil to ActorAreaFunction (according to doc)

--- lib/al/Library/Area/AreaObjUtil.h
+++ lib/al/Library/Area/AreaObjUtil.h
@@ -4,15 +4,9 @@
 
 namespace al {
 class AreaObj;
-class LiveActor;
 
 bool isInAreaObj(const IUseAreaObj* area, const char* name, const sead::Vector3f& pos);
 AreaObj* tryFindAreaObj(const IUseAreaObj* area, const char* name, const sead::Vector3f& pos);
-
-bool isInAreaObj(const LiveActor*, const char*);
-bool isInDeathArea(const LiveActor*);
-
-AreaObj* tryFindAreaObj(const LiveActor*, const char*);
 
 bool tryGetAreaObjArg(s32*, const AreaObj*, const char*);
 bool tryGetAreaObjArg(f32*, const AreaObj*, const char*);
