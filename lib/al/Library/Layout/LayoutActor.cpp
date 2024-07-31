2c06a03d644c2114897880e980b5576f03fd918d
Wexos <wexosmk@gmail.com>
2024-07-31 20:51:20 +0200

    Fixed LayoutActor VTable

--- lib/al/Library/Layout/LayoutActor.cpp
+++ lib/al/Library/Layout/LayoutActor.cpp
@@ -3,7 +3,6 @@
 #include "Library/Audio/AudioKeeper.h"
 #include "Library/Layout/LayoutActorUtil.h"
 #include "Library/Layout/LayoutPartsActorKeeper.h"
-#include "Library/Layout/LayoutSceneInfo.h"
 
 namespace al {
 void LayoutActor::appear() {
@@ -22,41 +21,5 @@ void LayoutActor::initLayoutPartsActorKeeper(s32 capacity) {
 
 void LayoutActor::initLayoutKeeper(LayoutKeeper* layoutKeeper) {
     mLayoutKeeper = layoutKeeper;
-}
-
-NerveKeeper* LayoutActor::getNerveKeeper() const {
-    return mNerveKeeper;
-}
-
-const char* LayoutActor::getName() const {
-    return mName.cstr();
-}
-
-EffectKeeper* LayoutActor::getEffectKeeper() const {
-    return mEffectKeeper;
-}
-
-AudioKeeper* LayoutActor::getAudioKeeper() const {
-    return mAudioKeeper;
-}
-
-LayoutActionKeeper* LayoutActor::getLayoutActionKeeper() const {
-    return mLayoutActionKeeper;
-}
-
-LayoutKeeper* LayoutActor::getLayoutKeeper() const {
-    return mLayoutKeeper;
-}
-
-CameraDirector* LayoutActor::getCameraDirector() const {
-    return mLayoutSceneInfo->getCameraDirector();
-}
-
-SceneObjHolder* LayoutActor::getSceneObjHolder() const {
-    return mLayoutSceneInfo->getSceneObjHolder();
-}
-
-const MessageSystem* LayoutActor::getMessageSystem() const {
-    return mLayoutSceneInfo->getMessageSystem();
 }
 }  // namespace al
