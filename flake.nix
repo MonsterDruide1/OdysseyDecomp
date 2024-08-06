048f981b99b3e18949dbb5268e16e708fa3e350f
MonsterDruide1 <5958456@gmail.com>
2024-08-07 00:15:45 +0200

    dev: Add `flake.nix`-devshell and `direnv` setup

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+    systems = import inputs.systems;
+    perSystem = { config, self', inputs', pkgs, system, ... }: let
+      overlays = [ (import inputs.rust-overlay) ];
+      pkgs = import inputs.nixpkgs {
+        inherit system overlays;
+      };
+      rustToolchain = pkgs.pkgsBuildHost.rust-bin.stable.latest.default;
+    in {
+      _module.args.pkgs = import inputs.nixpkgs {
+        config.allowUnfree = true;
+      };
+      devShells.default = pkgs.mkShell rec {
+        buildInputs = with pkgs; [
+          cmake
+          ninja
+          clang
+          ccache
+          pkg-config
+
+          rustToolchain
+          (python3.withPackages (python-pkgs: [
+            python-pkgs.capstone
+            python-pkgs.colorama
+            python-pkgs.cxxfilt
+            python-pkgs.pyelftools
+            python-pkgs.ansiwrap
+            python-pkgs.watchdog
+            python-pkgs.python-Levenshtein
+            python-pkgs.toml
+          ]))
+          openssl
+          libclang
+          ncurses5
+          ncurses6
+        ];
+        LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath buildInputs}";
+        LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+      };
+    };
+  };
+}
