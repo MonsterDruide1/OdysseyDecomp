d8efb8ff27bc5ad1a2330723031c326f92f9beef
MonsterDruide1 <5958456@gmail.com>
2024-09-02 21:00:46 +0200

    Library/Camera: Implement `CameraTicketId` (#152)

---
+++ lib/al/Library/Camera/CameraTicketId.h
@@ -0,0 +1,23 @@
+#pragma once
+
+namespace al {
+class PlacementId;
+class ByamlIter;
+
+class CameraTicketId {
+public:
+    CameraTicketId(const PlacementId* placementId, const char* suffix);
+
+    bool isEqual(const CameraTicketId& other) const;
+    static bool isEqual(const CameraTicketId& ticket1, const CameraTicketId& ticket2);
+    bool isEqual(const ByamlIter& iter) const;
+
+    const char* tryGetObjId() const;
+    const char* getObjId() const;
+
+private:
+    const PlacementId* mPlacementId;
+    const char* mSuffix;
+};
+
+}  // namespace al
