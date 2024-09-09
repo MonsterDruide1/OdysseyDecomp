758ea233edb573048c158f287672d3a498519d71
MonsterDruide1 <5958456@gmail.com>
2024-09-10 01:42:41 +0200

    Add documentation config

---
+++ docs/doxygen_config.py
@@ -0,0 +1,15 @@
+# Configuration file for https://mcss.mosra.cz/documentation/doxygen/
+DOXYFILE = "Doxyfile"
+MAIN_PROJECT_URL = "https://smo.monsterdrui.de"
+SHOW_UNDOCUMENTED = True
+SEARCH_RESULT_ID_BYTES = 4
+SEARCH_FILE_OFFSET_BYTES = 4
+SEARCH_NAME_SIZE_BYTES = 2
+SEARCH_DOWNLOAD_BINARY = True
+HTML_HEADER = """
+<style>
+dl {
+    padding-left: 1em;
+}
+</style>
+"""
