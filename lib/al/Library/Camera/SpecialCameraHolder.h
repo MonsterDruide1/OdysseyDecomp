02015fb45b3df7b983f81fe560571cd741a60eba
MonsterDruide1 <5958456@gmail.com>
2024-09-25 23:28:01 +0200

    Library/Camera: Implement `SpecialCameraHolder` (#161)

---
+++ lib/al/Library/Camera/SpecialCameraHolder.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <basis/seadTypes.h>
+
+namespace al {
+class CameraTicket;
+
+class SpecialCameraHolder {
+public:
+    SpecialCameraHolder();
+
+    void allocEntranceCameraBuffer(s32 maxEntries);
+    void registerEntranceCamera(CameraTicket* ticket);
+    CameraTicket* findEntranceCamera(const char* suffix) const;
+
+private:
+    CameraTicket** mEntranceCameras = nullptr;
+    s32 mMaxEntranceCameras = 0;
+    s32 mNumEntranceCameras = 0;
+};
+
+}  // namespace al
