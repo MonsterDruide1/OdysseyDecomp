3d6069b929ac29e08ffd4007b7a48bde6262478c
MonsterDruide1 <5958456@gmail.com>
2024-10-18 16:53:31 +0200

    Player: Implement `PlayerAreaChecker` (#167)

--- src/Util/Area.h
+++ src/Util/Area.h
@@ -4,8 +4,13 @@
 
 namespace al {
 class LiveActor;
+class AreaObj;
+class IUseAreaObj;
 }  // namespace al
 
 namespace rs {
-bool isInChangeStageArea(const al::LiveActor*, const sead::Vector3f*);
+bool isInChangeStageArea(const al::LiveActor* actor, const sead::Vector3f* pos);
+bool tryFindForceRecoveryArea(sead::Vector3f* targetPos, sead::Vector3f* targetUp,
+                              const al::AreaObj** area, const al::IUseAreaObj* obj,
+                              const sead::Vector3f& pos);
 }  // namespace rs
