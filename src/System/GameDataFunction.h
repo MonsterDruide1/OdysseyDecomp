2acbd54278c6bce7f3c6d9496eea876f3e5e7e49
MonsterDruide1 <5958456@gmail.com>
2024-11-11 22:25:30 +0100

    System: Adjust return type of `tryChangeNextStage*`

--- src/System/GameDataFunction.h
+++ src/System/GameDataFunction.h
@@ -119,12 +119,12 @@ public:
     static void getPlayTimeAcrossFile(GameDataHolderAccessor);
     static void getSaveDataIdForPrepo(GameDataHolderAccessor);
     static void startDemoStage(GameDataHolderWriter, const char*);
-    static void tryChangeNextStage(GameDataHolderWriter, const ChangeStageInfo*);
-    static void tryChangeNextStageWithStartRaceFlag(GameDataHolderWriter, const ChangeStageInfo*);
-    static void tryChangeNextStageWithStartRaceYukimaru(GameDataHolderWriter,
+    static bool tryChangeNextStage(GameDataHolderWriter, const ChangeStageInfo*);
+    static bool tryChangeNextStageWithStartRaceFlag(GameDataHolderWriter, const ChangeStageInfo*);
+    static bool tryChangeNextStageWithStartRaceYukimaru(GameDataHolderWriter,
                                                         const ChangeStageInfo*);
-    static void tryChangeNextStageWithDemoWorldWarp(GameDataHolderWriter, const char*);
-    static void tryChangeNextStageWithWorldWarpHole(GameDataHolderWriter, const char*);
+    static bool tryChangeNextStageWithDemoWorldWarp(GameDataHolderWriter, const char*);
+    static bool tryChangeNextStageWithWorldWarpHole(GameDataHolderWriter, const char*);
     static void changeNextStageWithStartTimeBalloon(GameDataHolderWriter, s32);
     static void changeNextStageWithEndTimeBalloon(GameDataHolderWriter);
     static void changeNextStageWithCloset(GameDataHolderWriter);
