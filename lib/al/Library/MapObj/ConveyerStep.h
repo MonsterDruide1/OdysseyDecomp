3ce71f1da97f0e7dc86b5c29b7a8b277c4333353
Fuzzy2319 <61144247+Fuzzy2319@users.noreply.github.com>
2024-12-23 16:25:14 +0100

    Library/MapObj: Implement `ConveyerStep` (#208)

---
+++ lib/al/Library/MapObj/ConveyerStep.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "Library/LiveActor/LiveActor.h"
+
+namespace al {
+class ConveyerKeyKeeper;
+
+class ConveyerStep : public LiveActor {
+public:
+    ConveyerStep(const char* name);
+
+    void init(const ActorInitInfo& info) override;
+    bool receiveMsg(const SensorMsg* message, HitSensor* other, HitSensor* self) override;
+
+    void setHost(LiveActor* host);
+    void setConveyerKeyKeeper(const ConveyerKeyKeeper* conveyerKeyKeeper, f32 coord);
+    void setTransByCoord(f32 coord, bool isForwards);
+    void setTransByCoord(f32 coord, bool isForwards, bool isForceReset);
+    void setTransAndResetByCoord(f32 coord);
+
+    void exeWait();
+
+private:
+    LiveActor* mHost = nullptr;
+    const ConveyerKeyKeeper* mConveyerKeyKeeper = nullptr;
+    const char* mKeyHitReactionName = nullptr;
+    const char* mActionName = nullptr;
+    f32 mCurrentCoord = 0.0f;
+    f32 mMaxCoord = 0.0f;
+    bool mIsExist = true;
+};
+}  // namespace al
---
+++ lib/al/Project/LiveActor/ConveyerKeyKeeper.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <math/seadQuat.h>
+#include <math/seadVector.h>
+
+namespace al {
+class ActorInitInfo;
+class PlacementInfo;
+
+// TODO: I'm not sure about this
+struct ConveyerKey {
+    void* _0[5];
+    PlacementInfo& mPlacementInfo;
+};
+
+class ConveyerKeyKeeper {
+public:
+    ConveyerKeyKeeper();
+
+    void init(const ActorInitInfo& info);
+    void calcPosAndQuat(sead::Vector3f* pos, sead::Quatf* quat, s32* index, f32 coord) const;
+    void calcPosAndQuatByKeyIndex(sead::Vector3f* pos, sead::Quatf* quat, s32 index) const;
+    void calcClippingSphere(sead::Vector3f* clippingTrans, f32* clippingRadius, f32 offset) const;
+    const ConveyerKey* getConveyerKey(s32 index) const;  // return type depends on the type of _0
+
+    f32 get_34() const { return _34; }
+
+private:
+    ConveyerKey** mConveyerKeys;  // array of a struct/class with a size of 0x30?
+    s32 mConveyerKeyCount;
+    sead::Quatf mQuat;
+    sead::Vector3f mTrans;
+    sead::Vector3f _28;
+    f32 _34;
+};
+}  // namespace al
