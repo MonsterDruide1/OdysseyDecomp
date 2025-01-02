d57df6ef9ecf0f33e24e06de9acfed8f33d98d90
Fuzzy2319 <alexandre.verbrigghe@orange.fr>
2025-01-02 20:58:21 +0100

    fix:missing const qualifiers in RailUtil

--- lib/al/Library/Rail/RailUtil.h
+++ lib/al/Library/Rail/RailUtil.h
@@ -12,9 +12,11 @@ void setRailPosToStart(IUseRail* railHolder);
 void moveSyncRail(LiveActor* actor, f32 speed);
 void moveSyncRailLoop(LiveActor* actor, f32 speed);
 void moveSyncRailTurn(LiveActor* actor, f32 speed);
-void calcRailUp(sead::Vector3f* out, IUseRail* railHolder);
-const sead::Vector3f& getRailDir(IUseRail* railHolder);
-f32 getRailCoord(IUseRail* railHolder);
-bool isExistRail(IUseRail* railHolder);
+void calcRailUp(sead::Vector3f* out, const IUseRail* railHolder);
+const sead::Vector3f& getRailDir(const IUseRail* railHolder);
+f32 getRailCoord(const IUseRail* railHolder);
+bool isExistRail(const IUseRail* railHolder);
 void setRailClippingInfo(sead::Vector3f*, LiveActor* actor, f32, f32);
+bool isRailReachedGoal(const IUseRail* railHolder);
+bool isRailReachedEnd(const IUseRail* railHolder);
 }  // namespace al
