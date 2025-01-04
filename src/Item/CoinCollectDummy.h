c3725f62d38be43de3268edc196f18cc8762cc78
Narr the Reg <5944268+german77@users.noreply.github.com>
2025-01-04 09:44:49 -0600

    Item: Implement `CoinCollectDummy` (#221)

---
+++ src/Item/CoinCollectDummy.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <math/seadVector.h>
+
+#include "Library/LiveActor/LiveActor.h"
+
+class CoinCollectHintState;
+
+class CoinCollectDummy : public al::LiveActor {
+public:
+    CoinCollectDummy(const char* name);
+
+    void init(const al::ActorInitInfo& initInfo) override;
+    void appear() override;
+
+    void appearHint(const sead::Vector3f& position);
+    void reappearHint();
+    void deleteHint();
+    void exeHint();
+
+private:
+    CoinCollectHintState* mHintState = nullptr;
+};
