5b01e02ca531b363d0a05c7bf700c6498e3d26e2
Muz-dev <113846204+Muz-dev@users.noreply.github.com>
2025-01-04 21:45:44 +0000

    HintPhotoLayoutHolder Implemented

---
+++ src/Layout/DecideIconLayout.cpp
@@ -0,0 +1,1 @@
+#include "Layout/DecideIconLayout.h"
---
+++ src/Scene/HintPhotoLayoutHolder.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "Library/Layout/LayoutActor.h"
+#include "Library/Layout/LayoutInitInfo.h"
+#include "Library/Scene/ISceneObj.h"
+
+class DecideIconLayout;
+
+class HintPhotoLayoutHolder : public al::ISceneObj {
+public:
+    HintPhotoLayoutHolder();
+    void init(const al::LayoutInitInfo& info);
+    bool isInit() const;
+    const al::LayoutActor* getPhotoLayout() const;
+    DecideIconLayout* getDecideIcon() const;
+private:
+    al::LayoutActor* mLayoutActor;
+    DecideIconLayout* mDecideIconLayout;
+};
