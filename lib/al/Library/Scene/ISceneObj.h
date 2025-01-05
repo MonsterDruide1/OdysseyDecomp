dde36af891d77d374cc651c04b2b79763a086d69
german77 <5944268+german77@users.noreply.github.com>
2025-01-05 15:30:12 -0600

    revert destructor changes

--- lib/al/Library/Scene/ISceneObj.h
+++ lib/al/Library/Scene/ISceneObj.h
@@ -5,6 +5,7 @@ class ActorInitInfo;
 
 class ISceneObj {
 public:
+    virtual ~ISceneObj();
     virtual const char* getSceneObjName() const = 0;
     virtual void initAfterPlacementSceneObj(const ActorInitInfo&) = 0;
     virtual void initSceneObj() = 0;
