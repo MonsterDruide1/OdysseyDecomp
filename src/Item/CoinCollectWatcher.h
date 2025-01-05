f628c9521adf898d9aab6fbaaeb5efa20eb16d30
german77 <5944268+german77@users.noreply.github.com>
2025-01-05 09:34:41 -0600

    item: Implement CoinCollectWatcher

---
+++ src/Item/CoinCollectWatcher.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <basis/seadTypes.h>
+
+#include "Library/Scene/ISceneObj.h"
+
+namespace al {
+class ActorInitInfo;
+class IUseSceneObjHolder;
+class LiveActor;
+}  // namespace al
+
+class CoinCollectLayout;
+
+class CoinCollectWatcher : public al::ISceneObj {
+public:
+    CoinCollectWatcher();
+    virtual ~CoinCollectWatcher();
+
+    const char* getSceneObjName() const override;
+    void initAfterPlacementSceneObj(const al::ActorInitInfo& initInfo) override;
+
+    void registerCoin(bool isCountUpCoin);
+    void countup(const al::LiveActor* actor);
+
+private:
+    u32 mCoinUpCount = 0;
+    u32 mRegisteredCoins = 0;
+    CoinCollectLayout* mCoinCollectLayout = nullptr;
+};
+
+namespace rs {
+CoinCollectWatcher* createCoinCollectWatcher(const al::IUseSceneObjHolder* objHolder);
+}  // namespace rs
