4b159c6baa584f5ba2fdcc23c9565bff17bb5105
GRAnimated <devin@nhscan.com>
2025-01-10 20:46:14 -0500

    Implement `PlayerPowerGlove` and `PlayerSword`

--- src/Util/Sensor.h
+++ src/Util/Sensor.h
@@ -15,6 +15,8 @@ bool sendMsgEnemyAttack2D(al::HitSensor* source, al::HitSensor* target);
 bool sendMsgNoticePlayerDamage(al::HitSensor* source, al::HitSensor* target);
 bool sendMsgTouchFireDrum2D(al::HitSensor* source, al::HitSensor* target);
 bool sendMsgItemAmiiboKoopa(al::HitSensor* source, al::HitSensor* target);
+bool sendMsgPlayerSwordAttack(al::HitSensor* source, al::HitSensor* target);
+bool sendMsgCapAttack(al::HitSensor* source, al::HitSensor* target);
 
 bool isMsgCapTouchWall(const al::SensorMsg*);
 bool tryReceiveMsgInitCapTargetAndSetCapTargetInfo(const al::SensorMsg*, const CapTargetInfo*);
