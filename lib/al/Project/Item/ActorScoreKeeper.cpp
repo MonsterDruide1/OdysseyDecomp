4bb63437dd596035366e115cdb4c41e85866b018
LynxDev2 <128722393+LynxDev2@users.noreply.github.com>
2025-01-12 13:39:01 +0700

    Add ctor and dtor mismatch detection and more options

--- lib/al/Project/Item/ActorScoreKeeper.cpp
+++ lib/al/Project/Item/ActorScoreKeeper.cpp
@@ -5,7 +5,7 @@
 namespace al {
 ActorScoreKeeper::ActorScoreKeeper() = default;
 
-//NON_MATCHING
+// NON_MATCHING
 void ActorScoreKeeper::init(const ByamlIter& iter) {
     if (iter.isTypeArray()) {
         mSize = iter.getSize();
