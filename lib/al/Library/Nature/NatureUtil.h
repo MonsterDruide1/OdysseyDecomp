db17ae39daaf0915e922319b0c3b5122b3caed70
german77 <5944268+german77@users.noreply.github.com>
2025-01-14 22:36:48 -0600

    make calcFindWaterSurface return bool

--- lib/al/Library/Nature/NatureUtil.h
+++ lib/al/Library/Nature/NatureUtil.h
@@ -10,7 +10,7 @@ bool isInWaterPos(const LiveActor* actor, const sead::Vector3f& pos);
 bool isInWater(const LiveActor* actor);
 bool isInIceWaterPos(const LiveActor* actor, const sead::Vector3f& pos);
 
-void calcFindWaterSurface(sead::Vector3f*, sead::Vector3f*, const LiveActor*, const sead::Vector3f&,
+bool calcFindWaterSurface(sead::Vector3f*, sead::Vector3f*, const LiveActor*, const sead::Vector3f&,
                           const sead::Vector3f&, f32);
 
 }  // namespace al
