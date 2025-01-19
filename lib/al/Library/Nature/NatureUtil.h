4aa3f346deb673200043d890a0ecf752f3620177
Narr the Reg <5944268+german77@users.noreply.github.com>
2025-01-19 04:58:18 -0600

    Item: Implement `CoinCollectEmpty` (#266)

--- lib/al/Library/Nature/NatureUtil.h
+++ lib/al/Library/Nature/NatureUtil.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <basis/seadTypes.h>
 #include <math/seadVector.h>
 
 namespace al {
@@ -8,5 +9,8 @@ class LiveActor;
 bool isInWaterPos(const LiveActor* actor, const sead::Vector3f& pos);
 bool isInWater(const LiveActor* actor);
 bool isInIceWaterPos(const LiveActor* actor, const sead::Vector3f& pos);
+
+bool calcFindWaterSurface(sead::Vector3f*, sead::Vector3f*, const LiveActor*, const sead::Vector3f&,
+                          const sead::Vector3f&, f32);
 
 }  // namespace al
