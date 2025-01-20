c8cc225027768b6e0752e11cf13fd5ed1509e908
german77 <5944268+german77@users.noreply.github.com>
2025-01-20 17:13:30 -0600

    address Comments

--- src/Item/Coin.h
+++ src/Item/Coin.h
@@ -33,7 +33,7 @@ public:
     void appearCoinRail();
     void appearCoinChameleon(const sead::Vector3f&, const sead::Vector3f&, const sead::Vector3f&);
     void appearLimitTime(s32);
-    void appearBlow(const sead::Vector3f& velocity, s32 delayTime);
+    void appearBlow(const sead::Vector3f& direction, s32 delayTime);
     void rotate();
     void appearBlowUp(s32, s32);
     void appearBlowUpCommon(s32, f32, f32, s32, s32);
@@ -62,13 +62,13 @@ private:
 
 namespace CoinFunction {
 
-const sead::Vector3f* getAppearAboveVelocity();
-void appearCoinBlowVeryLittle(Coin* coin, const sead::Vector3f& normalVelocity);
-void appearCoinBlowLittle(Coin* coin, const sead::Vector3f& normalVelocity);
-void appearCoinBlowMiddle(Coin* coin, const sead::Vector3f& normalVelocity);
-void appearCoinBlowLarge(Coin* coin, const sead::Vector3f& normalVelocity);
-void appearCoinBlowLargeEnemy(Coin* coin, const sead::Vector3f& normalVelocity);
-void appearCoinBlowBoss(Coin* coin, const sead::Vector3f& normalVelocity);
-void appearCoinBlowGk(Coin* coin, const sead::Vector3f& normalVelocity);
+const sead::Vector3f& getAppearAboveVelocity();
+void appearCoinBlowVeryLittle(Coin* coin, const sead::Vector3f& direction);
+void appearCoinBlowLittle(Coin* coin, const sead::Vector3f& direction);
+void appearCoinBlowMiddle(Coin* coin, const sead::Vector3f& direction);
+void appearCoinBlowLarge(Coin* coin, const sead::Vector3f& direction);
+void appearCoinBlowLargeEnemy(Coin* coin, const sead::Vector3f& direction);
+void appearCoinBlowBoss(Coin* coin, const sead::Vector3f& direction);
+void appearCoinBlowGk(Coin* coin, const sead::Vector3f& direction);
 
 }  // namespace CoinFunction
