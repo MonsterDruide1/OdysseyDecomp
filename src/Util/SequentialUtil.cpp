700c411c81e0f8459d57c6ba7365b0fce552df68
Naii-the-Baf <sfabian200@gmail.com>
2025-01-22 17:33:27 -0600

    Util: Implement SequentialUtil

---
+++ src/Util/SequentialUtil.cpp
@@ -0,0 +1,21 @@
+#include "Util/SequentialUtil.h"
+
+#include "Library/Math/MathUtil.h"
+
+void rs::convertCsecTo(s32* minutes, s32* seconds, s32* csec, s32 time) {
+    *minutes = time / 6000;
+    *seconds = (time - (time / 6000) * 6000) / 100;
+    *csec = time - (*minutes * 6000) - (*seconds * 100);
+}
+
+s32 RaceTimeFunction::getRaceTimeMaxFrame() {
+    return 215999;
+}
+
+s32 RaceTimeFunction::getRaceTimeMaxCsec() {
+    return 359999;
+}
+
+s32 RaceTimeFunction::clampRaceRecordCsec(s32 csec) {
+    return sead::Mathi::clamp(csec, 0, 359999);
+}
---
+++ src/Util/SequentialUtil.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <basis/seadTypes.h>
+#include <math/seadVector.h>
+
+namespace rs {
+void convertCsecTo(s32* minutes, s32* seconds, s32* csec, s32 time);
+}  // namespace rs
+
+namespace RaceTimeFunction {
+s32 getRaceTimeMaxFrame();
+s32 getRaceTimeMaxCsec();
+s32 clampRaceRecordCsec(s32 csec);
+}  // namespace RaceTimeFunction
