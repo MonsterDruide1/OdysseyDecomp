870709f6c98ab194527773e2878ebffb2f6cde76
Fuzzy2319 <alexandre.verbrigghe@orange.fr>
2025-01-24 11:52:08 +0100

    add:VisibleSwitchMapParts partial implementation

---
+++ lib/al/Library/MapObj/VisibleSwitchMapParts.cpp
@@ -0,0 +1,162 @@
+#include "Library/MapObj/VisibleSwitchMapParts.h"
+
+#include "Library/Collision/PartsConnector.h"
+#include "Library/LiveActor/ActorActionFunction.h"
+#include "Library/LiveActor/ActorInitFunction.h"
+#include "Library/LiveActor/ActorModelFunction.h"
+#include "Library/LiveActor/ActorPoseKeeper.h"
+#include "Library/LiveActor/ActorSensorMsgFunction.h"
+#include "Library/Nerve/NerveSetupUtil.h"
+#include "Library/Placement/PlacementFunction.h"
+#include "Library/Stage/StageSwitchKeeper.h"
+#include "Library/Stage/StageSwitchUtil.h"
+#include "Library/Thread/FunctorV0M.h"
+
+namespace {
+using namespace al;
+
+NERVE_IMPL(VisibleSwitchMapParts, Show)
+NERVE_IMPL(VisibleSwitchMapParts, Hide)
+NERVE_IMPL(VisibleSwitchMapParts, Disappear)
+NERVE_IMPL(VisibleSwitchMapParts, DisappearDither)
+NERVE_IMPL(VisibleSwitchMapParts, Appear)
+NERVE_IMPL(VisibleSwitchMapParts, AppearDither)
+
+NERVES_MAKE_STRUCT(VisibleSwitchMapParts, Show, Hide, Disappear, DisappearDither, Appear,
+                   AppearDither)
+}  // namespace
+
+namespace al {
+VisibleSwitchMapParts::VisibleSwitchMapParts(const char* name) : LiveActor(name) {}
+
+void VisibleSwitchMapParts::init(const ActorInitInfo& info) {
+    using VisibleSwitchMapPartsFunctor =
+        FunctorV0M<al::VisibleSwitchMapParts*, void (al::VisibleSwitchMapParts::*)()>;
+
+    initMapPartsActor(this, info, nullptr);
+    initNerve(this, &NrvVisibleSwitchMapParts.Show, 0);
+
+    makeActorAlive();
+
+    if (isValidStageSwitch(this, "SwitchAppear") && isValidStageSwitch(this, "SwitchDisappear"))
+        return;
+
+    if (isValidStageSwitch(this, "SwitchDisappear")) {
+        listenStageSwitchOnOff(
+            this, "SwitchDisappear",
+            VisibleSwitchMapPartsFunctor(this, &VisibleSwitchMapParts::startDisappear),
+            VisibleSwitchMapPartsFunctor(this, &VisibleSwitchMapParts::startAppear));
+    } else if (isValidStageSwitch(this, "SwitchAppear")) {
+        listenStageSwitchOnOff(
+            this, "SwitchAppear",
+            VisibleSwitchMapPartsFunctor(this, &VisibleSwitchMapParts::startAppear),
+            VisibleSwitchMapPartsFunctor(this, &VisibleSwitchMapParts::startDisappear));
+        setNerve(this, &NrvVisibleSwitchMapParts.Hide);
+    } else {
+        return;
+    }
+
+    listenStageSwitchOn(
+        this, "SwitchSuddenDisappear",
+        VisibleSwitchMapPartsFunctor(this, &VisibleSwitchMapParts::startSuddenDisappear));
+
+    tryGetArg(&mConnectMapCheckLength, info, "ConnectMapCheckLength");
+    tryGetArg((s32*)&mConnectMapDirType, info, "ConnectMapDirType");
+    tryGetArg(&mDitherFrame, info, "DitherFrame");
+    tryGetArg(&mIsUsingAlphaMask, info, "UseAlphaMask");
+
+    if (mConnectMapDirType != ConnectMapDirType::Invalid)
+        mMtxConnector = createMtxConnector(this);
+}
+
+void VisibleSwitchMapParts::startDisappear() {
+    if (isNerve(this, &NrvVisibleSwitchMapParts.Disappear) ||
+        isNerve(this, &NrvVisibleSwitchMapParts.DisappearDither) ||
+        isNerve(this, &NrvVisibleSwitchMapParts.Hide))
+        return;
+
+    if (mIsUsingAlphaMask)
+        setNerve(this, &NrvVisibleSwitchMapParts.DisappearDither);
+    else
+        setNerve(this, &NrvVisibleSwitchMapParts.Disappear);
+}
+
+void VisibleSwitchMapParts::startAppear() {
+    if (isNerve(this, &NrvVisibleSwitchMapParts.Appear) ||
+        isNerve(this, &NrvVisibleSwitchMapParts.AppearDither))
+        return;
+
+    if (mIsUsingAlphaMask)
+        setNerve(this, &NrvVisibleSwitchMapParts.AppearDither);
+    else
+        setNerve(this, &NrvVisibleSwitchMapParts.Appear);
+}
+
+void VisibleSwitchMapParts::startSuddenDisappear() {
+    if (isNerve(this, &NrvVisibleSwitchMapParts.Hide))
+        return;
+
+    if (mIsUsingAlphaMask)
+        setModelAlphaMask(this, 0.0f);
+
+    setNerve(this, &NrvVisibleSwitchMapParts.Hide);
+}
+
+void VisibleSwitchMapParts::initAfterPlacement() {
+    if (mMtxConnector == nullptr)
+        return;
+
+    sead::Vector3f dir;
+    switch (mConnectMapDirType) {
+    case ConnectMapDirType::Side:
+        calcSideDir(&dir, this);
+
+        break;
+    case ConnectMapDirType::Up:
+        calcUpDir(&dir, this);
+
+        break;
+    case ConnectMapDirType::Front:
+        calcFrontDir(&dir, this);
+
+        break;
+    default:
+        break;
+    }
+
+    switch (mConnectMapDirType) {
+    case ConnectMapDirType::NegSide:
+    case ConnectMapDirType::NegUp:
+    case ConnectMapDirType::NegFront:
+        dir = -dir;
+
+        break;
+    default:
+        break;
+    }
+
+    attachMtxConnectorToCollision(mMtxConnector, this, getTrans(this),
+                                  mConnectMapCheckLength * dir);
+}
+
+void VisibleSwitchMapParts::control() {
+    if (mMtxConnector == nullptr)
+        return;
+
+    connectPoseQT(this, mMtxConnector);
+}
+
+bool VisibleSwitchMapParts::receiveMsg(const SensorMsg* message, HitSensor* other,
+                                       HitSensor* self) {
+    if (isMsgScreenPointInvalidCollisionParts(message) &&
+        isNerve(this, &NrvVisibleSwitchMapParts.Hide))
+        return true;
+
+    return false;
+}
+
+void VisibleSwitchMapParts::exeShow() {
+    if (isFirstStep(this))
+        tryStartAction(this, "Wait");
+}
+}  // namespace al
---
+++ lib/al/Library/MapObj/VisibleSwitchMapParts.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "Library/LiveActor/LiveActor.h"
+
+namespace al {
+class MtxConnector;
+
+class VisibleSwitchMapParts : public LiveActor {
+public:
+    VisibleSwitchMapParts(const char* name);
+
+    void init(const ActorInitInfo& info) override;
+    void initAfterPlacement() override;
+    void control() override;
+    bool receiveMsg(const SensorMsg* message, HitSensor* other, HitSensor* self) override;
+
+    void startDisappear();
+    void startAppear();
+    void startSuddenDisappear();
+
+    void exeShow();
+    void exeDisappear();
+    void exeDisappearDither();
+    void exeHide();
+    void exeAppear();
+    void exeAppearDither();
+
+    enum class ConnectMapDirType : s32 {
+        Invalid = -1,
+        Side = 0,
+        NegSide = 1,
+        Up = 2,
+        NegUp = 3,
+        Front = 4,
+        NegFront = 5
+    };
+
+private:
+    ConnectMapDirType mConnectMapDirType = ConnectMapDirType::Invalid;
+    MtxConnector* mMtxConnector = nullptr;
+    f32 mConnectMapCheckLength = 150.0f;
+    bool mIsUsingAlphaMask = true;
+    s32 mDitherFrame = 30;
+};
+
+static_assert(sizeof(VisibleSwitchMapParts) == 0x128);
+}  // namespace al
