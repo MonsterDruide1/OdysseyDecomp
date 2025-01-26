f37d9fc681917bda627877f2affc9f158400ff54
Naii-the-Baf <74033491+Naii-the-Baf@users.noreply.github.com>
2025-01-26 08:07:03 -0600

    System: Implement `RaceRecord` (#300)

---
+++ src/System/RaceRecord.cpp
@@ -0,0 +1,70 @@
+#include "System/RaceRecord.h"
+
+#include "Library/Base/StringUtil.h"
+#include "Library/Yaml/ByamlIter.h"
+#include "Library/Yaml/Writer/ByamlWriter.h"
+
+#include "Util/SequentialUtil.h"
+
+RaceRecord::RaceRecord() {
+    mName.clear();
+}
+
+void RaceRecord::setName(const char* name) {
+    mName.format(name);
+}
+
+const char* RaceRecord::getName() const {
+    return mName.cstr();
+}
+
+void RaceRecord::init() {
+    mRecord = RaceTimeFunction::getRaceTimeMaxCsec();
+    mBestRecord = RaceTimeFunction::getRaceTimeMaxCsec();
+    mLapRecord = RaceTimeFunction::getRaceTimeMaxCsec();
+    mIsWin = false;
+    mIsExistRecord = false;
+    mIsRecordSet = false;
+}
+
+bool RaceRecord::isEqualName(const char* name) {
+    return al::isEqualString(mName.cstr(), name);
+}
+
+void RaceRecord::setRecord(s32 record, s32 bestRecord, s32 lapRecord, bool isWin) {
+    mIsExistRecord = true;
+    mIsRecordSet = true;
+    if (record < mRecord)
+        mRecord = record;
+    if (bestRecord < mBestRecord)
+        mBestRecord = bestRecord;
+    if (lapRecord < mLapRecord)
+        mLapRecord = lapRecord;
+    mIsWin = isWin;
+}
+
+void RaceRecord::write(al::ByamlWriter* writer) {
+    writer->pushHash(mName.cstr());
+    writer->addInt("Record", mRecord);
+    writer->addInt("LapRecord", mLapRecord);
+    writer->addBool("IsWin", mIsWin);
+    writer->addBool("IsExistRecord", mIsExistRecord);
+    writer->pop();
+}
+
+void RaceRecord::read(const al::ByamlIter& reader) {
+    mRecord = RaceTimeFunction::getRaceTimeMaxCsec();
+    mBestRecord = RaceTimeFunction::getRaceTimeMaxCsec();
+    mLapRecord = RaceTimeFunction::getRaceTimeMaxCsec();
+    mIsWin = false;
+    mIsExistRecord = false;
+    mIsRecordSet = false;
+
+    al::ByamlIter recordData{};
+
+    reader.tryGetIterByKey(&recordData, mName.cstr());
+    recordData.tryGetIntByKey(&mRecord, "Record");
+    recordData.tryGetIntByKey(&mLapRecord, "LapRecord");
+    recordData.tryGetBoolByKey(&mIsWin, "IsWin");
+    recordData.tryGetBoolByKey(&mIsExistRecord, "IsExistRecord");
+}
