12b8f4c998080a5b215295c2f30d8d280f3063cd
Narr the Reg <5944268+german77@users.noreply.github.com>
2025-01-27 04:45:26 -0600

    Item: Implement `CoinCollect2D` (#289)

---
+++ src/MapObj/CapMessageShowInfo.h
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace al {
+class IUseSceneObjHolder;
+}  // namespace al
+
+namespace rs {
+bool tryShowCapMsgCollectCoinGetFirst(const al::IUseSceneObjHolder*);
+}  // namespace rs
