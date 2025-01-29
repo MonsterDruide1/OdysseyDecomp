4dab1183d398ff1222bd3e5d89f49f8d30f4c849
Naii-the-Baf <74033491+Naii-the-Baf@users.noreply.github.com>
2025-01-29 03:45:51 -0600

    Util: Implement `SequentialUtil` (#301)

--- src/Util/SequentialUtil.h
+++ src/Util/SequentialUtil.h
@@ -1,14 +1,13 @@
 #pragma once
 
 #include <basis/seadTypes.h>
-#include <math/seadVector.h>
 
 namespace rs {
-void convertCsecTo(s32*, s32*, s32*, s32);
-}
+void convertCsecTo(s32* minutes, s32* seconds, s32* csec, s32 time);
+}  // namespace rs
 
 namespace RaceTimeFunction {
 s32 getRaceTimeMaxFrame();
 s32 getRaceTimeMaxCsec();
-s32 clampRaceRecordCsec(s32);
+s32 clampRaceRecordCsec(s32 csec);
 }  // namespace RaceTimeFunction
