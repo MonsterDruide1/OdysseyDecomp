18d44d5479a29559a70cb694c7320ef31fc37fec
Fuzzy2319 <alexandre.verbrigghe@orange.fr>
2025-01-29 15:40:40 +0100

    add:WheelMapParts partial implementation
    # Conflicts:
    #	data/odyssey_functions.csv

---
+++ lib/al/Library/MapObj/WheelMapParts.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "Library/LiveActor/LiveActor.h"
+
+namespace al {
+class WheelMovement;
+class SwitchKeepOnAreaGroup;
+class SwitchOnAreaGroup;
+
+class WheelMapParts : public LiveActor {
+public:
+    WheelMapParts(const char* name);
+
+    void init(const ActorInitInfo& info) override;
+    void control() override;
+    bool receiveMsg(const SensorMsg* message, HitSensor* other, HitSensor* self) override;
+
+    void appearAndSetStart();
+
+    void exeWait();
+    void exeMove();
+    void exeAssistStop();
+
+private:
+    sead::Matrix34f mSurfaceEffectMtx = sead::Matrix34f::ident;
+    WheelMovement* mWheelMovement = nullptr;
+    SwitchKeepOnAreaGroup* mSwitchKeepOnAreaGroup = nullptr;
+    SwitchOnAreaGroup* mSwitchOnAreaGroup = nullptr;
+    s32 _150 = 0;
+};
+
+static_assert(sizeof(WheelMapParts) == 0x158);
+}  // namespace al
