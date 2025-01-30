beddd2664983059f18334b593a08cfe5d4413144
Naii-the-Baf <sfabian200@gmail.com>
2025-01-30 14:18:13 -0600

    System: Implement `SphinxQuizData`

---
+++ src/System/SphinxQuizData.cpp
@@ -0,0 +1,57 @@
+#include "System/SphinxQuizData.h"
+
+#include "Library/Yaml/ByamlIter.h"
+#include "Library/Yaml/Writer/ByamlWriter.h"
+
+SphinxQuizData::SphinxQuizData(s32 arraySize) : mArraySize(arraySize) {
+    mIsAnswerCorrectSphinxQuiz = new bool[mArraySize];
+    mIsAnswerCorrectSphinxQuizAll = new bool[mArraySize];
+
+    init();
+}
+
+void SphinxQuizData::init() {
+    if (0 < mArraySize) {
+        for (s64 i = 0; i < mArraySize; i++) {
+            mIsAnswerCorrectSphinxQuiz[i] = false;
+            mIsAnswerCorrectSphinxQuizAll[i] = false;
+        }
+    }
+}
+
+void SphinxQuizData::answerCorrectSphinxQuiz(s32 index) {
+    mIsAnswerCorrectSphinxQuiz[index] = true;
+}
+
+void SphinxQuizData::answerCorrectSphinxQuizAll(s32 index) {
+    mIsAnswerCorrectSphinxQuizAll[index] = true;
+}
+
+bool SphinxQuizData::isAnswerCorrectSphinxQuiz(s32 index) const {
+    return mIsAnswerCorrectSphinxQuiz[index];
+}
+
+bool SphinxQuizData::isAnswerCorrectSphinxQuizAll(s32 index) const {
+    return mIsAnswerCorrectSphinxQuizAll[index];
+}
+
+void SphinxQuizData::write(al::ByamlWriter* writer) {
+    for (s64 i = 0; i < mArraySize; i++) {
+        writer->pushHash();
+        writer->addBool("IsAnswerCorrectSphinxQuiz", mIsAnswerCorrectSphinxQuiz[i]);
+        writer->addBool("IsAnswerCorrectSphinxQuizAll", mIsAnswerCorrectSphinxQuizAll[i]);
+        writer->pop();
+    }
+}
+
+void SphinxQuizData::read(const al::ByamlIter& reader) {
+    init();
+
+    for (s64 i = 0; i < mArraySize; i++) {
+        al::ByamlIter sphinxData{};
+        reader.tryGetIterByIndex(&sphinxData, i);
+        sphinxData.tryGetBoolByKey(&mIsAnswerCorrectSphinxQuiz[i], "IsAnswerCorrectSphinxQuiz");
+        sphinxData.tryGetBoolByKey(&mIsAnswerCorrectSphinxQuizAll[i],
+                                   "IsAnswerCorrectSphinxQuizAll");
+    }
+}
