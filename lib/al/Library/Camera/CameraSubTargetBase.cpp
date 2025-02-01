f49f1c3846a4a0f56b53f88e9caa3c953ecb3ea3
Muz-dev <113846204+Muz-dev@users.noreply.github.com>
2025-02-01 22:09:06 +0000

    Implemented al::CameraStopJudge. No other implementations required

--- lib/al/Library/Camera/CameraSubTargetBase.cpp
+++ lib/al/Library/Camera/CameraSubTargetBase.cpp
@@ -1,3 +1,26 @@
-//
-// Created by larbe on 1/24/25.
-//
+#include "Library/Camera/CameraSubTargetBase.h"
+#include "Library/Area/IUseAreaObj.h"
+#include "Library/Area/AreaObjUtil.h"
+#include "Library/Scene/Scene.h"
+
+al::CameraStopJudge::CameraStopJudge() {
+    mIsInObjArea = false;
+    mIsNotInObjArea = false;
+    mStopJudge = false;
+    mAreaObjDirector = nullptr;
+}
+
+bool al::CameraStopJudge::isStop() const {
+    if (mStopJudge)
+        return false;
+    return mIsInObjArea || mIsNotInObjArea;
+}
+
+void al::CameraStopJudge::update(sead::Vector3f const& position) {
+    mIsInObjArea = al::isInAreaObj(this, "CameraStopArea", position);
+    return;
+}
+
+al::AreaObjDirector* al::CameraStopJudge::getAreaObjDirector() const {
+    return mAreaObjDirector;
+}
