3f5398eeeef804c5836a0e77a613e4bc924ed32b
Naii-the-Baf <74033491+Naii-the-Baf@users.noreply.github.com>
2025-02-02 04:31:35 -0600

    System: Implement `SphinxQuizData` (#324)

---
+++ src/System/SphinxQuizData.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <basis/seadTypes.h>
+
+#include "System/ByamlSave.h"
+
+namespace al {
+class ByamlIter;
+class ByamlWriter;
+}  // namespace al
+
+class SphinxQuizData : public ByamlSave {
+public:
+    SphinxQuizData(s32 arraySize);
+    void init();
+    void answerCorrectSphinxQuiz(s32 index);
+    void answerCorrectSphinxQuizAll(s32 index);
+    bool isAnswerCorrectSphinxQuiz(s32 index) const;
+    bool isAnswerCorrectSphinxQuizAll(s32 index) const;
+    void write(al::ByamlWriter* writer) override;
+    void read(const al::ByamlIter& reader) override;
+
+private:
+    bool* mIsAnswerCorrectSphinxQuiz;
+    bool* mIsAnswerCorrectSphinxQuizAll;
+    s32 mArraySize;
+};
