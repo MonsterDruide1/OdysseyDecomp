6b312f78b9651b68687827c9c11b465328146373
MonsterDruide1 <5958456@gmail.com>
2025-02-02 02:52:14 +0100

    Player: Implement `PlayerStateSlope`

--- src/Player/PlayerActionSlopeSlideControl.h
+++ src/Player/PlayerActionSlopeSlideControl.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <basis/seadTypes.h>
+#include <math/seadVector.h>
 
 namespace al {
 class LiveActor;
@@ -17,8 +18,20 @@ public:
     void setupCutSlideOppositeDir();
     f32 update(f32, f32, f32, f32, f32, f32, f32, f32, f32, f32, bool);
 
+    const sead::Vector3f& getDirSlide() const { return mDirSlide; }
+
+    const sead::Vector3f& getGroundNormal() const { return mGroundNormal; }
+
+    const sead::Vector3f& getHorizontalVelocity() const { return mHorizontalVelocity; }
+
 private:
-    void* filler[0x48 / 8];
+    al::LiveActor* mPlayer;
+    const PlayerConst* mConst;
+    const PlayerInput* mInput;
+    const IUsePlayerCollision* mCollision;
+    sead::Vector3f mDirSlide = {0.0f, 0.0f, 0.0f};
+    sead::Vector3f mGroundNormal = {0.0f, 0.0f, 0.0f};
+    sead::Vector3f mHorizontalVelocity = {0.0f, 0.0f, 0.0f};
 };
 
 static_assert(sizeof(PlayerActionSlopeSlideControl) == 0x48);
