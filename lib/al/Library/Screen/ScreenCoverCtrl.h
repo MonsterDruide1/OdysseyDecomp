f898404618fc5dac9c91a1305c492f607ac9dd93
LynxDev2 <128722393+LynxDev2@users.noreply.github.com>
2025-02-06 14:21:26 +0700

    Implement ActorSceneFunction

---
+++ lib/al/Library/Screen/ScreenCoverCtrl.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <basis/seadTypes.h>
+
+namespace al {
+
+class ScreenCoverCtrl {
+public:
+    ScreenCoverCtrl();
+
+    void update();
+    void reqeustCaptureScreenCover(s32 coverFrames);
+
+private:
+    s32 mCoverFrames = -1;
+    bool mIsScreenCovered = false;
+};
+
+static_assert(sizeof(ScreenCoverCtrl) == 0x8);
+}  // namespace al
