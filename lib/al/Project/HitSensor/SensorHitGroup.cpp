040a94de403e1e821eacce96d87ec5b5a378aa05
LynxDev2 <128722393+LynxDev2@users.noreply.github.com>
2025-02-06 14:52:31 +0700

    Finish SensorHitGroup implementation

--- lib/al/Project/HitSensor/SensorHitGroup.cpp
+++ lib/al/Project/HitSensor/SensorHitGroup.cpp
@@ -1,9 +1,11 @@
 #include "Project/HitSensor/SensorHitGroup.h"
 
+#include "Project/HitSensor/HitSensor.h"
+
 namespace al {
 
-SensorHitGroup::SensorHitGroup(s32 maxSensors, const char* groupName)
-    : mMaxSensors(maxSensors), mSensors(new HitSensor*[maxSensors]) {
+SensorHitGroup::SensorHitGroup(s32 maxSensors, const char* groupName) : mMaxSensors(maxSensors) {
+    mSensors = new HitSensor*[maxSensors];
     for (s32 i = 0; i < mMaxSensors; i++)
         mSensors[i] = nullptr;
 }
@@ -13,8 +15,23 @@ void SensorHitGroup::add(HitSensor* pSensor) {
     mSensorCount++;
 }
 
+void SensorHitGroup::remove(HitSensor* sensor) {
+    for (s32 i = 0; i < mSensorCount; i++) {
+        if (mSensors[i] == sensor) {
+            mSensors[i] = mSensors[mSensorCount - 1];
+            mSensorCount--;
+            return;
+        }
+    }
+}
+
 HitSensor* SensorHitGroup::getSensor(s32 idx) const {
     return mSensors[idx];
+}
+
+void SensorHitGroup::clear() const {
+    for (s32 i = 0; i < mSensorCount; i++)
+        mSensors[i]->setSensorCountZero();
 }
 
 }  // namespace al
