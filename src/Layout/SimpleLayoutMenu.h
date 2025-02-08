15792b2b3f562b09c3bb02fbe4b90775508c109c
GRAnimated <devin@nhscan.com>
2025-02-07 22:08:13 -0500

    Implement `SimpleLayoutMenu`

---
+++ src/Layout/SimpleLayoutMenu.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "Library/Layout/LayoutActor.h"
+
+namespace al {
+class LayoutInitInfo;
+}
+
+class SimpleLayoutMenu : public al::LayoutActor {
+public:
+    SimpleLayoutMenu(const char* name, const char* layoutName, const al::LayoutInitInfo& info,
+                     const char* archiveName, bool localize);
+    SimpleLayoutMenu(al::LayoutActor* parent, const char* name, const char* layoutName,
+                     const al::LayoutInitInfo& info, const char* archiveName);
+
+    void startAppear(const char* actionName);
+    void startEnd(const char* actionName);
+    void exeAppear();
+    void exeWait();
+    void exeEnd();
+    void exeEndWait();
+    bool isAppearOrWait() const;
+    bool isWait() const;
+    bool isEndWait() const;
+
+private:
+    s32 field_12C = -1;
+};
