74226dcc11817c497403ac5b904039a87004bc99
GRAnimated <devin@nhscan.com>
2025-02-08 03:11:27 -0500

    Implement `StainArea`

---
+++ src/Area/StainArea.cpp
@@ -0,0 +1,12 @@
+#include "Area/StainArea.h"
+#include "Library/Area/AreaInitInfo.h"
+#include "Library/Placement/PlacementFunction.h"
+
+StainArea::StainArea(const char* name) : al::AreaObj(name) {}
+
+void StainArea::init(const al::AreaInitInfo& info) {
+    AreaObj::init(info);
+    s32 stainType = 0;
+    al::tryGetArg(&stainType, info.getPlacementInfo(), "StainType");
+    mStainType = stainType;
+}
---
+++ src/Area/StainArea.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "Library/Area/AreaObj.h"
+
+class StainArea : public al::AreaObj {
+public:
+    StainArea(const char* name);
+    void init(const al::AreaInitInfo& info) override;
+
+    s32 getStainType() const { return mStainType; }
+
+private:
+    s32 mStainType = 0;
+};
