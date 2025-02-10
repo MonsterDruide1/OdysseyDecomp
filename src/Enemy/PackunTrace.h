1f2d759cb64c99f7f4cd22e4aaa569d89e069fe0
GRAnimated <devin@nhscan.com>
2025-02-10 00:00:29 -0500

    Fix

--- src/Enemy/PackunTrace.h
+++ src/Enemy/PackunTrace.h
@@ -10,10 +10,10 @@ class PackunTrace : public al::LiveActor {
 public:
     PackunTrace(al::LiveActor* parentActor);
 
-    void init(const al::ActorInitInfo& info);
-    void initAfterPlacement();
-    void control();
-    void appear();
+    void init(const al::ActorInitInfo& info) override;
+    void initAfterPlacement() override;
+    void control() override;
+    void appear() override;
 
 private:
     al::MtxConnector* mMtxConnector = nullptr;
