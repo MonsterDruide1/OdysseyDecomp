b0a232fcae59e7bbfd1b19d38932db71eb5f6236
LynxDev2 <128722393+LynxDev2@users.noreply.github.com>
2025-02-14 23:51:23 +0700

    PR: fix clang-format for headers with changed include

--- src/MapObj/CitySignal.cpp
+++ src/MapObj/CitySignal.cpp
@@ -4,8 +4,8 @@
 #include "Library/LiveActor/ActorActionFunction.h"
 #include "Library/LiveActor/ActorInitUtil.h"
 #include "Library/LiveActor/ActorModelFunction.h"
-#include "Library/LiveActor/ActorSensorUtil.h"
 #include "Library/LiveActor/ActorSensorMsgFunction.h"
+#include "Library/LiveActor/ActorSensorUtil.h"
 #include "Library/Nerve/NerveSetupUtil.h"
 #include "Library/Nerve/NerveUtil.h"
 
