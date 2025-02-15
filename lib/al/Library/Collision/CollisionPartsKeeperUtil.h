ab9fa563131fa2ec888c0a2b2a68d25130ae050e
german77 <5944268+german77@users.noreply.github.com>
2025-02-15 17:07:41 -0600

    Enemy: Implement Pecho

--- lib/al/Library/Collision/CollisionPartsKeeperUtil.h
+++ lib/al/Library/Collision/CollisionPartsKeeperUtil.h
@@ -13,4 +13,6 @@ namespace alCollisionUtil {
 bool getFirstPolyOnArrow(const al::IUseCollision*, sead::Vector3f*, al::Triangle*,
                          const sead::Vector3f&, const sead::Vector3f&,
                          const al::CollisionPartsFilterBase*, const al::TriangleFilterBase*);
-}
+s32 checkStrikeArrow(const al::IUseCollision*, const sead::Vector3f&, const sead::Vector3f&,
+                     const al::CollisionPartsFilterBase*, const al::TriangleFilterBase*);
+}  // namespace alCollisionUtil
