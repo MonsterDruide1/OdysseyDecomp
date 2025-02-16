d84159d70247d4b9551c292f3511b6934dcffad0
GRAnimated <devin@nhscan.com>
2025-02-16 08:17:05 -0500

    MapObj: Implement `SphinxRideMiniGameObjGenerator` (#344)

---
+++ src/MapObj/SphinxRideMiniGameObjGenerator.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <container/seadPtrArray.h>
+
+#include "Library/LiveActor/LiveActor.h"
+
+class SphinxRideMiniGameObjGenerator : public al::LiveActor {
+public:
+    SphinxRideMiniGameObjGenerator(const char* name, f32 unused1, f32 unused2);
+
+    void init(const al::ActorInitInfo& info) override;
+    void makeActorDeadAllActor();
+    void exeWait();
+    void appearAllActor();
+
+private:
+    sead::PtrArray<al::LiveActor> mActorArray;
+    sead::Vector3f* mTransArray = nullptr;
+    bool mIsAppearRandom = false;
+};
