ebfd43290d53677e5c87a989f2651e808a953120
Moddimation <vakhidmytr06@gmail.com>
2025-02-16 10:41:16 +0100

    Library: Work on `Camera` and `Projection` (#336)
    Co-authored-by: Moddimation <vakhdmyt06@gmail.com>

---
+++ lib/al/Library/Camera/CameraViewInfo.cpp
@@ -0,0 +1,37 @@
+#include "Library/Camera/CameraViewInfo.h"
+
+#include "Library/Projection/Projection.h"
+
+namespace al {
+
+CameraViewInfo::CameraViewInfo(s32 index, const sead::LookAtCamera& lookAtCam,
+                               const Projection& projection, const CameraViewFlag& flag,
+                               const OrthoProjectionInfo& orthoProjectionInfo)
+    : mIndex(index), mLookAtCam(lookAtCam), mProjection(projection), mCamViewFlag(flag),
+      mOrthoProjectionInfo(orthoProjectionInfo) {}
+
+const sead::Projection& CameraViewInfo::getProjectionSead() const {
+    return mProjection.getProjectionSead();
+}
+
+const sead::Matrix44f& CameraViewInfo::getProjMtx() const {
+    return mProjection.getProjMtx();
+}
+
+const sead::Matrix44f& CameraViewInfo::getProjMtxStd() const {
+    return mProjection.getMtxStd();
+}
+
+f32 CameraViewInfo::getAspect() const {
+    return mProjection.getAspect();
+}
+
+f32 CameraViewInfo::getNear() const {
+    return mProjection.getNear();
+}
+
+f32 CameraViewInfo::getFar() const {
+    return mProjection.getFar();
+}
+
+}  // namespace al
