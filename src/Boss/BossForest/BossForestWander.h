2a59d176425bf03dd765803c8a65657d3a5d318d
GRAnimated <devin@nhscan.com>
2025-02-16 06:25:47 -0500

    Boss/BossForest: Implement `BossForestWander` (#315)

---
+++ src/Boss/BossForest/BossForestWander.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "Library/LiveActor/LiveActor.h"
+
+namespace al {
+class KeyPoseKeeper;
+}
+
+class BossForestWander : public al::LiveActor {
+public:
+    BossForestWander(const char* name);
+    void init(const al::ActorInitInfo& info) override;
+
+    void exeWaitSwitchStart();
+    void exeWait();
+    void exeMove();
+    void exeEnd();
+
+private:
+    al::KeyPoseKeeper* mKeyPoseKeeper = nullptr;
+    s32 mMoveTime = -1;
+    s32 mWaitTime = -1;
+    sead::Vector3f mClippingRadius = sead::Vector3f::zero;
+};
