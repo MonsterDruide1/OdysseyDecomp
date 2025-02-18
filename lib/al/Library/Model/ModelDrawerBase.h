4711de266e44fb94dbca596e1b511289197db160
tetraxile <tetraxile@proton.me>
2025-02-18 17:46:34 +0000

    tidying up

--- lib/al/Library/Model/ModelDrawerBase.h
+++ lib/al/Library/Model/ModelDrawerBase.h
@@ -14,23 +14,22 @@ class ModelDrawBufferCounter;
 class ModelKeeper;
 class ModelDrawDisplayList;
 class ModelCtrl;
+class MeshDrawerTable;
 
 class ModelDrawerBase : public HioNode {
 public:
     ModelDrawerBase(const char* name);
     virtual ~ModelDrawerBase();
-
     virtual void createTable() = 0;
     virtual void draw() const = 0;
+    virtual void registerModel(ModelCtrl* model);
+    virtual void addModel(ModelCtrl* model);
+    virtual void removeModel(ModelCtrl* model);
+    virtual void updateModel(ModelCtrl* model);
 
-    virtual void registerModel(ModelCtrl*);
-    virtual void addModel(ModelCtrl*);
-    virtual void removeModel(ModelCtrl*);
-    virtual void updateModel(ModelCtrl*);
-
-    void setDrawInfo(agl::DrawContext*, const GraphicsSystemInfo*, const ModelDrawBufferCounter*,
-                     const ModelKeeper*);
-    void addModelToTable(ModelCtrl*);
+    void setDrawInfo(agl::DrawContext* drawCtx, const GraphicsSystemInfo* gfxInfo,
+                     const ModelDrawBufferCounter* bufferCounter, const ModelKeeper* modelKeeper);
+    void addModelToTable(ModelCtrl* model);
     void getDrawBufferIndex();
     void createModelDrawDisplayList(s32);
     bool isDraw() const;
@@ -42,9 +41,12 @@ private:
     const GraphicsSystemInfo* mGraphicsSystemInfo;
     const ModelDrawBufferCounter* mModelDrawBufferCounter;
     ModelDrawDisplayList* mDrawDisplayList;
-    s32 mCapacity;
-    s32 mSize;
-    ModelCtrl** mMdlCtrls;
-    void* field_48;
+    s32 mModelMaxNum;
+    s32 mModelNum;
+    ModelCtrl** mModels;
+    MeshDrawerTable* mMeshDrawerTable;
 };
+
+static_assert(sizeof(ModelDrawerBase) == 0x50);
+
 }  // namespace al
