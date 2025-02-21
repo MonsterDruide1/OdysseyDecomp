e0f8cf44821eb64771cec32ed0fe0a68e20974ed
tetraxile <tetraxile@proton.me>
2025-02-21 06:16:16 +0000

    finish `read` and `readResource`

---
+++ lib/al/Project/Controller/PadDataArcReader.cpp
@@ -0,0 +1,55 @@
+#include "Project/Controller/PadDataArcReader.h"
+
+#include "Library/Controller/PadDataPack.h"
+#include "Library/Resource/ResourceUtil.h"
+
+namespace al {
+
+PadDataArcReader::PadDataArcReader(const char* path) : mPath(path) {
+    al::findOrCreateResource(path, nullptr);
+}
+
+PadDataArcReader::PadDataArcReader(const char* path, const char* resourceName) : mPath(path) {
+    readResource(resourceName);
+}
+
+void PadDataArcReader::readResource(const char* resourceName) {
+    mCursorFrame = 0;
+    al::Resource* resource = al::findOrCreateResource(mPath, nullptr);
+    sead::FixedSafeString<256> filename;
+    filename.format("%s.bin", resourceName);
+    mDataFrames = (PadDataPack*)resource->getOtherFile(filename);
+    checkEnd();
+    PadDataPack* checkFrame = mDataFrames;
+    while (checkFrame->mTrig != -1)
+        checkFrame++;
+    mTotalFrame = checkFrame - mDataFrames;
+}
+
+void PadDataArcReader::checkEnd() {
+    if (mDataFrames[mCursorFrame].mTrig == -1)
+        mIsEnd = true;
+}
+
+void PadDataArcReader::read(PadDataPack* frameData) {
+    if (mIsEnd)
+        return;
+
+    const PadDataPack& curFrame = mDataFrames[mCursorFrame++];
+    *frameData = curFrame;
+    checkEnd();
+}
+
+bool PadDataArcReader::isEnd() const {
+    return mIsEnd;
+}
+
+u32 PadDataArcReader::getCursorFrame() const {
+    return mCursorFrame;
+}
+
+u32 PadDataArcReader::getRemainFrame() const {
+    return mTotalFrame - mCursorFrame;
+}
+
+}  // namespace al
