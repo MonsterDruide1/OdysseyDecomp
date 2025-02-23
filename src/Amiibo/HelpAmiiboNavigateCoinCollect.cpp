ff1487b5b1a9b448739af1d78ef02082e3f93947
german77 <5944268+german77@users.noreply.github.com>
2025-02-23 10:22:58 -0600

    Amiibo: Implement HelpAmiiboNavigateCoinCollect

---
+++ src/Amiibo/HelpAmiiboNavigateCoinCollect.cpp
@@ -0,0 +1,62 @@
+#include "Amiibo/HelpAmiiboNavigateCoinCollect.h"
+
+#include "Library/LiveActor/ActorFlagFunction.h"
+#include "Library/LiveActor/ActorPoseKeeper.h"
+#include "Library/Nfp/NfpFunction.h"
+#include "Library/Scene/SceneObjUtil.h"
+#include "Library/Se/SeFunction.h"
+
+#include "Amiibo/HelpAmiiboDirector.h"
+#include "Item/CoinCollect.h"
+#include "Item/CoinCollectHolder.h"
+#include "ModeBalloon/TimeBalloonHintArrow.h"
+#include "Scene/SceneObjFactory.h"
+
+HelpAmiiboNavigateCoinCollect::HelpAmiiboNavigateCoinCollect(HelpAmiiboDirector* director,
+                                                             al::LiveActor* actor)
+    : HelpAmiiboExecutor(director, actor, "コレクトコイン誘導お助け") {}
+
+void HelpAmiiboNavigateCoinCollect::initAfterPlacement(const al::ActorInitInfo& initInfo) {
+    HelpAmiiboExecutor::initAfterPlacement(initInfo);
+    mTimeBalloonHintArrow = new TimeBalloonHintArrow();
+    mTimeBalloonHintArrow->init(initInfo);
+    mTimeBalloonHintArrow->makeActorDead();
+}
+
+bool HelpAmiiboNavigateCoinCollect::isTriggerTouch(const al::NfpInfo& nfpInfo) const {
+    return al::isCharacterIdBaseKinopio(nfpInfo);
+}
+
+void HelpAmiiboNavigateCoinCollect::activate() {
+    HelpAmiiboExecutor::activate();
+    CoinCollect* coinCollect =
+        al::getSceneObj<CoinCollectHolder>(getActor(), SceneObjID_CoinCollectHolder)
+            ->tryFindAliveCoinCollect(sead::Vector3f::zero, true);
+
+    if (coinCollect != nullptr) {
+        al::startSe(getDirector(), "AmiiboMario");
+        mCoinCollect = coinCollect;
+        mTimeBalloonHintArrow->updateRotate(al::getTrans(coinCollect));
+        mTimeBalloonHintArrow->appear();
+        return;
+    }
+
+    al::startSe(getDirector(), "AmiiboKoopa");
+}
+
+bool HelpAmiiboNavigateCoinCollect::execute() {
+    if (mCoinCollect == nullptr)
+        return true;
+
+    if (al::isDead(mCoinCollect)) {
+        mTimeBalloonHintArrow->kill();
+        return true;
+    }
+
+    mTimeBalloonHintArrow->updateRotate(al::getTrans(mCoinCollect));
+    return false;
+}
+
+HelpAmiiboType HelpAmiiboNavigateCoinCollect::getType() const {
+    return HelpAmiiboType::Koopa;
+}
