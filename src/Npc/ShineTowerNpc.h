3c8d73445aaf5cfb9c507ec41a52c2d906c8327d
Naii-the-Baf <74033491+Naii-the-Baf@users.noreply.github.com>
2025-02-23 07:41:44 -0600

    Util: Implement `ShineTowerNpc` (#394)

---
+++ src/Npc/ShineTowerNpc.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <basis/seadTypes.h>
+
+#include "Library/LiveActor/LiveActor.h"
+
+namespace al {
+class ActorInitInfo;
+class EventFlowExecutor;
+}  // namespace al
+
+class ShineTowerNpc : public al::LiveActor {
+public:
+    ShineTowerNpc(const char* name);
+    void init(const al::ActorInitInfo& actorInitInfo) override;
+    void noBalloon();
+    void startBalloon();
+    void exeWait();
+    void exeGoKoopa();
+    void exeNoBalloon();
+
+private:
+    al::EventFlowExecutor* mEventFlowExecutor = nullptr;
+    s32 mRemainingShineCount = 0;
+};
+
+s32 getRemainingShineCount(al::LiveActor* actor);
