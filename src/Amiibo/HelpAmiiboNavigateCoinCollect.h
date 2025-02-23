09185f2ce9266c5de80a77f61bdc365593586dc3
german77 <5944268+german77@users.noreply.github.com>
2025-02-23 10:22:58 -0600

    Amiibo: Implement HelpAmiiboNavigateCoinCollect

---
+++ src/Amiibo/HelpAmiiboNavigateCoinCollect.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "Amiibo/HelpAmiiboExecutor.h"
+
+namespace al {
+struct NfpInfo;
+
+class LiveActor;
+}  // namespace al
+
+class CoinCollect;
+class HelpAmiiboDirector;
+class TimeBalloonHintArrow;
+
+class HelpAmiiboNavigateCoinCollect : public HelpAmiiboExecutor {
+public:
+    HelpAmiiboNavigateCoinCollect(HelpAmiiboDirector* director, al::LiveActor* actor);
+
+    void initAfterPlacement(const al::ActorInitInfo& initInfo) override;
+    bool isTriggerTouch(const al::NfpInfo& nfpInfo) const override;
+    void activate() override;
+    bool execute() override;
+    HelpAmiiboType getType() const override;
+
+private:
+    CoinCollect* mCoinCollect = nullptr;
+    TimeBalloonHintArrow* mTimeBalloonHintArrow = nullptr;
+};
