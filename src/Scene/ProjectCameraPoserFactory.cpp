f51f2583209a765d7a3ce0c0e0eb797792ceabfa
Fuzzy2319 <alexandre.verbrigghe@orange.fr>
2025-02-25 22:01:26 +0100

    fix:requested changes

--- src/Scene/ProjectCameraPoserFactory.cpp
+++ src/Scene/ProjectCameraPoserFactory.cpp
@@ -2,38 +2,34 @@
 
 #include "Library/Camera/CreateCameraPoserFunction.h"
 
-// clang-format off
 const al::NameToCreator<al::CameraPoserCreatorFunction> sProjectCameraPoserFactoryEntries1[] = {
-    {"制限付きフォロー", nullptr},	// CameraPoserFollowLimit
-    {"制限付き平行", nullptr}, 	// CameraPoserFollowLimit
-    {"2D平行", nullptr},      	// CameraPoserFollowLimit
-    {"固定", nullptr},         	// al::CameraPoserFix
-    {"完全固定", nullptr},      	// al::CameraPoserFix
-    {"出入口専用固定", nullptr}, // al::CameraPoserFix
-    {"定点", nullptr},         	// al::CameraPoserFixPoint
-    {"その場定点", nullptr},    	// al::CameraPoserFixPoint
-    {"完全追従定点", nullptr},  	// al::CameraPoserFixPoint
-    {"レース", nullptr},        	// al::CameraPoserRace
-    {"レール移動", nullptr},     	// al::CameraPoserRailMoveLookAt
-    {"キノピオ探検隊", nullptr}, 	// al::CameraPoserKinopioBrigade
-    {"会話用2点間", nullptr},   	// al::CameraPoserTalk
-    {"映像撮影レール", nullptr} 	// al::CameraPoserRailMoveMovie
-};
+    /* CameraPoserFollowLimit */ {"制限付きフォロー", nullptr},
+    /* CameraPoserFollowLimit */ {"制限付き平行", nullptr},
+    /* CameraPoserFollowLimit */ {"2D平行", nullptr},
+    /* al::CameraPoserFix */ {"固定", nullptr},
+    /* al::CameraPoserFix */ {"完全固定", nullptr},
+    /* al::CameraPoserFix */ {"出入口専用固定", nullptr},
+    /* al::CameraPoserFixPoint */ {"定点", nullptr},
+    /* al::CameraPoserFixPoint */ {"その場定点", nullptr},
+    /* al::CameraPoserFixPoint */ {"完全追従定点", nullptr},
+    /* al::CameraPoserRace */ {"レース", nullptr},
+    /* al::CameraPoserRailMoveLookAt */ {"レール移動", nullptr},
+    /* al::CameraPoserKinopioBrigade */ {"キノピオ探検隊", nullptr},
+    /* al::CameraPoserTalk */ {"会話用2点間", nullptr},
+    /* al::CameraPoserRailMoveMovie */ {"映像撮影レール", nullptr}};
 
 const al::NameToCreator<al::CameraPoserCreatorFunction> sProjectCameraPoserFactoryEntries2[] = {
-    {"ボス戦カメラ", nullptr},              	// al::CameraPoserBossBattle
-    {"スタート", nullptr},                		// al::CameraPoserEntrance
-    {"看板用2点間", nullptr},          		// al::CameraPoserLookBoard
-    {"見下ろし", nullptr},                	// al::CameraPoserLookDown
-    {"主観", nullptr},                 		// al::CameraPoserSubjective
-    {"塔", nullptr},                   		// al::CameraPoserTower
-    {"キー移動固定", nullptr},             	// al::KeyMoveCameraFix
-    {"キー移動レール移動", nullptr},          	// al::KeyMoveCameraRailMove
-    {"キー移動ズーム", nullptr},             	// al::KeyMoveCameraZoom
-    {"シナリオ紹介シンプルズームカメラ", nullptr},	// ScenarioStartCameraPoserSimpleZoom
-    {"シナリオ紹介レール移動カメラ", nullptr}		// ScenarioStartCameraPoserRailMove
-};
-// clang-format on
+    /* al::CameraPoserBossBattle */ {"ボス戦カメラ", nullptr},
+    /* al::CameraPoserEntrance */ {"スタート", nullptr},
+    /* al::CameraPoserLookBoard */ {"看板用2点間", nullptr},
+    /* al::CameraPoserLookDown */ {"見下ろし", nullptr},
+    /* al::CameraPoserSubjective */ {"主観", nullptr},
+    /* al::CameraPoserTower */ {"塔", nullptr},
+    /* al::KeyMoveCameraFix */ {"キー移動固定", nullptr},
+    /* al::KeyMoveCameraRailMove */ {"キー移動レール移動", nullptr},
+    /* al::KeyMoveCameraZoom */ {"キー移動ズーム", nullptr},
+    /* ScenarioStartCameraPoserSimpleZoom */ {"シナリオ紹介シンプルズームカメラ", nullptr},
+    /* ScenarioStartCameraPoserRailMove */ {"シナリオ紹介レール移動カメラ", nullptr}};
 
 ProjectCameraPoserFactory::ProjectCameraPoserFactory()
     : CameraPoserFactory("プロジェクトカメラファクトリー") {
