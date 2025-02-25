ac547e29ac7eec66ba200cec50257e29e4d3f997
german77 <5944268+german77@users.noreply.github.com>
2025-02-25 11:36:51 -0600

    Npc: Implement VolleyballBall

---
+++ src/Npc/VolleyballBall.h
@@ -0,0 +1,66 @@
+#pragma once
+
+#include <math/seadQuat.h>
+#include <math/seadVector.h>
+
+#include "Library/LiveActor/LiveActor.h"
+
+namespace al {
+
+class ActorInitInfo;
+class HitSensor;
+class ParabolicPath;
+class SensorMsg;
+}  // namespace al
+
+class VolleyballNpc;
+
+class VolleyballBall : public al::LiveActor {
+public:
+    VolleyballBall(const char* name);
+
+    void init(const al::ActorInitInfo& initInfo) override;
+    void control() override;
+    bool receiveMsg(const al::SensorMsg* message, al::HitSensor* other,
+                    al::HitSensor* self) override;
+    void attackSensor(al::HitSensor* self, al::HitSensor* other) override;
+
+    void attack(const sead::Vector3f& startingPostion, const sead::Vector3f& endPosition,
+                f32 averageSeed);
+    void toss(const sead::Vector3f& startingPostion, const sead::Vector3f& endPosition);
+    void reset();
+
+    bool isMiss() const;
+    bool isReturnEnd() const;
+    bool isTossEnd() const;
+    bool isActive() const;
+    bool isRetry() const;
+    bool isRetryEnd() const;
+
+    void exeWait();
+    void exeAttack();
+    void exeOnGround();
+    void endOnGround();
+    void exeReturn();
+    void exeReturnSmash();
+    void exeReturnEnd();
+    void exeMiss();
+    void exeMissReaction();
+    void exeToss();
+    void exeTossEnd();
+    void exeRetry();
+    void exeRetryEnd();
+
+private:
+    sead::Vector3f mEndPosition = sead::Vector3f::zero;
+    al::ParabolicPath* mFlyPath = nullptr;
+    al::ParabolicPath* mReturnPath = nullptr;
+    f32 mAverageSpeed = 0.0f;
+    s32 mPathTime = 0;
+    VolleyballNpc* mNpc = nullptr;
+    bool _138 = false;
+    sead::Quatf mOrientation = sead::Quatf::unit;
+    s32 mRandomDeviation = 0;
+    s32 mMissReactionDelay = 0;
+    s32 mMissDelay = 0;
+};
