5643318536578e80433096cdb51be16b07f48183
Muz-dev <113846204+Muz-dev@users.noreply.github.com>
2025-03-01 19:25:09 +0000

    Project/Camera: Implemented

--- lib/al/Library/Area/AreaObjUtil.h
+++ lib/al/Library/Area/AreaObjUtil.h
@@ -6,6 +6,7 @@ namespace al {
 class AreaObj;
 class AreaObjGroup;
 class IUseAreaObj;
+class AreaObjGroup;
 
 AreaObj* tryFindAreaObj(const IUseAreaObj* area, const char* name, const sead::Vector3f& pos);
 bool isInAreaObj(const AreaObjGroup* group, const sead::Vector3f& pos);
@@ -17,5 +18,6 @@ bool tryGetAreaObjArg(s32*, const AreaObj*, const char*);
 bool tryGetAreaObjArg(f32*, const AreaObj*, const char*);
 bool tryGetAreaObjArg(bool*, const AreaObj*, const char*);
 bool tryGetAreaObjStringArg(const char**, const AreaObj*, const char*);
-
+AreaObjGroup* tryFindAreaObjGroup(const IUseAreaObj* AreaObj, const char* name);
+AreaObj* tryGetAreaObj(const AreaObjGroup*, const sead::Vector3f&);
 }  // namespace al
