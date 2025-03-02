14bd91e3352eca8fc1f17e18eaadf33bf9ed690b
LynxDev2 <128722393+LynxDev2@users.noreply.github.com>
2025-03-02 23:32:10 +0200

    workflow: Fix `OdysseyHeaders` with authors with spaces (#420)

--- .github/scripts/push-headers.sh
+++ .github/scripts/push-headers.sh
@@ -12,7 +12,7 @@ cd "./OdysseyHeaders"
 echo "Open root of OdysseyHeaders repo"
 
 git add -A .
-git config user.name $AUTHOR_NAME
+git config user.name "$AUTHOR_NAME"
 git config user.email $AUTHOR_MAIL
 git commit -am "$MESSAGE"
 git push -f -u origin $HEADER_BRANCH
