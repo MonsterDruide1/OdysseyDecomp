4d6da789ef0ef50d8c52962a6e81e72abeaedbfd
GRAnimated <devin@nhscan.com>
2025-03-03 17:05:50 -0500

    Implement `GolemClimbFootPrint`

---
+++ src/Boss/GolemClimb/GolemClimbFootPrint.cpp
@@ -0,0 +1,19 @@
+#include "Boss/GolemClimb/GolemClimbFootPrint.h"
+#include "Library/LiveActor/ActorActionFunction.h"
+#include "Library/LiveActor/ActorInitUtil.h"
+
+GolemClimbFootPrint::GolemClimbFootPrint() : al::LiveActor("ゴーレムのぼり足跡") {}
+
+void GolemClimbFootPrint::init(const al::ActorInitInfo& info) {
+    al::initActorWithArchiveName(this, info, "GolemClimbFootPrint", nullptr);
+}
+
+void GolemClimbFootPrint::appear() {
+    al::startAction(this, "Disappear");
+    makeActorAlive();
+}
+
+void GolemClimbFootPrint::control() {
+    if (al::isActionEnd(this))
+        kill();
+}
