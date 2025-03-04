e1fda4ada28daa1332ac881d7baa02ee53d69b9c
Narr the Reg <5944268+german77@users.noreply.github.com>
2025-03-04 03:36:47 -0600

    Enemy: Implement `Pecho` (#373)

--- src/Scene/ProjectActorFactory.cpp
+++ src/Scene/ProjectActorFactory.cpp
@@ -31,6 +31,7 @@
 #include "Boss/Mofumofu/MofumofuScrap.h"
 #include "Enemy/Gamane.h"
 #include "Enemy/Mummy.h"
+#include "Enemy/Pecho.h"
 #include "Enemy/Togezo.h"
 #include "Enemy/Togezo2D.h"
 #include "Item/Coin.h"
@@ -393,7 +394,7 @@ const al::NameToCreator<al::ActorCreatorFunction> sProjectActorFactoryEntries[] 
     {"PeachWorldGate", nullptr},
     {"PeachWorldMoatWater", nullptr},
     {"PeachWorldTree", al::createActorFunction<PeachWorldTree>},
-    {"Pecho", nullptr},
+    {"Pecho", al::createActorFunction<Pecho>},
     {"Pen", nullptr},
     {"PictureStageChange", nullptr},
     {"PillarKeyMoveParts", nullptr},
