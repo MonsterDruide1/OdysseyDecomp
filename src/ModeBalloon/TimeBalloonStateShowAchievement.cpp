07891f189995905cba2bb3af084a8acfac393d81
GRAnimated <devin@nhscan.com>
2025-03-04 14:19:18 -0500

    ModeBalloon: Implement `TimeBalloonStateShowAchievement` (#431)

---
+++ src/ModeBalloon/TimeBalloonStateShowAchievement.cpp
@@ -0,0 +1,26 @@
+#include "ModeBalloon/TimeBalloonStateShowAchievement.h"
+
+#include "Library/Nerve/NerveSetupUtil.h"
+#include "Library/Nerve/NerveUtil.h"
+
+namespace {
+NERVE_IMPL(TimeBalloonStateShowAchievement, Show)
+
+NERVES_MAKE_NOSTRUCT(TimeBalloonStateShowAchievement, Show)
+}  // namespace
+
+TimeBalloonStateShowAchievement::TimeBalloonStateShowAchievement(const al::ActorInitInfo& info)
+    : al::NerveStateBase("実績を見るステート") {
+    initNerve(&Show, 0);
+}
+
+void TimeBalloonStateShowAchievement::appear() {
+    setDead(false);
+    al::setNerve(this, &Show);
+}
+
+void TimeBalloonStateShowAchievement::exeShow() {}
+
+void TimeBalloonStateShowAchievement::exeClose() {
+    kill();
+}
