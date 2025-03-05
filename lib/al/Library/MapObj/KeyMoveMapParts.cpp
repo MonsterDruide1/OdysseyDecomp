1ef4d56f8201135f0d63cf8d2040fd36ef94db2b
LynxDev2 <128722393+LynxDev2@users.noreply.github.com>
2025-03-05 17:51:13 +0200

    Library/LiveActor: Split `ActorSensorFunction` (#370)

--- lib/al/Library/MapObj/KeyMoveMapParts.cpp
+++ lib/al/Library/MapObj/KeyMoveMapParts.cpp
@@ -13,7 +13,7 @@
 #include "Library/LiveActor/ActorModelFunction.h"
 #include "Library/LiveActor/ActorMovementFunction.h"
 #include "Library/LiveActor/ActorPoseUtil.h"
-#include "Library/LiveActor/ActorSensorMsgFunction.h"
+#include "Library/LiveActor/ActorSensorUtil.h"
 #include "Library/MapObj/ChildStep.h"
 #include "Library/Nerve/NerveSetupUtil.h"
 #include "Library/Nerve/NerveUtil.h"
