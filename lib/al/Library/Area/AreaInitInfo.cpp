3c89870998669540bbea29c32dd59fc81de626bd
Fuzzy2319 <alexandre.verbrigghe@orange.fr>
2025-03-05 09:51:42 +0100

    update:remove useless comment && fix:lint

--- lib/al/Library/Area/AreaInitInfo.cpp
+++ lib/al/Library/Area/AreaInitInfo.cpp
@@ -3,20 +3,17 @@
 namespace al {
 AreaInitInfo::AreaInitInfo() : PlacementInfo() {}
 
-AreaInitInfo::AreaInitInfo(const al::PlacementInfo& placementInfo,
-                           al::StageSwitchDirector* stageSwitchDirector,
-                           al::SceneObjHolder* sceneObjHolder)
+AreaInitInfo::AreaInitInfo(const PlacementInfo& placementInfo,
+                           StageSwitchDirector* stageSwitchDirector, SceneObjHolder* sceneObjHolder)
     : PlacementInfo(placementInfo), mStageSwitchDirector(stageSwitchDirector),
       mSceneObjHolder(sceneObjHolder) {}
 
-AreaInitInfo::AreaInitInfo(const al::PlacementInfo& placementInfo, const al::AreaInitInfo& initInfo)
+AreaInitInfo::AreaInitInfo(const PlacementInfo& placementInfo, const AreaInitInfo& initInfo)
     : PlacementInfo(placementInfo), mStageSwitchDirector(initInfo.mStageSwitchDirector),
       mSceneObjHolder(initInfo.mSceneObjHolder) {}
 
-void AreaInitInfo::set(const al::PlacementInfo& placementInfo,
-                       al::StageSwitchDirector* stageSwitchDirector,
-                       al::SceneObjHolder* sceneObjHolder) {
-    //    PlacementInfo::set(placementInfo.getPlacementIter(), placementInfo.getZoneIter());
+void AreaInitInfo::set(const PlacementInfo& placementInfo, StageSwitchDirector* stageSwitchDirector,
+                       SceneObjHolder* sceneObjHolder) {
     setZoneIter(placementInfo.getZoneIter());
     setPlacementIter(placementInfo.getPlacementIter());
     mStageSwitchDirector = stageSwitchDirector;
