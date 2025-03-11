07b5663ae27cccffb894d1020958f0a298125a21
Naii-the-Baf <sfabian200@gmail.com>
2025-03-11 13:40:38 -0600

    Layout: Implement `ShopUtil`

---
+++ src/Layout/ShopUtil.cpp
@@ -0,0 +1,115 @@
+#include "Layout/ShopUtil.h"
+
+#include "Library/Layout/LayoutActionFunction.h"
+#include "Library/Layout/LayoutActorUtil.h"
+#include "Library/LiveActor/ActorActionFunction.h"
+#include "Library/LiveActor/ActorModelFunction.h"
+#include "Library/LiveActor/LiveActor.h"
+#include "Library/Message/MessageHolder.h"
+#include "Library/Play/Layout/SimpleLayoutAppearWaitEnd.h"
+#include "Library/Play/Layout/TalkMessageVoicePlayer.h"
+
+void ShopUtil::makeExplainLabel(al::StringTmp<256>* result, const char* firstString,
+                                const char* secondString, bool isAddPerson) {
+    const char* person = isAddPerson ? "Woman" : "";
+    result->format("%s%s%s", firstString, secondString, person);
+}
+
+void ShopUtil::startNpcMessage(al::SimpleLayoutAppearWaitEnd* layout, const char16* msgString,
+                               al::LiveActor* actor, const char* actorName, bool* something,
+                               al::TalkMessageVoicePlayer* voicePlayer) {
+    *something = false;
+    al::showPane(layout, "TxtMessage");
+
+    al::startTextPaneAnimWithAudioUser(layout, msgString, nullptr, nullptr, actor);
+    if (voicePlayer)
+        voicePlayer->start(layout, actor, msgString, 0);
+    al::startAction(actor, actorName);
+    return;
+}
+
+void ShopUtil::startNpcMessage(al::SimpleLayoutAppearWaitEnd* layout, const char* messageName,
+                               al::LiveActor* actor, const char* actorName, bool* something,
+                               al::TalkMessageVoicePlayer* voicePlayer) {
+    const char16* msgString = getSystemMessageStringForShop(layout, messageName);
+    *something = false;
+    al::showPane(layout, "TxtMessage");
+
+    al::startTextPaneAnimWithAudioUser(layout, msgString, nullptr, nullptr, actor);
+    if (voicePlayer)
+        voicePlayer->start(layout, actor, msgString, 0);
+    al::startAction(actor, actorName);
+    return;
+}
+
+const char16* ShopUtil::getSystemMessageStringForShop(const al::IUseMessageSystem* messageSystem,
+                                                      const char* messageName) {
+    return al::getSystemMessageString(messageSystem, "ShopMessage", messageName);
+}
+
+const char* ShopUtil::getShopNameCityMan() {
+    return "CityManShop";
+}
+
+const char* ShopUtil::getShopNameCityWoman() {
+    return "CityWomanShop";
+}
+
+const char* ShopUtil::getShopNameSeaWoman() {
+    return "SeaWoman";
+}
+
+const char* ShopUtil::getShopNameCapWoman() {
+    return "CapWoman";
+}
+
+const char* ShopUtil::getShopNameCapMan() {
+    return "CapMan";
+}
+
+bool ShopUtil::isShopWoman(const char* name) {
+    return al::isEqualString(name, getShopNameCapWoman()) ||
+           al::isEqualString(name, getShopNameSeaWoman()) ||
+           al::isEqualString(name, getShopNameCityWoman());
+}
+
+bool ShopUtil::isShopCap(const char* name) {
+    return al::isEqualString(name, getShopNameCapWoman()) ||
+           al::isEqualString(name, getShopNameCapMan());
+}
+
+const char* ShopUtil::getWaitShopInActionName(const al::LiveActor* actor) {
+    if (isShopCap(al::getModelName(actor)))
+        return "WaitShop";
+    return "WaitShopIn";
+}
+
+const char* ShopUtil::getWaitShopOutActionName(const al::LiveActor* actor) {
+    if (isShopCap(al::getModelName(actor)))
+        return "WaitShop";
+    return "WaitShopOut";
+}
+
+const char* ShopUtil::getTalkShopInActionName(const al::LiveActor* actor) {
+    if (isShopCap(al::getModelName(actor)))
+        return "TalkShop";
+    return "TalkShopIn";
+}
+
+const char* ShopUtil::getTalkShopOutActionName(const al::LiveActor* actor) {
+    if (isShopCap(al::getModelName(actor)))
+        return "TalkShop";
+    return "TalkShopOut";
+}
+
+const char* ShopUtil::getWelcomeShopInActionName(const al::LiveActor* actor) {
+    if (isShopCap(al::getModelName(actor)))
+        return "Welcome";
+    return "WelcomeIn";
+}
+
+const char* ShopUtil::getWelcomeShopOutActionName(const al::LiveActor* actor) {
+    if (isShopCap(al::getModelName(actor)))
+        return "Welcome";
+    return "WelcomeOut";
+}
