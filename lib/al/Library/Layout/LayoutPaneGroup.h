867f78a8c692a3a57e2b7b3140f081c60a15c084
GRAnimated <devin@nhscan.com>
2025-03-25 19:56:17 -0400

    Implement LayoutActionFunction

---
+++ lib/al/Library/Layout/LayoutPaneGroup.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <nn/ui2d/Layout.h>
+
+namespace al {
+
+class LayoutPaneGroup {
+public:
+    LayoutPaneGroup(const char* groupName);
+    void startAnim(const char* animName);
+    void getAnimator(const char* animName) const;
+    void setAnimFrame(f32 frame);
+    void setAnimFrameRate(f32 frameRate);
+    f32 getAnimFrame() const;
+    f32 getAnimFrameMax() const;
+    f32 getAnimFrameMax(const char* animName) const;
+    f32 getAnimFrameRate() const;
+    bool isAnimExist(const char* animName) const;
+    void tryGetAnimator(const char* animName) const;
+    bool isAnimEnd() const;
+    bool isAnimOneTime() const;
+    bool isAnimOneTime(const char* animName) const;
+    bool isAnimPlaying() const;
+    const char* getPlayingAnimName() const;
+    void pushAnimName(const char* animName);
+    void createAnimator(nn::ui2d::Layout* layout);
+    void animate(bool);
+
+private:
+    void* filler[3];
+};
+
+}  // namespace al
