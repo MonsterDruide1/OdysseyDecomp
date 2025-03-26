db9750cb01f1f67159e239202fe150398787f032
Moddimation <vakhidmytr06@gmail.com>
2025-03-26 02:28:36 +0100

    CameraPoseUpdater.h aktualisieren

--- lib/al/Library/Camera/CameraPoseUpdater.h
+++ lib/al/Library/Camera/CameraPoseUpdater.h
@@ -30,8 +30,8 @@ public:
               const CameraStartParamCtrl* startParamCtrl);
     void update();
     bool trySwitchCamera();
-    bool isActiveinterpole();
-    void startinterpole();
+    bool isActiveInterpole() const;
+    void startInterpole();
 
     void setNearClipDistance(f32 distance) { mNearClipDistance = distance; }
 
