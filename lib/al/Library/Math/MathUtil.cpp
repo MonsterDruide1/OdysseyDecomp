e4468aad79d876b5e7783c70744505b81759dbac
tetraxile <tetraxile@proton.me>
2025-03-31 06:54:17 +0100

    implement some MathUtil funcs

--- lib/al/Library/Math/MathUtil.cpp
+++ lib/al/Library/Math/MathUtil.cpp
@@ -2,8 +2,185 @@
 
 #include <math/seadMathCalcCommon.h>
 #include <random/seadGlobalRandom.h>
+#include "prim/seadBitUtil.h"
 
 namespace al {
+// f32 calcAngleRadian(const sead::Vector3f& a, const sead::Vector3f& b) {
+//     sead::Vector3f cross;
+//     cross.setCross(a, b);
+//     return sead::Mathf::atan2(cross.length(), a.dot(b));
+// }
+
+f32 calcAngleDegree(const sead::Vector2f& a, const sead::Vector2f& b) {
+    if (isNearZero(a, 0.001) || isNearZero(b, 0.001))
+        return 0.0f;
+
+    return sead::Mathf::rad2deg(sead::Mathf::atan2(a.cross(b), a.dot(b)));
+}
+
+bool isNearZero(const sead::Vector2f& vec, f32 tolerance) {
+    return vec.squaredLength() < tolerance * tolerance;
+}
+
+bool isNearZero(const sead::Vector3f& vec, f32 tolerance) {
+    return vec.squaredLength() < tolerance * tolerance;
+}
+
+f32 calcAngleOnPlaneDegreeOrZero(const sead::Vector3f& a, const sead::Vector3f& b,
+                                 const sead::Vector3f& c) {
+    f32 angle = 0.0f;
+    if (!tryCalcAngleOnPlaneDegree(&angle, a, b, c))
+        return 0.0f;
+
+    return angle;
+}
+
+bool isNearAngleDegreeHV(const sead::Vector3f& a, const sead::Vector3f& b, const sead::Vector3f& c,
+                         f32 d, f32 e) {
+    return isNearAngleRadianHV(a, b, c, sead::Mathf::deg2rad(d), sead::Mathf::deg2rad(e));
+}
+
+bool isNear(f32 value, f32 target, f32 tolerance) {
+    return sead::Mathf::abs(value - target) < sead::Mathf::abs(tolerance);
+}
+
+bool isNear(const sead::Vector2f& value, const sead::Vector2f& target, f32 tolerance) {
+    return (value - target).length() <= tolerance;
+}
+
+bool isNear(const sead::Vector3f& value, const sead::Vector3f& target, f32 tolerance) {
+    return (value - target).length() <= tolerance;
+}
+
+bool isNear(const sead::Color4f& value, const sead::Color4f& target, f32 tolerance) {
+    return sead::Mathf::abs(value.r - target.r) < tolerance &&
+           sead::Mathf::abs(value.g - target.g) < tolerance &&
+           sead::Mathf::abs(value.b - target.b) < tolerance &&
+           sead::Mathf::abs(value.a - target.a) < tolerance;
+}
+
+bool isNearZero(f32 value, f32 tolerance) {
+    return sead::Mathf::abs(value) < tolerance;
+}
+
+bool isNearZero(const sead::Matrix34f& value, f32 tolerance) {
+    sead::Vector3f vec;
+    value.getBase(vec, 0);
+    if (vec.squaredLength() < tolerance * tolerance)
+        return true;
+    value.getBase(vec, 1);
+    if (vec.squaredLength() < tolerance * tolerance)
+        return true;
+    value.getBase(vec, 2);
+    if (vec.squaredLength() < tolerance * tolerance)
+        return true;
+
+    return false;
+}
+
+bool isNearZeroOrGreater(f32 value, f32 tolerance) {
+    return value >= 0.0f || sead::Mathf::abs(value) < tolerance;
+}
+
+bool isNearZeroOrLess(f32 value, f32 tolerance) {
+    return value <= 0.0f || sead::Mathf::abs(value) < tolerance;
+}
+
+bool isExistNearZeroVal(const sead::Vector3f& vec, f32 tolerance) {
+    return sead::Mathf::abs(vec.x) < tolerance || sead::Mathf::abs(vec.y) < tolerance ||
+           sead::Mathf::abs(vec.z) < tolerance;
+}
+
+bool isNormalize(const sead::Vector3f& vec, f32 tolerance) {
+    return sead::Mathf::abs(1.0f - vec.length()) <= tolerance;
+}
+
+bool isParallelDirection(const sead::Vector2f& a, const sead::Vector2f& b, f32 tolerance) {
+    // looks awkward but mismatches without this
+    return !(tolerance < sead::Mathf::abs(a.cross(b)));
+}
+
+bool isNearDirection(const sead::Vector2f& a, const sead::Vector2f& b, f32 tolerance) {
+    if (a.dot(b) < 0.0f)
+        return false;
+
+    return !(sead::Mathf::abs(a.cross(b)) > tolerance);
+}
+
+void normalize(sead::Vector2f* vec) {
+    f32 len = vec->length();
+    if (len > 0.0f) {
+        const f32 invLen = 1.0f / len;
+        *vec *= invLen;
+    }
+}
+
+void normalize(sead::Vector3f* vec) {
+    vec->normalize();
+}
+
+bool tryNormalizeOrZero(sead::Vector2f* vec) {
+    if (isNearZero(*vec, 0.001f)) {
+        *vec = {0.0f, 0.0f};
+        return false;
+    } else {
+        normalize(vec);
+        return true;
+    }
+}
+
+bool tryNormalizeOrDirZ(sead::Vector3f* vec) {
+    if (isNearZero(*vec, 0.001f)) {
+        // mismatch if this isn't set twice
+        vec->set(0.0f, 0.0f, 0.0f);
+        vec->set(sead::Vector3f::ez);
+        return false;
+    } else {
+        normalize(vec);
+        return true;
+    }
+}
+
+void setLength(sead::Vector3f* vec, f32 lenSet) {
+    f32 len = vec->length();
+    if (len > 0.0f) {
+        const f32 scale = lenSet / len;
+        *vec *= scale;
+    }
+}
+
+void setProjectionLength(sead::Vector3f* out, const sead::Vector3f& vec, f32 len) {
+    const f32 scale = len / sead::Mathf::abs(vec.dot(*out));
+    *out *= scale;
+}
+
+bool limitLength(sead::Vector2f* out, const sead::Vector2f& vec, f32 limit) {
+    f32 len = vec.length();
+    if (len > limit) {
+        const f32 invLen = limit / len;
+        out->x = invLen * vec.x;
+        out->y = invLen * vec.y;
+        return true;
+    } else {
+        out->set(vec);
+        return false;
+    }
+}
+
+bool limitLength(sead::Vector3f* out, const sead::Vector3f& vec, f32 limit) {
+    f32 len = vec.length();
+    if (len > limit) {
+        const f32 invLen = limit / len;
+        out->x = invLen * vec.x;
+        out->y = invLen * vec.y;
+        out->z = invLen * vec.z;
+        return true;
+    } else {
+        out->set(vec);
+        return false;
+    }
+}
+
 f32 normalize(f32 var, f32 min, f32 max) {
     if (sead::Mathf::abs(max - min) < 0.001f) {
         if (var < min)
@@ -43,18 +220,6 @@ f32 normalize(s32 var, s32 min, s32 max) {
     return result;
 }
 
-f32 easeIn(f32 var) {
-    return (((var * -0.5f) + 1.5f) * var) * var;
-}
-
-f32 easeOut(f32 var) {
-    return (((var * -0.5f) * var) + 1.5f) * var;
-}
-
-f32 easeInOut(f32 var) {
-    return (((var * -2.0f) + 3.0f) * var) * var;
-}
-
 f32 sign(f32 result) {
     f32 v1;
     v1 = result < 0.0f;
@@ -73,6 +238,29 @@ s32 sign(s32 var) {
         return 1;
 
     return 0;
+}
+
+f32 cubeRoot(f32 x) {
+    const f32 onethird = 1.0f / 3.0f;
+
+    u32 i = 0x54a0fc86 - sead::BitUtil::bitCast<u32>(x) / 3;
+    f32 y = sead::BitUtil::bitCast<f32>(i);
+    y = y * onethird * (4.0f - x * y * y * y);
+    y = y * onethird * (4.0f - x * y * y * y);
+    y = y * onethird * (4.0f - x * y * y * y);
+    return x * y * y;
+}
+
+f32 easeIn(f32 var) {
+    return (((var * -0.5f) + 1.5f) * var) * var;
+}
+
+f32 easeOut(f32 var) {
+    return (((var * -0.5f) * var) + 1.5f) * var;
+}
+
+f32 easeInOut(f32 var) {
+    return (((var * -2.0f) + 3.0f) * var) * var;
 }
 
 f32 squareIn(f32 var) {
