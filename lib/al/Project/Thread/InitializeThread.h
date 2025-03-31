e0be51936b857aa847ac16724d9ee4f07176a63f
Aubrey Taylor <aubrey@hall.ly>
2025-03-31 10:32:45 -0600

    Project/Thread: Implement `InitializeThread`

---
+++ lib/al/Project/Thread/InitializeThread.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <basis/seadTypes.h>
+#include <prim/seadSafeString.h>
+
+namespace sead {
+class Heap;
+class DelegateThread;
+class Thread;
+}  // namespace sead
+
+namespace al {
+class FunctorBase;
+
+class InitializeThread {
+public:
+    InitializeThread(const sead::SafeString& threadName, const FunctorBase& functor,
+                     sead::Heap* heap, s32 priority, s32 stackSize);
+    void start();
+    bool tryWaitDoneAndDestroy();
+
+private:
+    void threadFunction(sead::Thread* thread, s64);
+
+    sead::DelegateThread* mThread = nullptr;
+    sead::Heap* mHeap = nullptr;
+    FunctorBase* mFunctor = nullptr;
+    bool mIsDone = false;
+};
+}  // namespace al
