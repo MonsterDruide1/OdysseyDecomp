334fafd12f922aecee12fc682a7655cf4bbc4ef9
Fuzzy2319 <61144247+Fuzzy2319@users.noreply.github.com>
2025-04-01 15:34:56 +0200

    Library/Area: Implement `SwitchAreaTargetInfo` (#488)

---
+++ lib/al/Library/Area/SwitchAreaTargetInfo.cpp
@@ -0,0 +1,42 @@
+#include "Library/Area/SwitchAreaTargetInfo.h"
+
+#include "Library/Camera/CameraUtil.h"
+#include "Library/Player/PlayerUtil.h"
+
+namespace al {
+SwitchAreaTargetInfo::SwitchAreaTargetInfo(s32 playerPositionCount, s32 cameraLookAtPositionSize)
+    : mPlayerTargetPositionSize(playerPositionCount), mPlayerPositionSize(playerPositionCount),
+      mCameraLookAtPositionSize(cameraLookAtPositionSize) {
+    mPlayerTargetPositions = new sead::Vector3f[mPlayerTargetPositionSize];
+    mPlayerPositions = new sead::Vector3f[mPlayerPositionSize];
+    mCameraLookAtPositions = new sead::Vector3f[mCameraLookAtPositionSize];
+}
+
+SwitchAreaTargetInfo::SwitchAreaTargetInfo(sead::Vector3f* mPlayerPositions,
+                                           s32 playerPositionCount)
+    : mPlayerTargetPositions(mPlayerPositions), mPlayerTargetPositionCount(playerPositionCount),
+      mPlayerTargetPositionSize(playerPositionCount), mPlayerPositions(mPlayerPositions),
+      mPlayerPositionCount(playerPositionCount), mPlayerPositionSize(playerPositionCount) {}
+
+void SwitchAreaTargetInfo::update(const PlayerHolder* playerHolder,
+                                  const SceneCameraInfo* sceneCameraInfo) {
+    mPlayerTargetPositionCount = 0;
+    mPlayerPositionCount = 0;
+
+    s32 playerNumMax = getPlayerNumMax(playerHolder);
+    for (s32 i = 0; i < playerNumMax; i++) {
+        const sead::Vector3f& playerPos = getPlayerPos(playerHolder, i);
+
+        mPlayerPositions[mPlayerPositionCount] = playerPos;
+        mPlayerPositionCount++;
+
+        if (!isPlayerDead(playerHolder, i) && isPlayerAreaTarget(playerHolder, i)) {
+            mPlayerTargetPositions[mPlayerTargetPositionCount] = playerPos;
+            mPlayerTargetPositionCount++;
+        }
+    }
+
+    mCameraLookAtPositions[0] = getCameraAt(sceneCameraInfo, 0);
+    mCameraLookAtPositionCount = 1;
+}
+}  // namespace al
