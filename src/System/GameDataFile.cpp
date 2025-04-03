710f413b6f5044c8f0ce19892a48cf41802e0e4f
ud2 <sjx233@qq.com>
2025-04-03 17:18:33 +0800

    Npc: Implement `BirdPlayerGlideCtrl` (#442)

--- src/System/GameDataFile.cpp
+++ src/System/GameDataFile.cpp
@@ -3,6 +3,7 @@
 #include "Library/Base/StringUtil.h"
 
 #include "System/GameDataFunction.h"
+#include "System/GameProgressData.h"
 
 // NON_MATCHING
 void GameDataFile::HintInfo::clear() {
@@ -92,4 +93,17 @@ bool GameDataFile::HintInfo::testFunc(s32 curWorldId, bool isGameClear, s32 scen
     if (mProgressBitflag.countOnBit())
         return mProgressBitflag.isOnBit(scenarioNo - 1);
     return true;
+}
+
+bool GameDataFile::isGameClear() const {
+    return isUnlockedWorld(GameDataFunction::getWorldIndexPeach()) &&
+           isAlreadyGoWorld(GameDataFunction::getWorldIndexPeach());
+}
+
+bool GameDataFile::isUnlockedWorld(s32 index) const {
+    return mGameProgressData->isUnlockWorld(index);
+}
+
+bool GameDataFile::isAlreadyGoWorld(s32 index) const {
+    return mGameProgressData->isAlreadyGoWorld(index);
 }
