beb28f61fd070fa5a410cb0d2bda6973670dc1de
Fuzzy2319 <alexandre.verbrigghe@orange.fr>
2025-04-04 15:56:39 +0200

    add:Resource implementation

--- lib/al/Library/Resource/Resource.cpp
+++ lib/al/Library/Resource/Resource.cpp
@@ -1,31 +1,43 @@
 #include "Library/Resource/Resource.h"
 
 #include <g3d/aglNW4FToNN.h>
-#include <heap/seadHeap.h>
+#include <heap/seadHeapMgr.h>
+#include <nn/g3d/ResFile.h>
+#include <resource/seadArchiveRes.h>
 
+#include "Library/Base/HashCodeUtil.h"
+#include "Library/Base/StringUtil.h"
 #include "Library/File/FileUtil.h"
 
 const sead::DirectoryEntry sEntries[0x1000];
 
 namespace al {
-Resource::Resource(const sead::SafeString& path) : mName(path) {
+Resource::Resource(const sead::SafeString& path)
+    : mName(path), mHeap(sead::HeapMgr::instance()->getCurrentHeap()) {
     mArchive = loadArchive(path);
     mDevice = new sead::ArchiveFileDevice(mArchive);
 }
 
-Resource::Resource(const sead::SafeString& path, sead::ArchiveRes* archive) : mName(path) {
+Resource::Resource(const sead::SafeString& path, sead::ArchiveRes* archive)
+    : mName(path), mHeap(sead::HeapMgr::instance()->getCurrentHeap()) {
     mArchive = archive;
     mDevice = new sead::ArchiveFileDevice(mArchive);
 }
 
-bool Resource::isExistFile(const sead::SafeString& path) const {
+bool Resource::isExistFile(const sead::SafeString& filePath) const {
     bool exists = false;
-    mDevice->tryIsExistFile(&exists, path);
+    mDevice->tryIsExistFile(&exists, filePath);
 
     return exists;
 }
 
-// bool Resource::isExistByml(const char*) const {}
+bool Resource::isExistByml(const char* filePath) const {
+    StringTmp<0x80> filePathExt;
+    filePathExt.copy(filePath);
+    filePathExt.append(".byml");
+
+    return isExistFile(filePathExt.cstr());
+}
 
 u32 Resource::getSize() const {
     return mArchive->getRawSize();
@@ -58,6 +70,87 @@ u32 Resource::getFileSize(const sead::SafeString& filePath) const {
     return mDevice->getFileSize(filePath);
 }
 
+const u8* Resource::getByml(const sead::SafeString& filePath) const {
+    StringTmp<0x80> filePathExt;
+    filePathExt.copy(filePath);
+    filePathExt.append(".byml");
+
+    return (const u8*)getFile(filePathExt);
+}
+
+const void* Resource::getFile(const sead::SafeString& filePath) const {
+    return mArchive->getFile(filePath);
+}
+
+const u8* Resource::tryGetByml(const sead::SafeString& filePath) const {
+    StringTmp<0x80> filePathExt;
+    filePathExt.copy(filePath);
+    filePathExt.append(".byml");
+
+    if (!isExistFile(filePathExt.cstr()))
+        return nullptr;
+
+    return (const u8*)getFile(filePathExt);
+}
+
+const void* Resource::getKcl(const sead::SafeString& filePath) const {
+    StringTmp<0x80> filePathExt;
+    filePathExt.copy(filePath);
+    filePathExt.append(".kcl");
+
+    return getFile(filePathExt);
+}
+
+const void* Resource::tryGetKcl(const sead::SafeString& filePath) const {
+    StringTmp<0x80> filePathExt;
+    filePathExt.copy(filePath);
+    filePathExt.append(".kcl");
+
+    if (!isExistFile(filePathExt.cstr()))
+        return nullptr;
+
+    return getFile(filePathExt);
+}
+
+const void* Resource::getPa(const sead::SafeString& filePath) const {
+    StringTmp<0x80> filePathExt = {"%s.pa", filePath.cstr()};
+
+    return getFile(filePathExt);
+}
+
+const void* Resource::tryGetPa(const sead::SafeString& filePath) const {
+    StringTmp<0x80> filePathExt;
+    filePathExt.copy(filePath);
+    filePathExt.append(".pa");
+
+    if (!isExistFile(filePathExt.cstr()))
+        return nullptr;
+
+    return getFile(filePathExt);
+}
+
+const void* Resource::getOtherFile(const sead::SafeString& filePath) const {
+    return getFile(filePath);
+}
+
+const char* Resource::getArchiveName() const {
+    return getBaseName(mName.cstr());
+}
+
+// bool Resource::tryCreateResGraphicsFile(const sead::SafeString& name, nn::g3d::ResFile* resFile)
+// {
+//     if (mResFile)
+//         return false;
+//
+//     mResFile = nn::g3d::ResFile::ResCast((void*)getFile(name));
+//     if (resFile)
+//         agl::g3d::ResFile::BindTexture(mResFile, resFile);
+//     agl::g3d::ResFile::Setup(mResFile);
+//     agl::g3d::ResFile::BindTexture(mResFile, mResFile);
+//
+//     return true;
+// }
+
 void Resource::cleanupResGraphicsFile() {
     if (!mResFile)
         return;
@@ -65,6 +158,7 @@ void Resource::cleanupResGraphicsFile() {
     mResFile->ReleaseTexture();
     mResFile->Reset();
     agl::g3d::ResFile::Cleanup(mResFile);
+
     mResFile = nullptr;
 }
 }  // namespace al
