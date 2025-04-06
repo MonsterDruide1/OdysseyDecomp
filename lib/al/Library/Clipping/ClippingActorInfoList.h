85e327121d6ebb5656206cbfe159c92a50624c21
-g <113846204+Muz-dev@users.noreply.github.com>
2025-04-06 18:29:24 +0100

    Ran clang-format

--- lib/al/Library/Clipping/ClippingActorInfoList.h
+++ lib/al/Library/Clipping/ClippingActorInfoList.h
@@ -14,6 +14,7 @@ public:
     ClippingActorInfo* find(const LiveActor* liveActor, s32* index) const;
     ClippingActorInfo* tryFind(const LiveActor* liveActor) const;
     bool isInList(const LiveActor* liveActor) const;
+
 private:
     s32 mMaxClippingInfo = 0;
     s32 mClippingInfoCount = 0;
