eb9c02f29cd160de59fb49cc4fe6f6af80282fdb
-g <113846204+Muz-dev@users.noreply.github.com>
2025-04-21 20:22:32 +0100

    Rebase finished

--- lib/al/Project/Clipping/ClippingFarAreaObserver.h
+++ lib/al/Project/Clipping/ClippingFarAreaObserver.h
@@ -19,17 +19,10 @@ private:
     const AreaObjDirector* mAreaObjDirector;
     const PlayerHolder* mPlayerHolder;
     const AreaObjGroup* mAreaObjGroup = nullptr;
-    const AreaObj* mAreaObj = nullptr;
-    f32 mDefaultFarClipDistanceHorizontal = 7000.0f;
-    f32 mDefaultFarClipDistanceVertical = 7000.0f;
-    f32 mDefaultFarClipDistanceHorizontalSub = 4000.0f;
-    f32 mDefaultFarClipDistanceVerticalSub = 4000.0f;
->>>>>>> 01c3dd20 (Implemented ClippingFarAreaObserver)
-=======
+    AreaObj* mAreaObj = nullptr;
     f32 mFarClipDistance = 7000.0f;
     f32 mDefaultFarClipDistance = 7000.0f;
     f32 mFarClipDistanceSub = 4000.0f;
     f32 mDefaultFarClipDistanceSub = 4000.0f;
->>>>>>> 3aa4b1c0 (Addressed review)
 };
 }  // namespace al
