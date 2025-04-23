ac29ee00f9a78b8152fe2de1b1493b2f9a72c637
Muz-dev <113846204+Muz-dev@users.noreply.github.com>
2025-04-23 20:01:34 +0100

    Project/Clipping: Implemented `ClippingFarAreaObserver` (#520)

--- lib/al/Project/Clipping/ClippingFarAreaObserver.h
+++ lib/al/Project/Clipping/ClippingFarAreaObserver.h
@@ -1,18 +1,28 @@
 #pragma once
 
-#include <basis/seadTypes.h>
+#include "Library/Area/AreaObjGroup.h"
 
 namespace al {
-
-class PlayerHolder;
 class AreaObjDirector;
+class PlayerHolder;
 
 class ClippingFarAreaObserver {
 public:
-    ClippingFarAreaObserver(const AreaObjDirector*, const PlayerHolder*);
-    void setDefaultFarClipDistance(f32);
-    void setDefaultFarClipDistanceSub(f32);
+    ClippingFarAreaObserver(const AreaObjDirector* areaObjDirector,
+                            const PlayerHolder* playerHolder);
     void endInit();
+    void setDefaultFarClipDistance(f32 distance);
+    void setDefaultFarClipDistanceSub(f32 distance);
     void update();
+
+private:
+    const AreaObjDirector* mAreaObjDirector;
+    const PlayerHolder* mPlayerHolder;
+    const AreaObjGroup* mAreaObjGroup = nullptr;
+    AreaObj* mAreaObj = nullptr;
+    f32 mFarClipDistance = 7000.0f;
+    f32 mDefaultFarClipDistance = 7000.0f;
+    f32 mFarClipDistanceSub = 4000.0f;
+    f32 mDefaultFarClipDistanceSub = 4000.0f;
 };
 }  // namespace al
