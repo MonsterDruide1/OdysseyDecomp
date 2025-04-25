87e1bbf14e786629f470dbcdf15e30a0ac42a299
Fuzzy2319 <alexandre.verbrigghe@orange.fr>
2025-04-25 22:00:33 +0200

    add:AreaObjUtil partial implementation

--- lib/al/Library/Area/AreaObjUtil.cpp
+++ lib/al/Library/Area/AreaObjUtil.cpp
@@ -3,7 +3,9 @@
 #include "Library/Area/AreaObj.h"
 #include "Library/Area/AreaObjDirector.h"
 #include "Library/Area/AreaObjGroup.h"
+#include "Library/Area/AreaShape.h"
 #include "Library/Area/IUseAreaObj.h"
+#include "Library/Placement/PlacementFunction.h"
 
 namespace al {
 AreaObj* tryFindAreaObj(const IUseAreaObj* areaUser, const char* name,
@@ -57,10 +59,7 @@ AreaObjGroup* tryFindAreaObjGroup(const IUseAreaObj* areaUser, const char* name)
 }
 
 bool isInAreaObj(const AreaObjGroup* group, const sead::Vector3f& position) {
-    if (group == nullptr)
-        return false;
-
-    return group->getInVolumeAreaObj(position) != nullptr;
+    return tryGetAreaObj(group, position) != nullptr;
 }
 
 AreaObj* tryGetAreaObj(const AreaObjGroup* group, const sead::Vector3f& position) {
@@ -75,6 +74,99 @@ bool isInAreaPos(const AreaObj* areaObj, const sead::Vector3f& position) {
 }
 
 bool isInAreaObj(const IUseAreaObj* areaUser, const char* name, const sead::Vector3f& position) {
-    return areaUser->getAreaObjDirector()->getInVolumeAreaObj(name, position) != nullptr;
+    return tryFindAreaObj(areaUser, name, position) != nullptr;
+}
+
+bool isExistAreaObj(const IUseAreaObj* areaUser, const char* name) {
+    return areaUser->getAreaObjDirector()->isExistAreaGroup(name);
+}
+
+bool isInDeathArea(const IUseAreaObj* areaUser, const sead::Vector3f& position) {
+    return isInAreaObj(areaUser, "DeathArea", position);
+}
+
+bool isInWaterArea(const IUseAreaObj* areaUser, const sead::Vector3f& position) {
+    return isInAreaObj(areaUser, "WaterArea", position);
+}
+
+bool isInPlayerControlOffArea(const IUseAreaObj* areaUser, const sead::Vector3f& position) {
+    return isInAreaObj(areaUser, "PlayerControlOffArea", position);
+}
+
+s32 calcAreaObjNum(const IUseAreaObj* areaUser, const char* name) {
+    AreaObjGroup* group = tryFindAreaObjGroup(areaUser, name);
+    if (group == nullptr)
+        return 0;
+
+    return group->getSize();
+}
+
+f32 calcWaterSinkDepth(const IUseAreaObj* areaUser, const sead::Vector3f& position) {
+    AreaObj* areaObj = tryFindAreaObj(areaUser, "WaterArea", position);
+    if (!areaObj)
+        return -1.0f;
+
+    sead::Vector3f hitPosition;
+    sead::Vector3f normal;
+    sead::Vector3f position2 = {position.x, position.y + 100000.0f, position.z};
+
+    if (!areaObj->getAreaShape()->checkArrowCollision(&hitPosition, &normal, position, position2))
+        return -1.0f;
+
+    return hitPosition.y - position.y;
+}
+
+bool tryGetAreaObjArg(s32* outArg, const AreaObj* areaObj, const char* key) {
+    if (areaObj->getPlacementInfo() == nullptr)
+        return false;
+
+    return tryGetArg(outArg, *areaObj->getPlacementInfo(), key);
+}
+
+bool tryGetAreaObjArg(f32* outArg, const AreaObj* areaObj, const char* key) {
+    if (areaObj->getPlacementInfo() == nullptr)
+        return false;
+
+    return tryGetArg(outArg, *areaObj->getPlacementInfo(), key);
+}
+
+bool tryGetAreaObjArg(bool* outArg, const AreaObj* areaObj, const char* key) {
+    if (areaObj->getPlacementInfo() == nullptr)
+        return false;
+
+    return tryGetArg(outArg, *areaObj->getPlacementInfo(), key);
+}
+
+bool tryGetAreaObjStringArg(const char** outArg, const AreaObj* areaObj, const char* key) {
+    if (areaObj->getPlacementInfo() == nullptr)
+        return false;
+
+    return tryGetStringArg(outArg, *areaObj->getPlacementInfo(), key);
+}
+
+const sead::Matrix34f& getAreaObjBaseMtx(const AreaObj* areaObj) {
+    return areaObj->getAreaMtx();
+}
+
+const sead::Vector3f& getAreaObjScale(const AreaObj* areaObj) {
+    return areaObj->getAreaShape()->getScale();
+}
+
+void getAreaObjDirFront(sead::Vector3f* outFrontDir, const AreaObj* areaObj) {
+    outFrontDir->x = getAreaObjBaseMtx(areaObj)(0, 2);
+    outFrontDir->y = getAreaObjBaseMtx(areaObj)(1, 2);
+    outFrontDir->z = getAreaObjBaseMtx(areaObj)(2, 2);
+}
+
+void getAreaObjDirUp(sead::Vector3f* outUpDir, const AreaObj* areaObj) {
+    outUpDir->x = getAreaObjBaseMtx(areaObj)(0, 1);
+    outUpDir->y = getAreaObjBaseMtx(areaObj)(1, 1);
+    outUpDir->z = getAreaObjBaseMtx(areaObj)(2, 1);
+}
+
+void getAreaObjDirSide(sead::Vector3f* outSideDir, const AreaObj* areaObj) {
+    outSideDir->x = getAreaObjBaseMtx(areaObj)(0, 0);
+    outSideDir->y = getAreaObjBaseMtx(areaObj)(1, 0);
+    outSideDir->z = getAreaObjBaseMtx(areaObj)(2, 0);
 }
 }  // namespace al
--- lib/al/Library/Area/AreaObjUtil.h
+++ lib/al/Library/Area/AreaObjUtil.h
@@ -31,13 +31,19 @@ bool isInAreaObj(const AreaObjGroup* group, const sead::Vector3f& position);
 AreaObj* tryGetAreaObj(const AreaObjGroup* group, const sead::Vector3f& position);
 bool isInAreaPos(const AreaObj* areaObj, const sead::Vector3f& position);
 bool isInAreaObj(const IUseAreaObj* areaUser, const char* name, const sead::Vector3f& position);
-
-bool isInDeathArea(const IUseAreaObj*, const sead::Vector3f&);
-
-bool tryGetAreaObjArg(s32*, const AreaObj*, const char*);
-bool tryGetAreaObjArg(f32*, const AreaObj*, const char*);
-bool tryGetAreaObjArg(bool*, const AreaObj*, const char*);
-bool tryGetAreaObjStringArg(const char**, const AreaObj*, const char*);
-
-const sead::Matrix34f& getAreaObjBaseMtx(const AreaObj*);
+bool isExistAreaObj(const IUseAreaObj* areaUser, const char* name);
+bool isInDeathArea(const IUseAreaObj* areaUser, const sead::Vector3f& position);
+bool isInWaterArea(const IUseAreaObj* areaUser, const sead::Vector3f& position);
+bool isInPlayerControlOffArea(const IUseAreaObj* areaUser, const sead::Vector3f& position);
+s32 calcAreaObjNum(const IUseAreaObj* areaUser, const char* name);
+f32 calcWaterSinkDepth(const IUseAreaObj* areaUser, const sead::Vector3f& position);
+bool tryGetAreaObjArg(s32* outArg, const AreaObj* areaObj, const char* key);
+bool tryGetAreaObjArg(f32* outArg, const AreaObj* areaObj, const char* key);
+bool tryGetAreaObjArg(bool* outArg, const AreaObj* areaObj, const char* key);
+bool tryGetAreaObjStringArg(const char** outArg, const AreaObj* areaObj, const char* key);
+const sead::Matrix34f& getAreaObjBaseMtx(const AreaObj* areaObj);
+const sead::Vector3f& getAreaObjScale(const AreaObj* areaObj);
+void getAreaObjDirFront(sead::Vector3f* outFrontDir, const AreaObj* areaObj);
+void getAreaObjDirUp(sead::Vector3f* outUpDir, const AreaObj* areaObj);
+void getAreaObjDirSide(sead::Vector3f* outSideDir, const AreaObj* areaObj);
 }  // namespace al
