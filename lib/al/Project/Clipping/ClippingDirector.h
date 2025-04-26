72d28e73e1a5e3e7885dc797e6a6d8d5889e1d6a
-g <113846204+Muz-dev@users.noreply.github.com>
2025-04-26 09:16:39 +0100

    Addressed clang-format

--- lib/al/Project/Clipping/ClippingDirector.h
+++ lib/al/Project/Clipping/ClippingDirector.h
@@ -39,8 +39,7 @@ private:
     void* filler[0x2];
     ClippingActorHolder* mClippingActorHolder;
     void* filler2[0x3];
-
-
 };
+
 static_assert(sizeof(ClippingDirector) == 0x38);
 }  // namespace al
