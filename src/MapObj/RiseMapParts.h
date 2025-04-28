1880d6d38d9ff080c058183aeddbef77f520b22a
Naii-the-Baf <sfabian200@gmail.com>
2025-04-28 17:39:23 -0600

    MapObj: Implement `AppearSwitchSave`

---
+++ src/MapObj/RiseMapParts.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <basis/seadTypes.h>
+
+#include "Library/LiveActor/LiveActor.h"
+
+namespace al {
+struct ActorInitInfo;
+class HitSensor;
+class SensorMsg;
+}  // namespace al
+
+class RiseMapParts : public al::LiveActor {
+public:
+    RiseMapParts(const char*);
+    void init(const al::ActorInitInfo&) override;
+    void initAfterPlacement() override;
+    bool receiveMsg(const al::SensorMsg* message, al::HitSensor* other,
+                    al::HitSensor* self) override;
+    bool isStop();
+    void control() override;
+    void setStop();
+    void setStopPose();
+
+    void exeWait();
+    void exeMove();
+    void start();
+    void exePause();
+    void exeStopBefore();
+    void exeStop();
+    void startRise();
+
+private:
+    void* filler[6];
+};
+
+static_assert(sizeof(RiseMapParts) == 0x138);
