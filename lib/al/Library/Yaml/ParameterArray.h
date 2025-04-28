e1ca79533ab614cf18e4e5a21f6180373d98b4cc
german77 <5944268+german77@users.noreply.github.com>
2025-04-27 23:24:13 -0600

    Library/Yaml: Implement ParameterList

--- lib/al/Library/Yaml/ParameterArray.h
+++ lib/al/Library/Yaml/ParameterArray.h
@@ -13,20 +13,26 @@ public:
     void tryGetParam(const ByamlIter& iter);
     bool isEqual(const ParameterArray& array) const;
     void copy(const ParameterArray& array);
-    void copyLerp(const ParameterArray& arrayA, const ParameterArray& arrayB, f32 value);
+    void copyLerp(const ParameterArray& arrayA, const ParameterArray& arrayB, f32 rate);
     void addObj(ParameterObj* obj);
     void clearObj();
     void removeObj(ParameterObj* obj);
     bool isExistObj(ParameterObj* obj);
 
-    ParameterObj* getFirstObj() const { return mFirstObj; }
+    ParameterObj* getRootObjNode() const { return mRootObjNode; }
+
+    ParameterArray* getNext() const { return mNext; }
+
+    void setNext(ParameterArray* array) { mNext = array; }
+
+    void setKey(const sead::SafeString& key) { mKey = key; }
 
     s32 getSize() const { return mSize; }
 
 private:
-    ParameterObj* mFirstObj = nullptr;
+    ParameterObj* mRootObjNode = nullptr;
     ParameterArray* mNext = nullptr;
-    sead::FixedSafeString<0x40> mParamObjKey;
+    sead::FixedSafeString<0x40> mKey;
     s32 mSize = 0;
 };
 
