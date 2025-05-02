9faca71acda69d4d3432d9eeaf2f49c4dbad2850
Narr the Reg <5944268+german77@users.noreply.github.com>
2025-05-02 13:55:05 -0600

    Project: Implement `RollingCubePose` (#568)

--- lib/al/Library/Math/MathUtil.h
+++ lib/al/Library/Math/MathUtil.h
@@ -8,6 +8,7 @@
 #include "Library/Math/Axis.h"
 
 namespace al {
+enum class Axis : s32;
 class VisitCellCallBack;
 
 template <typename T>
@@ -405,9 +406,9 @@ bool calcBoundingSphereSpotLight(sead::Vector3f*, f32*, const sead::Vector3f&,
 void calcBoundingSphereBox3f(sead::Vector3f*, f32*, const sead::BoundBox3f&);
 void calcArrowAabb(sead::BoundBox3f*, const sead::Vector3f&, const sead::Vector3f&);
 bool isNearCollideSphereAabb(const sead::Vector3f&, f32, const sead::BoundBox3f&);
-void calcBoxFacePoint(sead::Vector3f*, const sead::BoundBox3f&, s32);
-void calcBoxFacePoint(sead::Vector3f*, const sead::BoundBox3f&, s32, const sead::Matrix34f&);
-void calcBoxFacePoint(sead::Vector3f*, const sead::BoundBox3f&, s32, const sead::Quatf&,
+void calcBoxFacePoint(sead::Vector3f[4], const sead::BoundBox3f&, Axis);
+void calcBoxFacePoint(sead::Vector3f[4], const sead::BoundBox3f&, Axis, const sead::Matrix34f&);
+void calcBoxFacePoint(sead::Vector3f[4], const sead::BoundBox3f&, Axis, const sead::Quatf&,
                       const sead::Vector3f&);
 void calcFittingBoxPoseEqualAxisAll(sead::Quatf*, const sead::Quatf&, const sead::Quatf&);
 void calcFittingBoxPoseEqualAxisNone(sead::Quatf*, const sead::Quatf&, const sead::Quatf&);
