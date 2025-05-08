db1d1686b7a08fd8b49f066ff549f011a68d56a9
german77 <5944268+german77@users.noreply.github.com>
2025-05-08 13:26:49 -0600

    Project: Implement ArchiveEntry and FileEntryBase

---
+++ lib/al/Project/ArchiveEntry.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <basis/seadTypes.h>
+#include <prim/seadSafeString.h>
+#include <resource/seadResourceMgr.h>
+
+#include "Project/FileEntryBase.h"
+
+namespace sead {
+class ArchiveRes;
+class FileDevice;
+class Heap;
+}  // namespace sead
+
+namespace al {
+
+class ArchiveEntry : public FileEntryBase {
+public:
+    ArchiveEntry();
+
+    void load() override;
+    void setLoadRequestInfo(const sead::SafeStringBase<char>& fileName, sead::Heap* heap,
+                            sead::FileDevice* fileDevice);
+    sead::ArchiveRes* getArchiveRes();
+    void clear();
+
+private:
+    sead::ResourceMgr::LoadArg mLoadArg;
+    sead::ArchiveRes* mArchiveRes = nullptr;
+};
+
+static_assert(sizeof(ArchiveEntry) == 0x118);
+
+}  // namespace al
---
+++ lib/al/Project/FileEntryBase.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <basis/seadTypes.h>
+#include <prim/seadSafeString.h>
+#include <thread/seadMessageQueue.h>
+
+namespace al {
+
+class FileEntryBase {
+public:
+    FileEntryBase();
+
+    virtual void load() = 0;
+
+    void setFileName(const sead::SafeStringBase<char>& fileName);
+    const sead::FixedSafeString<0x40>& getFileName() const;
+    void sendMessageDone();
+    void waitLoadDone();
+    void clear();
+    void setLoadStateRequested();
+
+private:
+    enum class FileState : s32 { None, IsLoadRequested, IsSendMessageDone, IsLoadDone };
+
+    sead::FixedSafeString<0x40> mFileName;
+    FileState mFileState = FileState::None;
+    sead::MessageQueue mMessageQueue;
+};
+
+static_assert(sizeof(FileEntryBase) == 0xb8);
+}  // namespace al
