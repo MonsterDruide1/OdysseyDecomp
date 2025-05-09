2e58e795de1daee6324fde40f04c64050415aeb9
german77 <5944268+german77@users.noreply.github.com>
2025-05-09 10:26:04 -0600

    Project: Implement ArchiveHolder

---
+++ lib/al/Project/ArchiveHolder.cpp
@@ -0,0 +1,45 @@
+#include "Project/ArchiveHolder.h"
+
+#include <basis/seadNew.h>
+
+#include "Library/Base/StringUtil.h"
+
+namespace al {
+
+ArchiveHolder::ArchiveHolder() {
+    mArchiveEntries.allocBufferAssert(0x800, nullptr, 8);
+}
+
+ArchiveEntry* ArchiveHolder::addNewLoadRequestEntry(const sead::SafeString& fileName,
+                                                    sead::Heap* heap,
+                                                    sead::FileDevice* fileDevice) {
+    ArchiveEntry* entry = mArchiveEntries.get(mSize);
+    entry->setLoadRequestInfo(fileName, heap, fileDevice);
+    mSize++;
+    return entry;
+}
+
+ArchiveEntry* ArchiveHolder::tryFindEntry(const sead::SafeString& fileName) {
+    for (s32 i = 0; i < mSize; i++) {
+        ArchiveEntry* entry = mArchiveEntries.get(i);
+        if (isEqualString(entry->getFileName(), fileName))
+            return entry;
+    }
+    return nullptr;
+}
+
+void ArchiveHolder::waitLoadDoneAll() {
+    for (s32 i = 0; i < mSize; i++) {
+        ArchiveEntry* entry = mArchiveEntries.get(i);
+        if (entry->mFileState != FileState::IsLoadDone)
+            entry->waitLoadDone();
+    }
+}
+
+void ArchiveHolder::clearEntry() {
+    for (s32 i = 0; i < mSize; i++)
+        mArchiveEntries.get(i)->clear();
+    mSize = 0;
+}
+
+}  // namespace al
---
+++ lib/al/Project/ArchiveHolder.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <basis/seadTypes.h>
+#include <container/seadBuffer.h>
+#include <prim/seadSafeString.h>
+
+#include "Project/ArchiveEntry.h"
+
+namespace sead {
+class FileDevice;
+class Heap;
+}  // namespace sead
+
+namespace al {
+
+class ArchiveHolder {
+public:
+    ArchiveHolder();
+    ArchiveEntry* addNewLoadRequestEntry(const sead::SafeString& fileName, sead::Heap* heap,
+                                         sead::FileDevice* fileDevice);
+    ArchiveEntry* tryFindEntry(const sead::SafeString& fileName);
+    void waitLoadDoneAll();
+    void clearEntry();
+
+private:
+    sead::Buffer<ArchiveEntry> mArchiveEntries;
+    s32 mSize = 0;
+};
+
+}  // namespace al
