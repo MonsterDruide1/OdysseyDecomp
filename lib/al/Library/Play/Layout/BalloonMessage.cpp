bfbfcfb8bee0dbedb045211f0faa81ed1b506618
Fuzzy2319 <alexandre.verbrigghe@orange.fr>
2025-05-15 16:39:05 +0200

    add:BalloonMessage implementation

---
+++ lib/al/Library/Play/Layout/BalloonMessage.cpp
@@ -0,0 +1,252 @@
+#include "Library/Play/Layout/BalloonMessage.h"
+
+#include "Library/Layout/LayoutActionFunction.h"
+#include "Library/Layout/LayoutActorUtil.h"
+#include "Library/Layout/LayoutInitInfo.h"
+#include "Library/LiveActor/ActorClippingFunction.h"
+#include "Library/LiveActor/ActorFlagFunction.h"
+#include "Library/LiveActor/ActorInitUtil.h"
+#include "Library/LiveActor/ActorMovementFunction.h"
+#include "Library/LiveActor/ActorPoseUtil.h"
+#include "Library/LiveActor/LiveActor.h"
+#include "Library/Nerve/NerveSetupUtil.h"
+#include "Library/Nerve/NerveUtil.h"
+#include "Library/Play/Layout/TalkMessageVoicePlayer.h"
+#include "Library/Player/PlayerUtil.h"
+#include "Library/Screen/ScreenFunction.h"
+#include "Library/Se/SeFunction.h"
+
+namespace {
+using namespace al;
+
+NERVE_IMPL(BalloonMessage, Wait)
+NERVE_IMPL(BalloonMessage, Appear)
+NERVE_IMPL(BalloonMessage, Hide)
+NERVE_IMPL(BalloonMessage, End)
+
+NERVES_MAKE_NOSTRUCT(BalloonMessage, Wait)
+NERVES_MAKE_STRUCT(BalloonMessage, Appear, Hide, End)
+}  // namespace
+
+namespace al {
+BalloonMessage* BalloonMessage::create(const LiveActor* hostActor, const LayoutInitInfo& info,
+                                       const BalloonMessageInitParam& initParam,
+                                       bool isAutoUpdate) {
+    return new BalloonMessage(hostActor, info, initParam, isAutoUpdate);
+}
+
+BalloonMessage::BalloonMessage(const LiveActor* hostActor, const LayoutInitInfo& info,
+                               const BalloonMessageInitParam& initParam, bool isAutoUpdate)
+    : LayoutActor(initParam._0), mHostActor(hostActor), _138(initParam._10), _140(initParam._20),
+      _144(initParam._24), _154(initParam._34), _158(initParam._38), mIsAutoUpdate(isAutoUpdate),
+      _15d(initParam._3c) {
+    _148.y = initParam._30;
+
+    initLayoutActor(this, info, initParam._8, initParam._28);
+
+    mTalkMessageVoicePlayer = new TalkMessageVoicePlayer();
+    mTalkMessageVoicePlayer->set_420(true);
+
+    setText(initParam._18);
+    initNerve(&Wait, 0);
+
+    kill();
+}
+
+void BalloonMessage::setText(const char* message) {
+    setPaneStringFormat(this, _138, message);
+}
+
+inline void startAppear(LayoutActor* layout) {
+    startAction(layout, "Appear", nullptr);
+}
+
+void BalloonMessage::appear() {
+    updateTrans();
+    hidePushA();
+
+    if (!mIsAutoUpdate) {
+        startAction(this, "Appear", nullptr);
+        LayoutActor::appear();
+
+        if (!_15e)
+            mTalkMessageVoicePlayer->start(this, mHostActor, getPaneStringBuffer(this, _138), 5);
+
+        setNerve(this, &NrvBalloonMessage.Appear);
+
+        return;
+    }
+
+    if (isEnableAppear()) {
+        startAppear(this);
+        LayoutActor::appear();
+        setActionFrameRate(this, 1.0f, nullptr);
+        setNerve(this, &NrvBalloonMessage.Appear);
+
+        return;
+    }
+
+    LayoutActor::appear();
+    startFreezeActionEnd(this, "End", nullptr);
+    setNerve(this, &NrvBalloonMessage.Hide);
+}
+
+void BalloonMessage::updateTrans() {
+    sead::Vector2f layoutPos = sead::Vector2f::zero;
+
+    if (_158 == 0)
+        calcLayoutPosFromWorldPos(&layoutPos, mHostActor, getTrans(mHostActor) + _148);
+    else
+        calcLayoutPosFromWorldPosSub(&layoutPos, mHostActor, getTrans(mHostActor) + _148);
+
+    setLocalTrans(this, layoutPos);
+}
+
+void BalloonMessage::hidePushA() {
+    if (!isExistPane(this, "TxtA"))
+        return;
+
+    hidePane(this, "TxtA");
+}
+
+bool BalloonMessage::isEnableAppear() const {
+    if (!isNerve(this, &NrvBalloonMessage.End) && !isNerve(this, &NrvBalloonMessage.Hide) &&
+        isAlive())
+        return false;
+
+    return isNearPlayerActor(_140);
+}
+
+void BalloonMessage::appearWithPushA() {
+    appear();
+    showPushA();
+}
+
+void BalloonMessage::showPushA() {
+    if (!isExistPane(this, "TxtA"))
+        return;
+
+    showPane(this, "TxtA");
+}
+
+// TODO
+void BalloonMessage::control() {
+    u16 cond;
+    if ((*(u16*)&mIsAutoUpdate & 0xff) == 0) {
+        cond = *(u16*)&mIsAutoUpdate >> 8;
+    } else {
+        update();
+        cond = (u16)_15d;
+    }
+
+    if (cond == 0 || isNerve(this, &NrvBalloonMessage.End) ||
+        isNerve(this, &NrvBalloonMessage.Hide) || (!isClipped(mHostActor) && !isDead(mHostActor))) {
+        mTalkMessageVoicePlayer->update();
+        updateTrans();
+
+        return;
+    }
+
+    setNerve(this, &NrvBalloonMessage.End);
+}
+
+void BalloonMessage::update() {
+    if (isEnableAppear())
+        appear();
+
+    if (isEnableEnd())
+        setNerve(this, &NrvBalloonMessage.End);
+}
+
+bool BalloonMessage::isWait() const {
+    return isNerve(this, &Wait);
+}
+
+bool BalloonMessage::isVoicePlayerPlaying() const {
+    if (mTalkMessageVoicePlayer->isPlaying())
+        return true;
+
+    if (_168.cstr() == nullptr)
+        return false;
+
+    return checkIsPlayingSe(mHostActor, _168.cstr(), nullptr);
+}
+
+bool BalloonMessage::isShowPushA() const {
+    return !isHidePane(this, "TxtA");
+}
+
+bool BalloonMessage::isEnableEnd() const {
+    if (isNerve(this, &NrvBalloonMessage.End) || isNerve(this, &NrvBalloonMessage.Hide) ||
+        !isAlive())
+        return false;
+
+    return !isNearPlayerActor(_144);
+}
+
+void BalloonMessage::end() {
+    if (isNerve(this, &NrvBalloonMessage.End) || isNerve(this, &NrvBalloonMessage.Hide))
+        return;
+
+    setNerve(this, &NrvBalloonMessage.End);
+}
+
+void BalloonMessage::setTextW(const char16* message) {
+    setPaneString(this, _138, message, 0);
+}
+
+void BalloonMessage::exeAppear() {
+    if (isActionEnd(this, nullptr))
+        setNerve(this, &Wait);
+}
+
+void BalloonMessage::exeWait() {
+    if (isFirstStep(this))
+        startAction(this, "Wait", nullptr);
+}
+
+void BalloonMessage::exeEnd() {
+    if (isFirstStep(this))
+        startAction(this, "End", nullptr);
+
+    if (isActionEnd(this, nullptr)) {
+        if (mIsAutoUpdate)
+            setNerve(this, &NrvBalloonMessage.Hide);
+        else
+            kill();
+    }
+}
+
+void BalloonMessage::exeHide() {}
+
+bool BalloonMessage::isNearPlayerActor(f32 threshold) const {
+    if (_154 >= 0)
+        return isNear(mHostActor, getPlayerActor(mHostActor, _154), threshold);
+
+    return isNearPlayer(mHostActor, threshold);
+}
+
+BalloonMessage* createBalloonMessage(const LiveActor* hostActor, const ActorInitInfo& info,
+                                     const BalloonMessageInitParam& initParam) {
+    return BalloonMessage::create(hostActor, getLayoutInitInfo(info), initParam, true);
+}
+
+BalloonMessage* createBalloonMessageNoAutoUpdate(const LiveActor* hostActor,
+                                                 const ActorInitInfo& info,
+                                                 const BalloonMessageInitParam& initParam) {
+    return BalloonMessage::create(hostActor, getLayoutInitInfo(info), initParam, false);
+}
+
+BalloonMessage* createBalloonMessage(const LiveActor* hostActor, const ActorInitInfo& info) {
+    BalloonMessageInitParam initParam = {._18 = "未設定"};
+
+    return BalloonMessage::create(hostActor, getLayoutInitInfo(info), initParam, true);
+}
+
+BalloonMessage* createBalloonMessage(const LiveActor* hostActor, const ActorInitInfo& info,
+                                     const char* message) {
+    BalloonMessageInitParam initParam = {._18 = message};
+
+    return BalloonMessage::create(hostActor, getLayoutInitInfo(info), initParam, true);
+}
+}  // namespace al
---
+++ lib/al/Library/Play/Layout/BalloonMessage.h
@@ -0,0 +1,86 @@
+#pragma once
+
+#include <math/seadVector.h>
+
+#include "Library/Layout/LayoutActor.h"
+
+namespace al {
+class ActorInitInfo;
+class LayoutInitInfo;
+class LiveActor;
+class TalkMessageVoicePlayer;
+
+struct BalloonMessageInitParam {
+public:
+    const char* _0 = "バルーン";
+    const char* _8 = "BalloonSpeak";
+    const char* _10 = "TxtMessage";
+    const char* _18;
+    f32 _20 = 500.0f;
+    f32 _24 = 600.0f;
+    const char* _28 = nullptr;
+    f32 _30 = 160.0f;
+    s32 _34 = -1;
+    s32 _38 = 0;
+    bool _3c = false;
+};
+
+class BalloonMessage : public LayoutActor {
+public:
+    static BalloonMessage* create(const LiveActor* hostActor, const LayoutInitInfo& info,
+                                  const BalloonMessageInitParam& initParam, bool isAutoUpdate);
+
+    BalloonMessage(const LiveActor* hostActor, const LayoutInitInfo& info,
+                   const BalloonMessageInitParam& initParam, bool isAutoUpdate);
+
+    void appear() override;
+    void control() override;
+
+    void end();
+    void update();
+    void updateTrans();
+    void appearWithPushA();
+    void hidePushA();
+    void showPushA();
+
+    void setText(const char* message);
+    void setTextW(const char16* message);
+    bool isEnableAppear() const;
+    bool isEnableEnd() const;
+    bool isNearPlayerActor(f32 threshold) const;
+    bool isShowPushA() const;
+    bool isVoicePlayerPlaying() const;
+    bool isWait() const;
+
+    void exeAppear();
+    void exeWait();
+    void exeEnd();
+    void exeHide();
+
+private:
+    const LiveActor* mHostActor;
+    const char* _138;
+    f32 _140;
+    f32 _144;
+    sead::Vector3f _148 = sead::Vector3f::zero;
+    s32 _154;
+    s32 _158;
+    bool mIsAutoUpdate;
+    bool _15d;
+    bool _15e = false;
+    TalkMessageVoicePlayer* mTalkMessageVoicePlayer = nullptr;
+    sead::FixedSafeString<0x40> _168 = {};
+};
+
+BalloonMessage* createBalloonMessage(const LiveActor* hostActor, const ActorInitInfo& info,
+                                     const BalloonMessageInitParam& initParam);
+BalloonMessage* createBalloonMessageNoAutoUpdate(const LiveActor* hostActor,
+                                                 const ActorInitInfo& info,
+                                                 const BalloonMessageInitParam& initParam);
+BalloonMessage* createBalloonMessage(const LiveActor* hostActor, const ActorInitInfo& info);
+BalloonMessage* createBalloonMessage(const LiveActor* hostActor, const ActorInitInfo& info,
+                                     const char* message);
+
+static_assert(sizeof(BalloonMessageInitParam) == 0x40);
+static_assert(sizeof(BalloonMessage) == 0x1c0);
+}  // namespace al
