b1769b2056b495e62bf319f39f9eda864fe9953d
Naii-the-Baf <sfabian200@gmail.com>
2025-05-20 17:02:42 -0600

    MapObj: Implement `SignBoardDanger`

---
+++ src/MapObj/SignBoardDanger.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <basis/seadTypes.h>
+
+#include "Library/LiveActor/LiveActor.h"
+
+namespace al {
+struct ActorInitInfo;
+class HitSensor;
+class SensorMsg;
+}  // namespace al
+class CapHanger;
+class SignBoardBlow;
+
+class SignBoardDanger : public al::LiveActor {
+public:
+    SignBoardDanger(const char* name);
+    void init(const al::ActorInitInfo& info) override;
+    bool receiveMsg(const al::SensorMsg* msg, al::HitSensor* other, al::HitSensor* self) override;
+    bool isCanStartReaction();
+    void attackSensor(al::HitSensor* other, al::HitSensor* self) override;
+    void exeWait();
+    void exeReaction();
+    void exeDead();
+
+private:
+    SignBoardBlow* mSignBoardBlow = nullptr;
+    f32 mYRotation = 0.0f;
+    s32 _114 = 0;
+    CapHanger* mCapHanger = nullptr;
+};
+
+static_assert(sizeof(SignBoardDanger) == 0x120);
