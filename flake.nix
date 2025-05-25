ec7e78644ff722f2f9cf21ad582d570c87e9d79a
LynxDev2 <128722393+LynxDev2@users.noreply.github.com>
2025-05-25 17:56:19 +0300

    Setup and building: Use `clang`, `lld` and `viking` from cache repo  (#348)

--- flake.nix
+++ flake.nix
@@ -3,47 +3,43 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     systems.url = "github:nix-systems/default";
-    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
   outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } {
     systems = import inputs.systems;
-    perSystem = { config, self', inputs', pkgs, system, ... }: let
-      overlays = [ (import inputs.rust-overlay) ];
-      pkgs = import inputs.nixpkgs {
-        inherit system overlays;
-      };
-      rustToolchain = pkgs.pkgsBuildHost.rust-bin.stable.latest.default;
-    in {
-      _module.args.pkgs = import inputs.nixpkgs {
-        config.allowUnfree = true;
-      };
-      devShells.default = pkgs.mkShell rec {
-        buildInputs = with pkgs; [
-          cmake
-          ninja
-          llvmPackages_18.clang
-          ccache
-          pkg-config
+    perSystem = { config, self', inputs', pkgs, system, ... }:
+      let
+        pkgs = import inputs.nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        _module.args.pkgs = import inputs.nixpkgs {
+          config.allowUnfree = true;
+        };
+        devShells.default = pkgs.mkShell rec {
+          buildInputs = with pkgs; [
+            cmake
+            ninja
+            llvmPackages_18.clang-tools
+            ccache
+            pkg-config
 
-          rustToolchain
-          (python3.withPackages (python-pkgs: [
-            python-pkgs.capstone
-            python-pkgs.colorama
-            python-pkgs.cxxfilt
-            python-pkgs.pyelftools
-            python-pkgs.watchdog
-            python-pkgs.python-Levenshtein
-            python-pkgs.toml
-          ]))
-          openssl
-          llvmPackages_18.libclang
-          ncurses5
-          ncurses6
-        ];
-        LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath buildInputs}";
-        LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+            (python311.withPackages (python-pkgs: [
+              python-pkgs.capstone
+              python-pkgs.colorama
+              python-pkgs.cxxfilt
+              python-pkgs.pyelftools
+              python-pkgs.watchdog
+              python-pkgs.python-Levenshtein
+              python-pkgs.toml
+            ]))
+            openssl
+            ncurses5
+            ncurses6
+          ];
+          LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath buildInputs}";
+        };
       };
-    };
   };
 }
