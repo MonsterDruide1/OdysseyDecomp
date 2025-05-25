5fdfcf97567ae5889009282d4051079f5c1bedd6
MonsterDruide1 <5958456@gmail.com>
2025-05-25 15:08:22 +0200

    Player: Implement `PlayerCeilingCheck` (#607)

---
+++ src/Player/PlayerCeilingCheck.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <math/seadVector.h>
+
+namespace al {
+class CollisionDirector;
+class CollisionPartsFilterBase;
+}  // namespace al
+class PlayerCollisionCheckSphereMove;
+
+class PlayerCeilingCheck {
+public:
+    PlayerCeilingCheck(al::CollisionDirector* director);
+
+    void setupCeilingCheckNormal();
+    void update(const sead::Vector3f& trans, const sead::Vector3f& up, f32 tall,
+                f32 collisionRadiusSquat, f32 headClearance, f32 holdHeight);
+    void setupCeilingCheckGrab();
+    void setCollisionPartsFilter(const al::CollisionPartsFilterBase* filter);
+
+private:
+    PlayerCollisionCheckSphereMove* mCollisionCheckSphereMove = nullptr;
+    f32 mSafetyCeilSpace = 0.0f;
+    f32 mCeilCheckHeight = 0.0f;
+    bool mIsCeilingCheckNormal = true;
+    bool mIsCeilingCheckGrab = false;
+    bool mIsEnableStandUp = false;
+    bool mIsEnableHoldUp = false;
+    bool mIsPressedCeil = false;
+};
+
+static_assert(sizeof(PlayerCeilingCheck) == 0x18);
