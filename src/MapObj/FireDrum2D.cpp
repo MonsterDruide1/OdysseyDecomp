9538e1b6fd6d8be0842a921b338f066aee90eaaa
MonsterDruide1 <5958456@gmail.com>
2025-05-29 21:42:49 +0200

    Util: Implement `ActorDimensionKeeper` (#624)

--- src/MapObj/FireDrum2D.cpp
+++ src/MapObj/FireDrum2D.cpp
@@ -6,7 +6,7 @@
 #include "Library/Nerve/NerveSetupUtil.h"
 #include "Library/Nerve/NerveUtil.h"
 
-#include "Util/ActorDimensionKeeper.h"
+#include "Util/ActorDimensionUtil.h"
 #include "Util/SensorMsgFunction.h"
 
 namespace {
