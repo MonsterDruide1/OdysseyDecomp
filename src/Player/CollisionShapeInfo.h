3e438eba2d66a6650110aaf695db61628b4d26fc
MonsterDruide1 <5958456@gmail.com>
2025-05-29 22:34:29 +0200

    Player: Implement `CollisionShapeKeeper` (#615)

--- src/Player/CollisionShapeInfo.h
+++ src/Player/CollisionShapeInfo.h
@@ -41,3 +41,96 @@ private:
     CollisionShapeId mId;
     const char* mName;
 };
+
+class CollisionShapeInfoArrow : public CollisionShapeInfoBase {
+public:
+    CollisionShapeInfoArrow(const char*, const sead::Vector3f&, const sead::Vector3f&, f32, s32);
+
+    const sead::Vector3f& getBoundingCenter() const override;
+    const sead::Vector3f& getBoundingCenterWorld() const override;
+    f32 getBoundingRadius() const override;
+    f32 getBoundingRadiusWorld() const override;
+    f32 getCheckStepRange() const override;
+    f32 getCheckStepRangeWorld() const override;
+    void updateShapeOffset(const sead::Vector3f&) override;
+    void calcWorldShapeInfo(const sead::Matrix34f&, f32) override;
+    void calcRelativeShapeInfo(const sead::Matrix34f&) override;
+
+private:
+    void* _18[20];
+};
+
+static_assert(sizeof(CollisionShapeInfoArrow) == 0xb8);
+
+class CollisionShapeInfoSphere : public CollisionShapeInfoBase {
+public:
+    CollisionShapeInfoSphere(const char*, f32, const sead::Vector3f&);
+
+    const sead::Vector3f& getBoundingCenter() const override;
+    const sead::Vector3f& getBoundingCenterWorld() const override;
+    f32 getBoundingRadius() const override;
+    f32 getBoundingRadiusWorld() const override;
+    f32 getCheckStepRange() const override;
+    f32 getCheckStepRangeWorld() const override;
+    void updateShapeOffset(const sead::Vector3f&) override;
+    void calcWorldShapeInfo(const sead::Matrix34f&, f32) override;
+    void calcRelativeShapeInfo(const sead::Matrix34f&) override;
+
+    // TODO: rename parameter
+    void setSupportGround(const sead::Vector3f& up, f32 unk1) {
+        mIsSupportGround = true;
+        mUp = up;
+        _48 = unk1;
+    }
+
+    void setIgnoreGround() { mIsIgnoreGround = true; }
+
+private:
+    void* _18[5];
+    s32 _3x;
+    bool mIsSupportGround;
+    f32 _48;
+    f32 _4c;
+    sead::Vector3f mUp;
+    sead::Vector3f _60;
+    bool mIsIgnoreGround;
+};
+
+static_assert(sizeof(CollisionShapeInfoSphere) == 0x70);
+
+class CollisionShapeInfoDisk : public CollisionShapeInfoBase {
+public:
+    CollisionShapeInfoDisk(const char*, f32, const sead::Vector3f&, const sead::Vector3f&, f32);
+
+    void updateDiskShape(f32, const sead::Vector3f&, f32);
+
+    const sead::Vector3f& getBoundingCenter() const override;
+    const sead::Vector3f& getBoundingCenterWorld() const override;
+    f32 getBoundingRadius() const override;
+    f32 getBoundingRadiusWorld() const override;
+    f32 getCheckStepRange() const override;
+    f32 getCheckStepRangeWorld() const override;
+    void updateShapeOffset(const sead::Vector3f&) override;
+    void calcWorldShapeInfo(const sead::Matrix34f&, f32) override;
+    void calcRelativeShapeInfo(const sead::Matrix34f&) override;
+
+    // TODO: rename parameter
+    void setSupportGround(const sead::Vector3f& up, f32 unk1) {
+        mIsSupportGround = true;
+        mUp = up;
+        _84 = unk1;
+    }
+
+    void setIgnoreGround() { mIsIgnoreGround = true; }
+
+private:
+    void* _18[13];
+    bool mIsSupportGround;
+    f32 _84;
+    f32 _88;
+    sead::Vector3f mUp;
+    sead::Vector3f _98;
+    bool mIsIgnoreGround = false;
+};
+
+static_assert(sizeof(CollisionShapeInfoDisk) == 0xA8);
---
+++ src/Player/CollisionShapeKeeper.h
@@ -0,0 +1,69 @@
+#pragma once
+
+#include <container/seadPtrArray.h>
+#include <math/seadMatrix.h>
+#include <math/seadVector.h>
+
+class CollisionShapeInfoBase;
+class CollisionShapeInfoArrow;
+class CollisionShapeInfoSphere;
+class CollisionShapeInfoDisk;
+class CollidedShapeResult;
+
+class CollisionShapeKeeper {
+public:
+    CollisionShapeKeeper(s32 maxShapes, s32 maxCollideResults, s32 maxCollideSupportResults);
+    // TODO: rename parameters
+    void createShapeArrow(const char* name, const sead::Vector3f& start, const sead::Vector3f& end,
+                          f32 unk1, s32 unk2);
+    void createShapeSphere(const char* name, f32 radius, const sead::Vector3f& position);
+    // TODO: rename parameter
+    void createShapeSphereSupportGround(const char* name, f32 radius, const sead::Vector3f& pos,
+                                        const sead::Vector3f& up, f32 unk1);
+    void createShapeSphereIgnoreGround(const char* name, f32 radius, const sead::Vector3f& pos);
+    // TODO: rename parameters
+    void createShapeDisk(const char* name, f32 a1, const sead::Vector3f& a2,
+                         const sead::Vector3f& a3, f32 a4);
+    // TODO: rename parameters
+    void createShapeDiskSupportGround(const char* name, f32 a1, const sead::Vector3f& a2,
+                                      const sead::Vector3f& a3, f32 a4, const sead::Vector3f& a5,
+                                      f32 a6);
+    // TODO: rename parameters
+    void createShapeDiskIgnoreGround(const char* name, f32 a1, const sead::Vector3f& a2,
+                                     const sead::Vector3f& a3, f32 a4);
+    void updateShape();
+    void clearResult();
+    // TODO: rename parameters
+    void calcWorldShapeInfo(const sead::Matrix34f& a1, f32 a2);
+    // TODO: rename parameter
+    void calcRelativeShapeInfo(const sead::Matrix34f& a1);
+    void registerCollideResult(const CollidedShapeResult& result);
+    void registerCollideSupportResult(const CollidedShapeResult& result);
+    bool isCollidedResultFull() const;
+    bool isCollidedSupportResultFull() const;
+    bool isShapeArrow(s32 index) const;
+    bool isShapeSphere(s32 index) const;
+    bool isShapeDisk(s32 index) const;
+    const CollisionShapeInfoBase* getShapeInfoBase(s32 index) const;
+    const CollisionShapeInfoArrow* getShapeInfoArrow(s32 index) const;
+    const CollisionShapeInfoSphere* getShapeInfoSphere(s32 index) const;
+    const CollisionShapeInfoDisk* getShapeInfoDisk(s32 index) const;
+    u32 findShapeInfoIndex(const char* name) const;
+    const CollidedShapeResult* getCollidedShapeResult(s32 index) const;
+    const CollidedShapeResult* getCollidedShapeSupportResult(s32 index) const;
+
+private:
+    sead::Vector3f mBoundingCenter = {0.0f, 0.0f, 0.0f};
+    f32 mBoundingRadius = 0.0f;
+    sead::PtrArray<CollisionShapeInfoBase> mCollisionShape;
+    s32 mNumCollideResult = 0;
+    sead::PtrArray<CollidedShapeResult> mCollideShapeResult;
+    s32 mNumCollideSupportResult = 0;
+    sead::PtrArray<CollidedShapeResult> mCollideSupportResult;
+    f32 mCheckStepRange = 35.0f;
+    f32 _54 = 0.0f;
+    f32 _58 = 0.0f;
+    bool mHasShapeArrow = false;
+};
+
+static_assert(sizeof(CollisionShapeKeeper) == 0x60);
