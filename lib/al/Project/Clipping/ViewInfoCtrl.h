f2a0c1abd7df40cf54e40c890d1b7aef16aa8174
Muz-dev <113846204+Muz-dev@users.noreply.github.com>
2025-06-13 19:53:24 +0100

    Project/Clipping: Implement `ViewInfoCtrl` (#565)

--- lib/al/Project/Clipping/ViewInfoCtrl.h
+++ lib/al/Project/Clipping/ViewInfoCtrl.h
@@ -1,24 +1,46 @@
 #pragma once
 
+#include <basis/seadTypes.h>
+
 namespace al {
-class SceneCameraInfo;
-class PlayerHolder;
-class AreaObjGroup;
 class ClippingActorInfo;
+class ViewIdHolder;
+class PlayerHolder;
+class PlacementId;
+class AreaObjGroup;
+class SceneCameraInfo;
 
 class ViewInfoCtrl {
 public:
-    ViewInfoCtrl(const PlayerHolder*, const SceneCameraInfo*);
-
-    void initActorInfo(ClippingActorInfo*);
-    void initViewCtrlAreaGroup(const AreaObjGroup*);
+    ViewInfoCtrl(const PlayerHolder* playerHolder, const SceneCameraInfo* cameraInfo);
+    void initActorInfo(ClippingActorInfo* actorInfo);
+    void initViewCtrlAreaGroup(const AreaObjGroup* viewCtrlAreaGroup);
     bool update();
     void startCheckByCameraPos();
     void startCheckByLookAtPos();
     void startCheckByPlayerPos();
 
 private:
-    void* filler[0x7];
-};
+    enum class CheckType : s32 {
+        PlayerPos,
+        CameraPos,
+        LookAtPos,
+    };
 
+    struct ClippingPlacementId {
+        const PlacementId* parentId = nullptr;
+        bool isInViewCtrlArea = false;
+        bool _9 = false;
+    };
+
+    inline ClippingPlacementId* findClippingPlacementId_(const PlacementId& id) const;
+
+    const AreaObjGroup* mViewCtrlAreaGroup = nullptr;
+    s32 mClippingPlacementIdsSize = 0;
+    ClippingPlacementId** mClippingPlacementIds = nullptr;
+    bool mIsInvalid = false;
+    const PlayerHolder* mPlayerHolder = nullptr;
+    const SceneCameraInfo* mSceneCameraInfo = nullptr;
+    CheckType mCheckType = CheckType::PlayerPos;
+};
 }  // namespace al
