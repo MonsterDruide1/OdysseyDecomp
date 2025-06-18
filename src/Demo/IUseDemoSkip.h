73d83c367e0266898aab44335a13cce37571e270
ud2 <sjx233@qq.com>
2025-06-18 17:16:17 +0800

    System: Implement `GameDataFunction`

---
+++ src/Demo/IUseDemoSkip.h
@@ -0,0 +1,12 @@
+#pragma once
+
+class IUseDemoSkip {
+public:
+    virtual bool isFirstDemo() const = 0;
+
+    virtual bool isEnableSkipDemo() const = 0;
+
+    virtual void skipDemo() = 0;
+
+    virtual void updateOnlyDemoGraphics() {}
+};
