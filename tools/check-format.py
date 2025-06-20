56460b8324a4ecc4abadeef83a4a8ac23dd3583b
LynxDev2 <128722393+LynxDev2@users.noreply.github.com>
2025-06-20 18:11:53 +0300

    Fix offset variable lint

--- tools/check-format.py
+++ tools/check-format.py
@@ -457,7 +457,7 @@ def header_no_offset_comments(c, path):
 
 def header_lowercase_member_offset_vars(c, path):
     for line in c.splitlines():
-        if re.search(r"\s(field|gap|filler|pad)?_[0-9a-z]*[A-Z]", line):
+        if re.search(r"\s(field|gap|filler|pad|padding)?_[0-9a-z]*[A-Z]", line) and "," not in line and not line.endswith(");"):
             CHECK(lambda a: "#define" in a, line, "Characters in the names of offset variables need to be lowercase!", path)
 
 def header_bool_getter_name_prefix(c, path):
