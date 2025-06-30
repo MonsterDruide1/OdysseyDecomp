1b3e10d71b96e07b811fe68136df731a47a64ccb
LynxDev2 <128722393+LynxDev2@users.noreply.github.com>
2025-06-30 19:15:19 +0300

    PR: suggestions

--- .github/scripts/process-headers.py
+++ .github/scripts/process-headers.py
@@ -8,25 +8,28 @@ for root, _, files in os.walk(workdir):
     for file in files:
         file_path = os.path.join(root, file)
         # Only process .h and .hpp files
-        if ".h" not in str(file):
+        if not file.endswith(".h") and not file.endswith(".hpp"):
             continue
         with open(file_path, "r+") as file:
             lines = list(file)
-            for i, line in enumerate(lines):
-                if "private:" in line:
-                     lines[i] = line.replace("private:", "public:")
-                elif "protected:" in line:
-                    lines[i] = line.replace("protected:", "public:")
+            i = 0
+            while i < len(lines):
+                if "private:" in lines[i]:
+                     lines[i] = lines[i].replace("private:", "public:")
+                elif "protected:" in lines[i]:
+                    lines[i] = lines[i].replace("protected:", "public:")
                 if "al/" not in file_path and "game/" not in file_path:
+                    i += 1
                     continue
-                if "inline " in line and "(" in line and not "= default" in line:
+                if "inline " in lines[i] and "(" in lines[i] and not "= default" in lines[i]:
                     for j, line_inner in enumerate(lines[i:]):
                         line_inner_no_newline = line_inner.strip("\n")
                         if line_inner_no_newline.endswith("{") or line_inner_no_newline.endswith("}"):
                             break
                         if line_inner_no_newline.endswith(";"):
                             del lines[i:i+j+1]
-                            break;
+                            break
+                i += 1
             file.seek(0)
-            file.truncate(0)
             file.writelines(lines)
+            file.truncate()
