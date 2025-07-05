59b095b9c6dbd9aaf7820fec1dddefdde8c93f88
ExcellentGamer <excellentgamer417@gmail.com>
2025-07-05 13:51:08 -0400

    Implemented ExecuteSystemInitInfo

---
+++ lib/al/Project/Execute/ExecuteSystemInitInfo.cpp
@@ -0,0 +1,9 @@
+#include "Project/Execute/ExecuteSystemInitInfo.h"
+
+namespace al {
+
+ExecuteSystemInitInfo::ExecuteSystemInitInfo() : mData() {
+    mData = 0;
+}
+
+}  // namespace al
---
+++ lib/al/Project/Execute/ExecuteSystemInitInfo.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <math/seadVector.h>
+
+namespace al {
+
+class ExecuteSystemInitInfo {
+public:
+    ExecuteSystemInitInfo();
+
+private:
+    u64 mData;
+};
+
+}  // namespace al
