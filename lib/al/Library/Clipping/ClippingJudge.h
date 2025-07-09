9b6d24e38bd0259a61cf4868091cb254c1c265e0
Alex Larbe <alex.larbe@outlook.com>
2025-07-09 19:58:20 +0100

    Implemented: ClippingJudge

--- lib/al/Library/Clipping/ClippingJudge.h
+++ lib/al/Library/Clipping/ClippingJudge.h
@@ -6,6 +6,7 @@
 #include <math/seadVector.h>
 
 namespace al {
+class FrustumRadar;
 class SceneCameraInfo;
 class ClippingFarAreaObserver;
 
@@ -14,22 +15,27 @@ public:
     ClippingJudge(const ClippingFarAreaObserver*, const SceneCameraInfo*);
 
     void update();
-    bool isJudgedToClipFrustumUnUseFarLevel(const sead::Vector3f&, f32, f32) const;
-    bool isJudgeToClipFrustumCore(const sead::Vector3f&, f32, f32) const;
-    bool isJudgedToClipFrustum(const sead::Vector3f&, f32, f32, s32) const;
-    bool isJudgeToClipFrustumCore(const sead::Vector3f&, f32, f32, f32) const;
-    bool isInClipFrustum(const sead::Vector3f&, f32, f32, s32) const;
-    bool isInClipFrustumAllView(const sead::Vector3f&, f32, f32) const;
-    bool isJudgedToClipFrustumUnUseFarLevelObb(const sead::Matrix34f*, const sead::BoundBox3f&,
-                                               f32) const;
-    bool isJudgedToClipFrustumCoreObb(const sead::Matrix34f*, const sead::BoundBox3f&, f32) const;
-    bool isInClipFrustumObb(const sead::Matrix34f*, const sead::BoundBox3f&, f32, s32) const;
-    bool isJudgedToClipFrustumCoreObb(const sead::Matrix34f*, const sead::BoundBox3f&, f32,
-                                      f32) const;
-    bool isJudgedToClipFrustumObb(const sead::Matrix34f*, const sead::BoundBox3f&, f32, s32) const;
+    bool isJudgedToClipFrustumUnUseFarLevel(const sead::Vector3f& pos, f32 idx, f32 idy) const;
+    bool isJudgedToClipFrustumCore(const sead::Vector3f& pos, f32 idx, f32 idy) const;
+    bool isJudgedToClipFrustum(const sead::Vector3f& pos, f32 idx, f32 idy, s32 idz) const;
+    bool isJudgedToClipFrustumCore(const sead::Vector3f& pos, f32 idx, f32 idy, f32 idz) const;
+    bool isInClipFrustum(const sead::Vector3f& pos, f32 idx, f32 idy, s32 idz) const;
+    bool isInClipFrustumAllView(const sead::Vector3f& pos, f32 idx, f32 idy) const;
+    bool isJudgedToClipFrustumUnUseFarLevelObb(const sead::Matrix34f* mtx,
+                                               const sead::BoundBox3f& bound, f32 idx) const;
+    bool isJudgedToClipFrustumCoreObb(const sead::Matrix34f* mtx, const sead::BoundBox3f& bound,
+                                      f32 idx) const;
+    bool isInClipFrustumObb(const sead::Matrix34f* mtx, const sead::BoundBox3f& bound, f32 idx,
+                            s32 idy) const;
+    bool isJudgedToClipFrustumCoreObb(const sead::Matrix34f* mtx, const sead::BoundBox3f& bound,
+                                      f32 idx, f32 idy) const;
+    bool isJudgedToClipFrustumObb(const sead::Matrix34f* mtx, const sead::BoundBox3f& bound,
+                                  f32 idx, s32 idy) const;
 
 private:
-    void* filler[0x3];
+    const ClippingFarAreaObserver* mFarAreaObserver;
+    FrustumRadar** mFrustumRadars = nullptr;
+    const SceneCameraInfo* mCameraInfo;
 };
 
 }  // namespace al
