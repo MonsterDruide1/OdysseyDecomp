e14657e047daad8acfacce0f7f7d568280c737b1
ud2 <sjx233@qq.com>
2025-07-10 21:40:23 +0800

    System: Implement `GameDataFunction`

---
+++ src/Npc/SessionMusicianType.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <prim/seadEnum.h>
+
+SEAD_ENUM(SessionMusicianType
+    , Drum
+    , Bass
+    , Guitar
+    , Trumpet
+    , Vocal
+    , Saxophone
+    , Trombone
+)
---
+++ src/System/RankingCategory.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <prim/seadEnum.h>
+
+SEAD_ENUM(RankingCategory, JumpingRope, Volleyball)
