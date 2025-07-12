fab5ca8d24a5543afdb64882d0da076805d9fe66
Auggiethecat <noahthomassymonds@gmail.com>
2025-07-11 23:10:08 -0400

    Changed a couple types and fixed a weird whitespace issue.

--- lib/al/Project/Effect/EffectLight.h
+++ lib/al/Project/Effect/EffectLight.h
@@ -1,15 +1,15 @@
 #pragma once
 
 #include <gfx/seadColor.h>
-#include <math/seadMathCalcCommon.h>
+#include <math/seadVector.h>
 
 namespace al {
 class EffectLight {
 public:
-    void set(const sead::Mathi& pos, f32 intensity, const sead::Color4f& color, bool enabled);
+    void set(const sead::Vector3f& pos, f32 intensity, const sead::Color4f& color, bool enabled);
 
 private:
-    sead::Mathi mPosition;
+    sead::Vector3f mPosition;
     f32 mIntensity;
     sead::Color4f mColor;
     bool mIsEnabled;
