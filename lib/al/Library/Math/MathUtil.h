ecb322eb6c660f6863d43d7b5076dde6638dd19e
ud2 <sjx233@qq.com>
2025-07-21 04:42:53 +0800

    Npc: Implement `BigBirdStateWait` and `SmallBirdStateWait`

--- lib/al/Library/Math/MathUtil.h
+++ lib/al/Library/Math/MathUtil.h
@@ -171,7 +171,13 @@ f32 getRandomRadian();
 void getRandomVector(sead::Vector3f* vec, f32 factor);
 void getRandomDir(sead::Vector3f* vec);
 void getRandomDirH(sead::Vector3f*, const sead::Vector3f&);
-void rotateVectorDegree(sead::Vector3f*, const sead::Vector3f&, const sead::Vector3f&, f32);
+void rotateVectorDegree(sead::Vector3f* out, const sead::Vector3f& vec, const sead::Vector3f& axis,
+                        f32 degree);
+
+inline void rotateVectorDegree(sead::Vector3f* vec, const sead::Vector3f& axis, f32 degree) {
+    rotateVectorDegree(vec, *vec, axis, degree);
+}
+
 void getRandomOnCircle(sead::Vector2f*, f32);
 void getRandomInCircle(sead::Vector2f*, f32);
 void getRandomInCircleMinMaxRadius(sead::Vector2f*, f32, f32);
@@ -339,8 +345,20 @@ bool turnQuatFrontToDirDegreeH(sead::Quatf*, const sead::Vector3f&, f32);
 void rotateQuatAndTransDegree(sead::Quatf*, sead::Vector3f*, const sead::Quatf&,
                               const sead::Vector3f&, const sead::Vector3f&, const sead::Vector3f&,
                               f32);
-bool turnVecToVecDegree(sead::Vector3f*, const sead::Vector3f&, const sead::Vector3f&, f32);
-void turnVecToVecRate(sead::Vector3f*, const sead::Vector3f&, const sead::Vector3f&, f32);
+bool turnVecToVecDegree(sead::Vector3f* out, const sead::Vector3f& vec, const sead::Vector3f& other,
+                        f32 degree);
+
+inline void turnVecToVecDegree(sead::Vector3f* vec, const sead::Vector3f& other, f32 degree) {
+    turnVecToVecDegree(vec, *vec, other, degree);
+}
+
+void turnVecToVecRate(sead::Vector3f* out, const sead::Vector3f& vec, const sead::Vector3f& other,
+                      f32 rate);
+
+inline void turnVecToVecRate(sead::Vector3f* vec, const sead::Vector3f& other, f32 rate) {
+    turnVecToVecRate(vec, *vec, other, rate);
+}
+
 bool turnVecToVecCos(sead::Vector3f*, const sead::Vector3f&, const sead::Vector3f&, f32,
                      const sead::Vector3f&, f32);
 bool turnVecToVecCosOnPlane(sead::Vector3f*, const sead::Vector3f&, const sead::Vector3f&,
