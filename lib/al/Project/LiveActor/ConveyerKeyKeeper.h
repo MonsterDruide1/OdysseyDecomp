ddc3f59b748ce5e96e4282391f16de9101dd3743
Naii-the-Baf <sfabian200@gmail.com>
2025-07-21 03:01:24 -0600

    Project/LiveActor: Implement `ConveyerKeyKeeper` (#680)

--- lib/al/Project/LiveActor/ConveyerKeyKeeper.h
+++ lib/al/Project/LiveActor/ConveyerKeyKeeper.h
@@ -7,10 +7,16 @@ namespace al {
 struct ActorInitInfo;
 class PlacementInfo;
 
-// TODO: I'm not sure about this
 struct ConveyerKey {
-    void* _0[5];
-    const PlacementInfo& placementInfo;
+    f32 moveDistance;
+    f32 totalMoveDistance;
+    sead::Quatf quat;
+    sead::Vector3f moveDistanceVertical;
+    s32 interpolateType;
+    const PlacementInfo* placementInfo;
+
+    void init(const sead::Vector3f& keeperTrans, const sead::Vector3f& keeperDir,
+              const PlacementInfo& placement);
 };
 
 class ConveyerKeyKeeper {
@@ -21,18 +27,21 @@ public:
     void calcPosAndQuat(sead::Vector3f* pos, sead::Quatf* quat, s32* index, f32 coord) const;
     void calcPosAndQuatByKeyIndex(sead::Vector3f* pos, sead::Quatf* quat, s32 index) const;
     void calcClippingSphere(sead::Vector3f* clippingTrans, f32* clippingRadius, f32 offset) const;
-    const ConveyerKey* getConveyerKey(s32 index) const;  // return type depends on the type of _0
+    const ConveyerKey& getConveyerKey(s32 index) const;
 
     s32 getConveyerKeyCount() const { return mConveyerKeyCount; }
 
     f32 getTotalMoveDistance() const { return mTotalMoveDistance; }
 
 private:
-    ConveyerKey* mConveyerKeys;  // array of a struct/class with a size of 0x30?
-    s32 mConveyerKeyCount;
-    sead::Quatf mQuat;
-    sead::Vector3f mTrans;
-    sead::Vector3f mMoveDirection;
-    f32 mTotalMoveDistance;
+    ConveyerKey* mConveyerKeys = nullptr;
+    s32 mConveyerKeyCount = 0;
+    sead::Quatf mQuat = sead::Quatf::unit;
+    sead::Vector3f mTrans = sead::Vector3f::zero;
+    sead::Vector3f mMoveDirection = sead::Vector3f::ez;
+    f32 mTotalMoveDistance = 0.0f;
 };
+
+static_assert(sizeof(ConveyerKeyKeeper) == 0x38);
+
 }  // namespace al
