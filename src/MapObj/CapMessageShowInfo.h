ac75c6f565bb520bfc891eb8cd1b2c617b17805d
Naii-the-Baf <sfabian200@gmail.com>
2025-08-26 10:55:49 -0600

    Npc: Implement `VolleyballNpc`

--- src/MapObj/CapMessageShowInfo.h
+++ src/MapObj/CapMessageShowInfo.h
@@ -1,9 +1,13 @@
 #pragma once
 
+#include <basis/seadTypes.h>
+
 namespace al {
 class IUseSceneObjHolder;
 }  // namespace al
 
 namespace rs {
+void showCapMessage(const al::IUseSceneObjHolder*, const char*, s32, s32);
+bool isActiveCapMessage(const al::IUseSceneObjHolder*);
 bool tryShowCapMsgCollectCoinGetFirst(const al::IUseSceneObjHolder*);
 }  // namespace rs
