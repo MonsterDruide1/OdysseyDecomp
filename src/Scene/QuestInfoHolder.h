4a8cf5b8c4351ee8449ae2b6cc7574bbc9c856c5
ud2 <sjx233@qq.com>
2025-08-26 23:23:23 +0800

    fix return types

--- src/Scene/QuestInfoHolder.h
+++ src/Scene/QuestInfoHolder.h
@@ -3,6 +3,8 @@
 #include <basis/seadTypes.h>
 #include <container/seadPtrArray.h>
 
+#include "Library/Base/StringUtil.h"
+
 namespace al {
 class IUseSceneObjHolder;
 class LiveActor;
@@ -10,22 +12,23 @@ class PlacementInfo;
 struct ActorInitInfo;
 }  // namespace al
 class QuestInfo;
+class QuestInfoHolder;
 
 namespace rs {
-void getQuestInfoHolder(const al::IUseSceneObjHolder*);
-void tryCreateAndRegisterQuestInfoToHolder(const al::LiveActor*, const al::ActorInitInfo&);
-void createAndRegisterQuestInfoToHolderFromLinkedObj(const al::LiveActor*, const al::PlacementInfo&,
-                                                     bool);
+QuestInfoHolder* getQuestInfoHolder(const al::IUseSceneObjHolder*);
+QuestInfo* tryCreateAndRegisterQuestInfoToHolder(const al::LiveActor*, const al::ActorInitInfo&);
+QuestInfo* createAndRegisterQuestInfoToHolderFromLinkedObj(const al::LiveActor*,
+                                                           const al::PlacementInfo&, bool);
 void invalidateQuest(const QuestInfo*);
-void tryCreateAndRegisterQuestInfoToHolderFromLinkedObj(const al::LiveActor*,
-                                                        const al::ActorInitInfo&);
+sead::PtrArray<QuestInfo>*
+tryCreateAndRegisterQuestInfoToHolderFromLinkedObj(const al::LiveActor*, const al::ActorInitInfo&);
 void validateQuest(const QuestInfo*);
 const QuestInfo* const* getActiveQuestList(const al::IUseSceneObjHolder*);
 s32 getActiveQuestNum(const al::IUseSceneObjHolder*);
 s32 getActiveQuestNumForMap(const al::IUseSceneObjHolder*);
 bool isActiveQuestAllEqualNo(const al::IUseSceneObjHolder*);
-const char* getActiveQuestLabel(const al::IUseSceneObjHolder*);
-const char* getActiveQuestStageName(const al::IUseSceneObjHolder*);
+al::StringTmp<128> getActiveQuestLabel(const al::IUseSceneObjHolder*);
+al::StringTmp<128> getActiveQuestStageName(const al::IUseSceneObjHolder*);
 bool isActiveQuest(const QuestInfo*);
 bool isActiveQuest(const sead::PtrArray<QuestInfo>*);
 s32 getActiveQuestNo(const al::IUseSceneObjHolder*);
