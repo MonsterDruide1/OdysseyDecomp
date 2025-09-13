330a05ba8c132e5413255513acd1a261381553b6
Codeman4033 <92231243+codeman4033@users.noreply.github.com>
2025-09-13 00:59:35 -0500

    Implement Util/SpecialBuildUtil.o

--- src/Util/SpecialBuildUtil.cpp
+++ src/Util/SpecialBuildUtil.cpp
@@ -4,24 +4,31 @@ namespace rs {
 bool isModeE3Rom() {
     return false;
 }
+
 bool isModeE3MovieRom() {
     return false;
 }
+
 bool isModeE3LiveRom() {
     return false;
 }
+
 bool isModeDiverRom() {
     return false;
 }
+
 bool isModeJungleGymRom() {
     return false;
 }
+
 bool isModeDiverOrJungleGymRom() {
     return false;
 }
+
 bool isModeMovieRom() {
     return false;
 }
+
 bool isModeEpdMovieRom() {
     return false;
 }
