31e5aa9144aca6ef6338ed79af2454bd651895b9
Narr the Reg <5944268+german77@users.noreply.github.com>
2025-09-23 18:33:32 -0600

    Library/Collision: Implement `PartsMtxConnector` (#742)

--- lib/al/Library/Collision/PartsMtxConnector.h
+++ lib/al/Library/Collision/PartsMtxConnector.h
@@ -14,28 +14,30 @@ public:
     virtual bool isConnecting() const;
     virtual void clear();
 
-    void init(const sead::Matrix34f*, const sead::Matrix34f&) const;
-    void init(const sead::Matrix34f*) const;
-    void multVec(sead::Vector3f*, const sead::Vector3f&) const;
-    void multTrans(sead::Vector3f*, const sead::Vector3f&) const;
-    void multMtx(sead::Matrix34f*, const sead::Matrix34f&) const;
-    void multQT(sead::Quatf*, sead::Vector3f*, sead::Vector3f*) const;
-    void multQT(sead::Quatf*, sead::Vector3f*, sead::Vector3f*, const sead::Quatf&,
-                const sead::Vector3f&) const;
-    void multQT(sead::Quatf*, sead::Vector3f*, const sead::Quatf&, const sead::Vector3f&) const;
+    void init(const sead::Matrix34f* parentMtx, const sead::Matrix34f& mtx);
+    void init(const sead::Matrix34f* parentMtx);
+    void multVec(sead::Vector3f* outVec, const sead::Vector3f& vec) const;
+    void multTrans(sead::Vector3f* outVec, const sead::Vector3f& vec) const;
+    void multMtx(sead::Matrix34f* outMtx, const sead::Matrix34f& mtx) const;
+    void multQT(sead::Quatf* outQuat, sead::Vector3f* outPos, sead::Vector3f* outScale) const;
+    void multQT(sead::Quatf* outQuat, sead::Vector3f* outPos, sead::Vector3f* outScale,
+                const sead::Quatf& baseQuat, const sead::Vector3f& baseTrans) const;
+    void multQT(sead::Quatf* outQuat, sead::Vector3f* outPos, const sead::Quatf& baseQuat,
+                const sead::Vector3f& baseTrans) const;
     const sead::Quatf& getBaseQuat() const;
     const sead::Vector3f& getBaseTrans() const;
-    void setBaseQuatTrans(const sead::Quatf&, const sead::Vector3f&);
-    void calcConnectInfo(sead::Vector3f*, sead::Quatf*, sead::Vector3f*, const sead::Vector3f&,
-                         const sead::Vector3f&) const;
-    void calcMtxWithOffset(sead::Matrix34f*, const sead::Vector3f&, const sead::Vector3f&) const;
-    bool tryGetParentTrans(sead::Vector3f*) const;
+    void setBaseQuatTrans(const sead::Quatf& quat, const sead::Vector3f& trans);
+    void calcConnectInfo(sead::Vector3f* outPos, sead::Quatf* outQuat, sead::Vector3f* outScale,
+                         const sead::Vector3f& trans, const sead::Vector3f& rotation) const;
+    void calcMtxWithOffset(sead::Matrix34f* outMtx, const sead::Vector3f& trans,
+                           const sead::Vector3f& rotation) const;
+    bool tryGetParentTrans(sead::Vector3f* outTrans) const;
 
 private:
-    sead::Matrix34f _8;
-    const sead::Matrix34f* _38;
-    sead::Quatf _40;
-    sead::Vector3f _50;
+    sead::Matrix34f mMtx = sead::Matrix34f::ident;
+    const sead::Matrix34f* mParentMtx = nullptr;
+    sead::Quatf mBaseQuat = sead::Quatf::unit;
+    sead::Vector3f mBaseTrans = sead::Vector3f::zero;
 };
 
 static_assert(sizeof(MtxConnector) == 0x60);
