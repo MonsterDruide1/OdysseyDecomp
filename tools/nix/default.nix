46a2ca5511c438d056b3bc0340678f7a7c0f1f01
Aubrey Taylor <aubrey@hall.ly>
2025-10-26 14:16:01 -0600

    rec

--- tools/nix/default.nix
+++ tools/nix/default.nix
@@ -1,19 +1,19 @@
 pkgs:
 let
-  artifacts = (builtins.fromJSON (builtins.readFile ./artifacts.json));
+  artifactDefs = (builtins.fromJSON (builtins.readFile ./artifacts.json));
   cache = (builtins.fromJSON (builtins.readFile ../cache-version.json));
   urlPrefix = cache.urlPrefix;
   version = cache.version;
 in
-{
+rec {
   cacheUrl = "${urlPrefix}/${version}";
   artifacts = builtins.mapAttrs (
     key:
     { filename, hash }:
     pkgs.callPackage ./tools.nix {
-      url = "${urlPrefix}/${version}/${filename}";
+      url = "${cacheUrl}/${filename}";
       sha256 = hash;
       inherit version;
     }
-  ) artifacts;
+  ) artifactDefs;
 }
