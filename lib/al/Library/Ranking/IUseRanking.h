419ee7cb0c0a108c7297b6bcd2dd01ae71dd5bb8
Naii-the-Baf <sfabian200@gmail.com>
2025-11-03 10:04:27 -0600

    Address review

--- lib/al/Library/Ranking/IUseRanking.h
+++ lib/al/Library/Ranking/IUseRanking.h
@@ -5,6 +5,8 @@ class RankingDirector;
 
 class IUseRanking {
 public:
+    virtual ~IUseRanking();
+
     virtual RankingDirector* getRankingDirector() const;
     virtual bool isAvailableRanking() const;
 };
