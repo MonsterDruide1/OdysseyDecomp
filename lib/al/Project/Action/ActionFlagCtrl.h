5539b5a52c29967bfab185e47f536e3d13dcb688
Narr the Reg <5944268+german77@users.noreply.github.com>
2025-11-22 08:05:55 -0600

    Project/Action: Implement `ActionFlagCtrl` (#783)

--- lib/al/Project/Action/ActionFlagCtrl.h
+++ lib/al/Project/Action/ActionFlagCtrl.h
@@ -9,11 +9,32 @@ struct ActionAnimDataInfo;
 class HitSensorKeeper;
 class LiveActor;
 
-struct ActionFlagCtrlInfo {
-    const char* name;
-    void* _8;
-    void* _10;
+enum class CtrlFlag : s16 {
+    NotFound = 0,
+    ValidOff = 2,
+    ValidOn = 3,
 };
+
+struct ActionSensorCtrlInfo {
+    ActionSensorCtrlInfo();
+
+    const char* name = nullptr;
+    CtrlFlag state = CtrlFlag::NotFound;
+    s16 startFrame = -1;
+    s16 endFrame = -1;
+};
+
+static_assert(sizeof(ActionSensorCtrlInfo) == 0x10);
+
+struct ActionFlagCtrlInfo {
+    ActionFlagCtrlInfo();
+
+    const char* actionName = nullptr;
+    CtrlFlag* ctrlFlags = nullptr;
+    ActionSensorCtrlInfo* sensorCtrlInfoArray = nullptr;
+};
+
+static_assert(sizeof(ActionFlagCtrlInfo) == 0x18);
 
 class ActionFlagCtrl {
 public:
@@ -21,23 +42,25 @@ public:
 
     ActionFlagCtrl(LiveActor* actor, const char* name);
 
-    ActionFlagCtrlInfo* findFlagInfo(const char* name) const;
     void initPost();
     void start(const char* name);
+    ActionFlagCtrlInfo* findFlagInfo(const char* name) const;
     void startCtrlFlag();
     void startCtrlSensor();
     void update(f32 frame, f32 frameRateMax, f32 frameRate, bool isStop);
-    void updateCtrlSensor(f32, f32, f32, bool);
-    bool isFlagValidOn(s32, bool);
-    bool isFlagValidOff(s32, bool);
+    void updateCtrlSensor(f32 frame, f32 frameRateMax, f32 frameRate, bool isStop);
+    bool isFlagValidOn(s32 index, bool isEnabled) const;
+    bool isFlagValidOff(s32 index, bool isEnabled) const;
 
 private:
     LiveActor* mParentActor;
-    const char* mName;
+    const char* mArchiveName;
     HitSensorKeeper* mHitSensorKeeper = nullptr;
     s32 mInfoCount = 0;
-    ActionFlagCtrlInfo** mInfos = nullptr;
-    ActionFlagCtrlInfo* mLastFlag = nullptr;
-    bool mIsBool = false;
+    ActionFlagCtrlInfo** mCtrlInfoArray = nullptr;
+    ActionFlagCtrlInfo* mCurrentCtrlInfo = nullptr;
+    bool mIsUpdateNeeded = false;
 };
+
+static_assert(sizeof(ActionFlagCtrl) == 0x38);
 }  // namespace al
