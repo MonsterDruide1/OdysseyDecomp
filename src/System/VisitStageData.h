f9b5f3200fcc489a6ae070c04f79efba759e61bc
ud2 <sjx233@qq.com>
2025-12-21 00:32:16 +0800

    System: Implement `GameDataFunction`

--- src/System/VisitStageData.h
+++ src/System/VisitStageData.h
@@ -15,8 +15,8 @@ public:
     void init();
     bool checkAlreadyVisit(const char*) const;
     void visit(const char*);
-    void write(al::ByamlWriter*);
-    void read(const al::ByamlIter&);
+    void write(al::ByamlWriter*) override;
+    void read(const al::ByamlIter&) override;
 
 private:
     void* _8;
