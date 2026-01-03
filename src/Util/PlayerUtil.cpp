d89cdc4d0a9ea1ce6ca766acecd023feb67ea4bc
emvivre <emvivre@urdn.com.ua>
2026-01-03 10:04:56 +0100

    src/Util/PlayerUtil.cpp: fix header ordering

--- src/Util/PlayerUtil.cpp
+++ src/Util/PlayerUtil.cpp
@@ -1,7 +1,7 @@
 #include "Util/PlayerUtil.h"
 
-#include "Library/Player/PlayerUtil.h"
 #include "Library/LiveActor/ActorPoseUtil.h"
+#include "Library/Player/PlayerUtil.h"
 
 const sead::Vector3f& rs::getPlayerPos(const al::LiveActor* actor) {
     const al::LiveActor* player = al::getPlayerActor(actor, 0);
