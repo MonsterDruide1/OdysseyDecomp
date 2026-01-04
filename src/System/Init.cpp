67c90f098395e87617b31e7ae510e765e0452077
MonsterDruide1 <5958456@gmail.com>
2026-01-04 12:04:26 +0100

    downgrade-patch

--- src/System/Init.cpp
+++ src/System/Init.cpp
@@ -10,11 +10,11 @@ extern "C" void nninitStartup() {
     uintptr_t allocatorHeap;
     uintptr_t recordingHeap;
 
-    nn::os::SetMemoryHeapSize(0xc8000000);
+    nn::os::SetMemoryHeapSize(0xc3000000);
     nn::os::AllocateMemoryBlock(&allocatorHeap, 0x2400000);
     nn::init::InitializeAllocator(reinterpret_cast<void*>(allocatorHeap), 0x2400000);
-    nn::os::AllocateMemoryBlock(&recordingHeap, 0x6000000);
-    nn::oe::EnableGamePlayRecording(reinterpret_cast<void*>(recordingHeap), 0x6000000);
+    //nn::os::AllocateMemoryBlock(&recordingHeap, 0x6000000);
+    //nn::oe::EnableGamePlayRecording(reinterpret_cast<void*>(recordingHeap), 0x6000000);
 }
 
 extern "C" void nnMain() {
