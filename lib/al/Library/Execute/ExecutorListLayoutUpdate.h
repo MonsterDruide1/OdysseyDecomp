413dae1545c7065d8c8b93e8570c9076a7b0a8d8
Narr the Reg <5944268+german77@users.noreply.github.com>
2026-01-06 05:55:41 -0600

    Library/Execute: Implement `ExecutorList` variants (#848)

---
+++ lib/al/Library/Execute/ExecutorListLayoutUpdate.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <basis/seadTypes.h>
+
+#include "Library/Execute/ExecutorListBase.h"
+
+namespace agl {
+class DrawContext;
+}
+
+namespace al {
+struct ExecuteSystemInitInfo;
+class LayoutActor;
+
+class ExecutorListLayoutUpdate : public ExecutorListBase {
+public:
+    ExecutorListLayoutUpdate(const char* name, s32 size);
+
+    void registerLayout(LayoutActor* layout);
+    void executeList() const override;
+
+    bool isActive() const override { return mSize > 0; }
+
+private:
+    s32 mCapacity;
+    s32 mSize = 0;
+    LayoutActor** mList = nullptr;
+};
+
+static_assert(sizeof(ExecutorListLayoutUpdate) == 0x20);
+
+}  // namespace al
