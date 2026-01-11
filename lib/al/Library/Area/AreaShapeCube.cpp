9e12ae80c0308c06ebf29f94e8a9d5b2eab103db
LynxDev2 <128722393+LynxDev2@users.noreply.github.com>
2026-01-11 12:54:55 +0200

    PR: Suggestion

--- lib/al/Library/Area/AreaShapeCube.cpp
+++ lib/al/Library/Area/AreaShapeCube.cpp
@@ -11,15 +11,31 @@ bool AreaShapeCube::isInVolume(const sead::Vector3f& pos) const {
     return isInLocalVolume(localPos);
 }
 
-bool AreaShapeCube::isInLocalVolume(const sead::Vector3f& pos) const {
-    f32 bottom = mOriginType == OriginType::Base ?
-                     0.0f :
-                     (mOriginType == OriginType::Top ? -1000.0f : -500.0f);
-    f32 top = mOriginType == OriginType::Base ? 1000.0f :
-                                                (mOriginType == OriginType::Top ? 0.0f : 500.0f);
+inline f32 getBottom(AreaShapeCube::OriginType originType) {
+    switch (originType) {
+    case AreaShapeCube::OriginType::Base:
+        return 0.0f;
+    case AreaShapeCube::OriginType::Top:
+        return -1000.0f;
+    default:  // AreaShapeCube::OriginType::Center
+        return -500.0f;
+    }
+}
 
-    sead::Vector3f min = {-500.0f, bottom, -500.0f};
-    sead::Vector3f max = {500.0f, top, 500.0f};
+inline f32 getTop(AreaShapeCube::OriginType originType) {
+    switch (originType) {
+    case AreaShapeCube::OriginType::Base:
+        return 1000.0f;
+    case AreaShapeCube::OriginType::Top:
+        return 0.0f;
+    default:  // AreaShapeCube::OriginType::Center
+        return 500.0f;
+    }
+}
+
+bool AreaShapeCube::isInLocalVolume(const sead::Vector3f& pos) const {
+    sead::Vector3f min = {-500.0f, getBottom(mOriginType), -500.0f};
+    sead::Vector3f max = {500.0f, getTop(mOriginType), 500.0f};
 
     if ((pos.y < min.y || max.y < pos.y) || (pos.x < min.x || max.x < pos.x) ||
         (pos.z < min.z || max.z < pos.z))
