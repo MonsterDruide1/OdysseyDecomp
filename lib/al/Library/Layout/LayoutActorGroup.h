63051c884f6da10a986c945803e5151e0d2f5a5e
GRAnimated <devin@nhscan.com>
2026-01-18 16:13:49 -0500

    Fixes

--- lib/al/Library/Layout/LayoutActorGroup.h
+++ lib/al/Library/Layout/LayoutActorGroup.h
@@ -1,7 +1,29 @@
 #pragma once
 
+#include <basis/seadTypes.h>
+
 namespace al {
 class LayoutActor;
 
-class LayoutActorGroup {};
+class LayoutActorGroup {
+public:
+    LayoutActorGroup(const char*, s32);
+    void registerActor(al::LayoutActor*);
+    al::LayoutActor* findDeadActor() const;
+    al::LayoutActor* tryFindDeadActor() const;
+
+    const char* getGroupName() const { return mGroupName; }
+
+    s32 getMaxActorCount() const { return mMaxActorCount; }
+
+    s32 getActorCount() const { return mActorCount; }
+
+    LayoutActor* getActor(s32 idx) const { return mActors[idx]; }
+
+public:
+    const char* mGroupName;
+    s32 mMaxActorCount;
+    s32 mActorCount;
+    LayoutActor** mActors;
+};
 }  // namespace al
