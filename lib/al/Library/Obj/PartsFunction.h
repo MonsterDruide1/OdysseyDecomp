38f32a50940939a07a3b264b68ce1e08f53d30ea
Naii-the-Baf <sfabian200@gmail.com>
2026-01-20 19:45:37 -0600

    Library/Obj: Implement `PartsFunction`

--- lib/al/Library/Obj/PartsFunction.h
+++ lib/al/Library/Obj/PartsFunction.h
@@ -14,36 +14,42 @@ class KeyMoveCameraObj;
 
 CollisionObj* createCollisionObj(const LiveActor* parent, const ActorInitInfo& info,
                                  const char* collisionFileName, HitSensor* hitSensor,
-                                 const char* joinMtxName, const char* suffix);
+                                 const char* jointMtxName, const char* suffix);
 CollisionObj* createCollisionObjMtx(const LiveActor* parent, const ActorInitInfo& info,
                                     const char* collisionFileName, HitSensor* hitSensor,
-                                    const sead::Matrix34f* joinMtx, const char* suffix);
+                                    const sead::Matrix34f* jointMtx, const char* suffix);
 
-PartsModel* createPartsModel(LiveActor*, const ActorInitInfo&, const char*, const char*,
-                             const sead::Matrix34f*);
-PartsModel* createPartsModelFile(LiveActor*, const ActorInitInfo&, const char*, const char*,
-                                 const char*);
-PartsModel* createPartsModelFileSuffix(LiveActor*, const ActorInitInfo&, const char*, const char*,
-                                       const char*, const char*);
-PartsModel* createSimplePartsModel(LiveActor*, const ActorInitInfo&, const char*, const char*,
-                                   const char*);
-PartsModel* createSimplePartsModelSuffix(LiveActor*, const ActorInitInfo&, const char*, const char*,
-                                         const char*, const char*);
-PartsModel* createPartsModelSuffix(LiveActor*, const ActorInitInfo&, const char*, const char*,
-                                   const char*, const sead::Matrix34f*);
-PartsModel* createPartsModelJoint(LiveActor*, const ActorInitInfo&, const char*, const char*,
-                                  const char*);
-PartsModel* createPartsModelSuffixJoint(LiveActor*, const ActorInitInfo&, const char*, const char*,
-                                        const char*, const char*);
-void appearBreakModelRandomRotateY(LiveActor*);
-bool updateSyncHostVisible(bool*, LiveActor*, const LiveActor*, bool);
+PartsModel* createPartsModel(LiveActor* parent, const ActorInitInfo& info, const char* name,
+                             const char* arcName, const sead::Matrix34f* jointMtx);
+PartsModel* createPartsModelFile(LiveActor* parent, const ActorInitInfo& info, const char* name,
+                                 const char* arcName, const char* suffix);
+PartsModel* createPartsModelFileSuffix(LiveActor* parent, const ActorInitInfo& info,
+                                       const char* name, const char* arcName, const char* arcSuffix,
+                                       const char* suffix);
+PartsModel* createSimplePartsModel(LiveActor* parent, const ActorInitInfo& info, const char* name,
+                                   const char* arcName, const char* suffix);
+PartsModel* createSimplePartsModelSuffix(LiveActor* parent, const ActorInitInfo& info,
+                                         const char* name, const char* arcName,
+                                         const char* arcSuffix, const char* suffix);
+PartsModel* createPartsModelSuffix(LiveActor* parent, const ActorInitInfo& info, const char* name,
+                                   const char* arcName, const char* suffix,
+                                   const sead::Matrix34f* jointMtx);
+PartsModel* createPartsModelJoint(LiveActor* parent, const ActorInitInfo& info, const char* name,
+                                  const char* arcName, const char* jointMtxName);
+PartsModel* createPartsModelSuffixJoint(LiveActor* parent, const ActorInitInfo& info,
+                                        const char* name, const char* arcName,
+                                        const char* arcSuffix, const char* jointMtxName);
+void appearBreakModelRandomRotateY(LiveActor* actor);
+bool updateSyncHostVisible(bool* isChildHidden, LiveActor* actor, const LiveActor* host, bool unk);
 bool isTraceModelRandomRotate(const LiveActor* actor);
-BreakModel* createBreakModel(const LiveActor*, const ActorInitInfo&, const char*, const char*,
-                             const char*, const sead::Matrix34f*, const char*);
-HostSyncTowerCamera* createHostSyncTowerCamera(LiveActor*, const ActorInitInfo&);
-void activateHostSyncTowerCamera(HostSyncTowerCamera*);
-void deactivateHostSyncTowerCamera(HostSyncTowerCamera*);
-void updateHostSyncTowerCamera(HostSyncTowerCamera*);
-KeyMoveCameraObj* tryCreateLinksKeyMoveCameraObj(const ActorInitInfo&, const char*);
-KeyMoveCameraObj* tryCreateLinksKeyMoveCameraObjWithInterpole(const ActorInitInfo&, const char*);
+BreakModel* createBreakModel(const LiveActor* parent, const ActorInitInfo& info,
+                             const char* objName, const char* modelName, const char* fileSuffixName,
+                             const sead::Matrix34f* rootMtx, const char* breakActionName);
+HostSyncTowerCamera* createHostSyncTowerCamera(LiveActor* actor, const ActorInitInfo& info);
+void activateHostSyncTowerCamera(HostSyncTowerCamera* camera);
+void deactivateHostSyncTowerCamera(HostSyncTowerCamera* camera);
+void updateHostSyncTowerCamera(HostSyncTowerCamera* camera);
+KeyMoveCameraObj* tryCreateLinksKeyMoveCameraObj(const ActorInitInfo& info, const char* childName);
+KeyMoveCameraObj* tryCreateLinksKeyMoveCameraObjWithInterpole(const ActorInitInfo& info,
+                                                              const char* childName);
 }  // namespace al
