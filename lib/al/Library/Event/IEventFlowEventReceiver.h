5f85077e5341dcd47c979442ae7b5fa094ab2828
MonsterDruide1 <5958456@gmail.com>
2026-02-13 22:06:02 +0100

    Library/Event: Make `IEventFlowEventReceiver` abstract

--- lib/al/Library/Event/IEventFlowEventReceiver.h
+++ lib/al/Library/Event/IEventFlowEventReceiver.h
@@ -5,7 +5,7 @@ class EventFlowEventData;
 
 class IEventFlowEventReceiver {
 public:
-    virtual bool receiveEvent(const EventFlowEventData* event);
+    virtual bool receiveEvent(const EventFlowEventData* event) = 0;
 };
 
 }  // namespace al
