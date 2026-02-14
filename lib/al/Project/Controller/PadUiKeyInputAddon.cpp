e592fdb91d2535cecee5320aaa72d84851a3c1f8
tetraxile <tetraxile@proton.me>
2026-02-14 13:22:07 +0100

    Project/Controller: implement PadUiKeyInputAddon

---
+++ lib/al/Project/Controller/PadUiKeyInputAddon.cpp
@@ -0,0 +1,55 @@
+#include "Project/Controller/PadUiKeyInputAddon.h"
+
+#include <controller/seadController.h>
+#include <controller/seadControllerBase.h>
+#include <controller/seadControllerDefine.h>
+#include <controller/seadControllerMgr.h>
+#include <prim/seadBitFlag.h>
+
+#include "Library/Controller/PadReplayFunction.h"
+#include "Library/Controller/ReplayController.h"
+
+namespace al {
+
+PadUiKeyInputAddon::PadUiKeyInputAddon(sead::Controller* controller)
+    : sead::ControllerAddon(controller) {
+    mId = sead::ControllerDefine::AddonId::cAddon_UiKeyInput;
+};
+
+PadUiKeyInputAddon::~PadUiKeyInputAddon() {}
+
+bool PadUiKeyInputAddon::calc() {
+    s32 port = mController->getMgr()->findControllerPort(mController);
+    sead::ControllerBase* controller;
+    if (isValidReplayController(port))
+        controller = getReplayController(port);
+    else
+        controller = mController;
+
+    u32 prevPadHold = mPadHold;
+
+    u32 maskUp = (1 << sead::Controller::cPadIdx_Up) | (1 << sead::Controller::cPadIdx_LeftStickUp);
+    u32 maskDown =
+        (1 << sead::Controller::cPadIdx_Down) | (1 << sead::Controller::cPadIdx_LeftStickDown);
+    u32 maskLeft =
+        (1 << sead::Controller::cPadIdx_Left) | (1 << sead::Controller::cPadIdx_LeftStickLeft);
+    u32 maskRight =
+        (1 << sead::Controller::cPadIdx_Right) | (1 << sead::Controller::cPadIdx_LeftStickRight);
+
+    mPadHold.changeBit(0, controller->isHold(maskUp));
+    mPadHold.changeBit(1, controller->isHold(maskDown));
+    mPadHold.changeBit(2, controller->isHold(maskLeft));
+    mPadHold.changeBit(3, controller->isHold(maskRight));
+
+    mPadTrig.setDirect(mPadHold & ~prevPadHold);
+    mPadHoldAndPrev.setDirect(mPadHold & prevPadHold);
+
+    mPadRepeat.changeBit(0, controller->isTrigWithRepeat(maskUp));
+    mPadRepeat.changeBit(1, controller->isTrigWithRepeat(maskDown));
+    mPadRepeat.changeBit(2, controller->isTrigWithRepeat(maskLeft));
+    mPadRepeat.changeBit(3, controller->isTrigWithRepeat(maskRight));
+
+    return mPadHold == 0;
+}
+
+}  // namespace al
---
+++ lib/al/Project/Controller/PadUiKeyInputAddon.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <controller/seadControllerAddon.h>
+#include <prim/seadBitFlag.h>
+
+namespace al {
+
+class PadUiKeyInputAddon : public sead::ControllerAddon {
+    SEAD_RTTI_OVERRIDE(PadUiKeyInputAddon, sead::ControllerAddon)
+
+public:
+    PadUiKeyInputAddon(sead::Controller* controller);
+    ~PadUiKeyInputAddon();
+
+    bool calc() override;
+
+private:
+    sead::BitFlag32 mPadTrig = 0;
+    sead::BitFlag32 mPadHold = 0;
+    sead::BitFlag32 mPadHoldAndPrev = 0;
+    sead::BitFlag32 mPadRepeat = 0;
+};
+
+static_assert(sizeof(PadUiKeyInputAddon) == 0x38);
+
+}  // namespace al
