82ee990c1af943f8285d60f80020d94cd724f0c7
guymakinggames <66076221+guymakinggames@users.noreply.github.com>
2026-02-21 09:14:26 +0000

    Area: Implement BirdGatheringSpotArea

---
+++ src/Area/BirdGatheringSpotArea.cpp
@@ -0,0 +1,53 @@
+#include "Area/BirdGatheringSpotArea.h"
+
+#include "Library/Area/AreaInitInfo.h"
+#include "Library/Area/AreaShape.h"
+#include "Library/Clipping/ClippingJudge.h"
+#include "Library/Math/MathUtil.h"
+#include "Library/Placement/PlacementFunction.h"
+
+extern "C" sead::Vector3f gDefaultBoundingBoxMinMax;
+
+BirdGatheringSpotArea::BirdGatheringSpotArea(const char* name) : al::AreaObj(name) {}
+
+void BirdGatheringSpotArea::init(const al::AreaInitInfo& info) {
+    al::AreaObj::init(info);
+    al::getArg(&mBirdNumMax, *getPlacementInfo(), "BirdNumMax");
+    getAreaShape()->calcLocalBoundingBox(&mClippingInfo.boundingBox);
+}
+
+BirdGatheringSpotArea::AreaClippingInfo::AreaClippingInfo() = default;
+
+void BirdGatheringSpotArea::updateClipping(const al::ClippingJudge* judge,
+                                           const sead::Vector3f& pos) {
+    const sead::Matrix34f& mtx = getAreaMtx();
+
+    mClippingInfo.isClipped =
+        judge->isJudgedToClipFrustumObb(&mtx, mClippingInfo.boundingBox, 300.0f, 1);
+
+    if (!mClippingInfo.isClipped)
+        mClippingInfo.sightDistance = (pos - mtx.getTranslation()).length();
+}
+
+void BirdGatheringSpotArea::calcRandomGroundTrans(sead::Vector3f* outTrans) const {
+    f32 rx = al::getRandom(-1.0f, 1.0f);
+    f32 rz = al::getRandom(-1.0f, 1.0f);
+
+    const sead::Vector3f& scale = getAreaShape()->getScale();
+
+    sead::Vector3f offset = {rx * scale.x * 500.0f, 0.0f, rz * scale.z * 500.0f};
+
+    outTrans->setMul(getAreaMtx(), offset);
+}
+
+bool BirdGatheringSpotArea::isGreaterPriorityNotClipped(const BirdGatheringSpotArea* other) const {
+    return other->mClippingInfo.sightDistance <= mClippingInfo.sightDistance;
+}
+
+bool BirdGatheringSpotArea::isClipped() const {
+    return mClippingInfo.isClipped;
+}
+
+f32 BirdGatheringSpotArea::getSightDistance() const {
+    return mClippingInfo.sightDistance;
+}
--- src/Area/BirdGatheringSpotArea.h
+++ src/Area/BirdGatheringSpotArea.h
@@ -1,32 +1,32 @@
 #pragma once
 
 #include <math/seadBoundBox.h>
+#include <math/seadVector.h>
 
 #include "Library/Area/AreaObj.h"
 
 namespace al {
+class AreaInitInfo;
 class ClippingJudge;
-}
+}  // namespace al
 
 class BirdGatheringSpotArea : public al::AreaObj {
 public:
-    struct AreaClippingInfo {
-        AreaClippingInfo();
-
-        bool isClipped = false;
-        f32 sightDistance = 0.0f;
-        sead::BoundBox3f boundingBox = sead::BoundBox3f(0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f);
-    };
-
     BirdGatheringSpotArea(const char* name);
-
     void init(const al::AreaInitInfo& info) override;
 
-    void calcRandomGroundTrans(sead::Vector3f* trans) const;
-    f32 getSightDistance() const;
+    void updateClipping(const al::ClippingJudge* judge, const sead::Vector3f& pos);
     bool isClipped() const;
+    f32 getSightDistance() const;
     bool isGreaterPriorityNotClipped(const BirdGatheringSpotArea* other) const;
-    void updateClipping(const al::ClippingJudge*, const sead::Vector3f&);
+    void calcRandomGroundTrans(sead::Vector3f* trans) const;
+
+    struct AreaClippingInfo {
+        AreaClippingInfo();
+        bool isClipped = false;
+        f32 sightDistance = 0.0f;
+        sead::BoundBox3f boundingBox = sead::BoundBox3f(sead::Vector3f::zero, sead::Vector3f::zero);
+    };
 
 private:
     s32 mBirdNumMax = 0;
