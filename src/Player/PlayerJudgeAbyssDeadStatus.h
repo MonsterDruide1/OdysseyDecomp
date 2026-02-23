dac71eac974e1fb79d85f78c0e2cc5edd32fe11e
Spletz47 <spletz47@gmail.com>
2026-02-24 10:30:37 +1100

    Player: Implement `PlayerRecoverySafetyPoint` and `PlayerJudgeAbyssDeadStatus`

---
+++ src/Player/PlayerJudgeAbyssDeadStatus.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "Player/IJudge.h"
+#include "Player/PlayerInfo.h"
+#include "Player/PlayerJudgeSameNerve.h"
+#include "Player/PlayerRecoverySafetyPoint.h"
+#include "Util/JudgeUtil.h"
+
+class PlayerJudgeSameNerve;
+class PlayerRecoverySafetyPoint;
+
+class PlayerJudgeAbyssDeadStatus : public IJudge {
+public:
+    PlayerJudgeAbyssDeadStatus(const PlayerJudgeSameNerve* nerve,
+                               const PlayerRecoverySafetyPoint* safePoint);
+
+    virtual void reset() override;
+    virtual void update() override;
+    virtual bool judge() const override;
+
+private:
+    const PlayerJudgeSameNerve* mNerve;
+    const PlayerRecoverySafetyPoint* mSafePoint;
+};
+
+static_assert(sizeof(PlayerJudgeAbyssDeadStatus) == 0x18);
