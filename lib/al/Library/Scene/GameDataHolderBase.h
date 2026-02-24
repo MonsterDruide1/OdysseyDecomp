f380d85c047307050643e4933bfa4ec5e0d80ed0
Narr the Reg <5944268+german77@users.noreply.github.com>
2026-02-23 18:09:44 -0600

    First review

--- lib/al/Library/Scene/GameDataHolderBase.h
+++ lib/al/Library/Scene/GameDataHolderBase.h
@@ -1,10 +1,7 @@
 #pragma once
 
-#include "Library/HostIO/HioNode.h"
-#include "Library/Message/IUseMessageSystem.h"
-#include "Library/Scene/ISceneObj.h"
-
 namespace al {
 
-class GameDataHolderBase : public ISceneObj, public HioNode, public IUseMessageSystem {};
+class GameDataHolderBase {};
+
 }  // namespace al
