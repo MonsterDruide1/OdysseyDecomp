cf0823a805d687e9a218d883cd329d680e24ad42
Fuzzy2319 <alexandre.verbrigghe@orange.fr>
2026-02-26 22:13:14 +0100

    add:EventFlowNodeActionLoop implementation

---
+++ lib/al/Library/Event/EventFlowNodeActionLoop.cpp
@@ -0,0 +1,37 @@
+#include "Library/Event/EventFlowNodeActionLoop.h"
+
+#include "Library/Event/EventFlowFunction.h"
+#include "Library/Event/EventFlowUtil.h"
+#include "Library/LiveActor/ActorActionFunction.h"
+
+namespace al {
+EventFlowNodeActionLoop::EventFlowNodeActionLoop(const char* name) : EventFlowNode(name) {}
+
+void EventFlowNodeActionLoop::init(const EventFlowNodeInitInfo& info) {
+    initEventFlowNode(this, info);
+    mActionName = getParamIterKeyString(info, "ActionName");
+    tryGetParamIterKeyBool(&mIsStartRandomFrame, info, "IsStartRandomFrame");
+    tryGetParamIterKeyInt(&mMaxStep, info, "MaxStep");
+    tryGetParamIterKeyFloat(&mActionFrameRate, info, "ActionFrameRate");
+}
+
+void EventFlowNodeActionLoop::start() {
+    EventFlowNode::start();
+    mStep = 0;
+    if (!isPlayingEventAction(getActor(), this, mActionName)) {
+        !mIsStartRandomFrame ? startEventAction(getActor(), this, mActionName) :
+                               startEventActionAtRandomFrame(getActor(), this, mActionName);
+    }
+
+    if (mActionFrameRate > 0.0f)
+        setActionFrameRate(getActor(), mActionFrameRate);
+
+    if (mMaxStep <= mStep)
+        end();
+}
+
+void EventFlowNodeActionLoop::control() {
+    if (mMaxStep <= ++mStep)
+        end();
+}
+}  // namespace al
