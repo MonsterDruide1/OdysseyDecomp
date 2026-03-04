62e95181eb7b334a3d7dc1544421493ac275db8f
Narr the Reg <5944268+german77@users.noreply.github.com>
2026-03-04 13:10:46 -0600

    Library/Audio: Implement AudioSoundArchiveInfo

--- lib/al/Library/Audio/AudioEventController.cpp
+++ lib/al/Library/Audio/AudioEventController.cpp
@@ -1,5 +1,6 @@
 #include "Library/Audio/AudioEventController.h"
 
+#include "Library/Audio/AudioInfo.h"
 #include "Library/Yaml/ByamlIter.h"
 
 namespace al {
@@ -18,6 +19,20 @@ AudioAddonSoundArchiveInfo* AudioAddonSoundArchiveInfo::createInfo(const ByamlIt
 s32 AudioAddonSoundArchiveInfo::compareInfo(const AudioAddonSoundArchiveInfo* lhs,
                                             const AudioAddonSoundArchiveInfo* rhs) {
     return strcmp(lhs->name, rhs->name);
+}
+
+AudioSoundArchiveInfo::AudioSoundArchiveInfo() = default;
+
+AudioSoundArchiveInfo* AudioSoundArchiveInfo::createInfo(const ByamlIter& iter) {
+    AudioSoundArchiveInfo* addon = new AudioSoundArchiveInfo();
+
+    ByamlIter addonIter;
+    if (!iter.tryGetIterByKey(&addonIter, "AddonSoundArchiveInfoList"))
+        addon->info = nullptr;
+
+    // BUG: addonIter can be invalid. It should have returned early on the previous check
+    addon->info = createAudioInfoList<AudioAddonSoundArchiveInfo>(addonIter, 0);
+    return addon;
 }
 
 }  // namespace al
