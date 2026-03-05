21f99b806e57eaa15e31835da92e0609d35012df
guymakinggames <66076221+guymakinggames@users.noreply.github.com>
2026-03-05 06:12:31 +0000

    System: Implement AchievementSaveData

--- src/System/AchievementSaveData.h
+++ src/System/AchievementSaveData.h
@@ -6,6 +6,13 @@
 
 class AchievementInfoReader;
 
+struct AchievementEntry {
+    bool isGotten = false;
+    u64 getTime = 0;
+};
+
+static_assert(sizeof(AchievementEntry) == 0x10);
+
 class AchievementSaveData : public ByamlSave {
 public:
     AchievementSaveData(const AchievementInfoReader* reader);
@@ -14,14 +21,15 @@ public:
     s32 findAchievementIndex(const char* name) const;
     bool checkGetAchievement(const char* name) const;
     bool checkGetAchievement(s32 index) const;
-    s64 findAchievementGetTime(s32 index) const;
+    u64 findAchievementGetTime(s32 index) const;
     s32 calcGetAchievementNum() const;
     s32 getAchievementNum() const;
     void write(al::ByamlWriter* writer) override;
     void read(const al::ByamlIter& save) override;
 
 private:
-    void* _padding[0x2];
+    AchievementEntry* mEntries = nullptr;
+    const AchievementInfoReader* mInfoReader;
 };
 
 static_assert(sizeof(AchievementSaveData) == 0x18);
