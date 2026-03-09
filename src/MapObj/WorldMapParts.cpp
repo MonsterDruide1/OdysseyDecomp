7174bae73fbb578ac8b8fcd98483081eedb0327f
guymakinggames <66076221+guymakinggames@users.noreply.github.com>
2026-03-09 13:20:55 +0000

    MapObj: Implement WorldMapPlayerIcon and WorldMapPartsFloat

--- src/MapObj/WorldMapParts.cpp
+++ src/MapObj/WorldMapParts.cpp
@@ -1,10 +1,13 @@
 #include "MapObj/WorldMapParts.h"
 
+#include <math/seadMathCalcCommon.h>
+
 #include "Library/LiveActor/ActorInitUtil.h"
 #include "Library/LiveActor/ActorModelFunction.h"
 #include "Library/LiveActor/ActorPoseUtil.h"
 #include "Library/LiveActor/LiveActorFunction.h"
 #include "Library/Math/MathUtil.h"
+#include "Library/Matrix/MatrixUtil.h"
 
 void recursivelyInvalidateOcclusionQuery(al::LiveActor* actor) {
     al::invalidateOcclusionQuery(actor);
@@ -64,4 +67,32 @@ WorldMapParts* WorldMapParts::create(const char* name, const char* arcName,
     initParts(newParts, arcName, initInfo, worldMtx, localMtx, suffix);
 
     return newParts;
+}
+
+WorldMapPartsFloat::WorldMapPartsFloat(const char* name, const sead::Vector3f& offset, s32 period,
+                                       f32 amplitude)
+    : WorldMapParts(name), mFloatOffset(offset), mPeriod(period), mAmplitude(amplitude) {}
+
+void WorldMapPartsFloat::control() {
+    f32 sinVal = sinf((f32)(mFrameCount++) / (f32)mPeriod * sead::Mathf::pi2());
+    mLocalMtx(0, 3) = mTranslation.x + sinVal * mAxis.x * mAmplitude;
+    mLocalMtx(1, 3) = sinVal * mAxis.y * mAmplitude + mTranslation.y;
+    mLocalMtx(2, 3) = sinVal * mAxis.z * mAmplitude + mTranslation.z;
+
+    sead::Matrix34f worldMtx = *mWorldMtx;
+    al::normalize(&worldMtx);
+    worldMtx.setMul(worldMtx, mLocalMtx);
+    al::updatePoseMtx(this, &worldMtx);
+}
+
+void WorldMapPartsFloat::setLocalMtx(const sead::Matrix34f& srcMtx) {
+    al::addTransMtxLocalOffset(&mLocalMtx, srcMtx, mFloatOffset);
+
+    mTranslation.x = mLocalMtx(0, 3);
+    mTranslation.y = mLocalMtx(1, 3);
+    mTranslation.z = mLocalMtx(2, 3);
+
+    mAxis.x = mLocalMtx(0, 1);
+    mAxis.y = mLocalMtx(1, 1);
+    mAxis.z = mLocalMtx(2, 1);
 }
