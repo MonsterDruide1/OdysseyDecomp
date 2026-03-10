e0b44fc799cc7a50f611e66af067aec602b6d0b3
GRAnimated <devin@nhscan.com>
2026-03-09 21:38:59 -0400

    start review

--- src/Npc/TrafficRailWatcher.cpp
+++ src/Npc/TrafficRailWatcher.cpp
@@ -26,18 +26,16 @@ void TrafficRailWatcher::registerActor(const al::LiveActor* actor) {
 void TrafficRailWatcher::stopByTraffic(const al::LiveActor* actor) {
     TrafficRailActorInfo** p = mActors;
     TrafficRailActorInfo* info;
-    do {
+    while (info = *p, info->actor != actor)
         info = *p++;
-    } while (info->actor != actor);
     info->status = 1;
 }
 
 void TrafficRailWatcher::restartByTraffic(const al::LiveActor* actor) {
     TrafficRailActorInfo** p = mActors;
     TrafficRailActorInfo* info;
-    do {
+    while (info = *p, info->actor != actor)
         info = *p++;
-    } while (info->actor != actor);
     info->status = 0;
 }
 
@@ -54,28 +52,10 @@ bool TrafficRailWatcher::isExist(const al::LiveActor* actor) const {
     return false;
 }
 
-bool TrafficRailWatcher::tryStopByOtherNpc(const al::LiveActor* actor) {
-    TrafficRailActorInfo* actorInfo = nullptr;
-    for (s32 i = 0; i < mActorCount; i++) {
-        if (mActors[i]->actor == actor) {
-            actorInfo = mActors[i];
-            break;
-        }
-    }
-
-    for (s32 j = 0; j < mActorCount; j++) {
-        if (mActors[j] != actorInfo) {
-            if (isNearOnTrafficRail(actor, mActors[j])) {
-                actorInfo->status = 2;
-                return true;
-            }
-        }
-    }
-    return false;
-}
-
-bool isNearOnTrafficRail(const al::LiveActor* actor, const TrafficRailActorInfo* otherEntry) {
-    if ((u32)(otherEntry->status - 1) > 1)
+// NON_MATCHING
+bool isNearOnTrafficRail(const al::LiveActor* actor,
+                         const TrafficRailWatcher::TrafficRailActorInfo* otherEntry) {
+    if (otherEntry->status != 1 && otherEntry->status != 2)
         return false;
 
     const al::LiveActor* otherActor = otherEntry->actor;
@@ -99,15 +79,35 @@ bool isNearOnTrafficRail(const al::LiveActor* actor, const TrafficRailActorInfo*
 
     bool isLoop = al::isLoopRail(actor);
     f32 otherCoord = al::getRailCoord(otherActor);
-
     if (isLoop && high < low) {
         if (low < otherCoord)
             return true;
-    } else if (low >= otherCoord) {
-        return false;
+    } else {
+        if (low >= otherCoord)
+            return false;
     }
 
     return al::getRailCoord(otherActor) < high;
+}
+
+bool TrafficRailWatcher::tryStopByOtherNpc(const al::LiveActor* actor) {
+    TrafficRailActorInfo* actorInfo = nullptr;
+    for (s32 i = 0; i < mActorCount; i++) {
+        if (mActors[i]->actor == actor) {
+            actorInfo = mActors[i];
+            break;
+        }
+    }
+
+    for (s32 j = 0; j < mActorCount; j++) {
+        if (mActors[j] != actorInfo) {
+            if (isNearOnTrafficRail(actor, mActors[j])) {
+                actorInfo->status = 2;
+                return true;
+            }
+        }
+    }
+    return false;
 }
 
 bool TrafficRailWatcher::tryRestartByOtherNpc(const al::LiveActor* actor) {
--- src/Npc/TrafficRailWatcher.h
+++ src/Npc/TrafficRailWatcher.h
@@ -6,16 +6,15 @@ namespace al {
 class LiveActor;
 class PlacementId;
 class PlacementInfo;
-PlacementId* createPlacementId(const PlacementInfo& placementInfo);
 }  // namespace al
-
-struct TrafficRailActorInfo {
-    const al::LiveActor* actor;
-    s32 status;
-};
 
 class TrafficRailWatcher {
 public:
+    struct TrafficRailActorInfo {
+        const al::LiveActor* actor;
+        s32 status;
+    };
+
     TrafficRailWatcher(const al::PlacementInfo& placementInfo);
 
     void registerActor(const al::LiveActor* actor);
@@ -26,11 +25,10 @@ public:
     bool tryStopByOtherNpc(const al::LiveActor* actor);
     bool tryRestartByOtherNpc(const al::LiveActor* actor);
 
+private:
     al::PlacementId* mPlacementId;
     s32 mActorCount;
     TrafficRailActorInfo** mActors;
 };
 
 static_assert(sizeof(TrafficRailWatcher) == 0x18);
-
-bool isNearOnTrafficRail(const al::LiveActor* actor, const TrafficRailActorInfo* otherEntry);
