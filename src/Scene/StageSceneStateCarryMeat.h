869c880ae06196fae464bee257f441046b9bf879
guymakinggames <66076221+guymakinggames@users.noreply.github.com>
2026-03-17 20:54:53 +0000

    Scene: Implement StageSceneStateCarryMeat

---
+++ src/Scene/StageSceneStateCarryMeat.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "Library/Nerve/NerveStateBase.h"
+
+namespace al {
+class Scene;
+}
+class StageSceneStateSkipDemo;
+
+class StageSceneStateCarryMeat : public al::HostStateBase<al::Scene> {
+public:
+    StageSceneStateCarryMeat(const char* name, al::Scene* scene);
+    void setState(StageSceneStateSkipDemo* skipDemo);
+    void appear() override;
+    void kill() override;
+    void exeFindMeat();
+    void exeCarryMeat();
+    void exeSkipDemoFindMeat();
+    void exeSkipDemoCarryMeat();
+
+private:
+    StageSceneStateSkipDemo* mSkipDemo = nullptr;
+};
+
+static_assert(sizeof(StageSceneStateCarryMeat) == 0x28);
