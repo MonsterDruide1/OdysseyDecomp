73812feed41de41774e2c4eccedbd95db82bba73
guymakinggames <66076221+guymakinggames@users.noreply.github.com>
2026-03-18 09:32:47 +0000

    MapObj: Implement PeachTrunk

---
+++ src/MapObj/PeachTrunk.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "Library/LiveActor/LiveActor.h"
+
+class PeachTrunk : public al::LiveActor {
+public:
+    PeachTrunk(const char* actorName);
+
+    void init(const al::ActorInitInfo& info) override;
+    bool receiveMsg(const al::SensorMsg* message, al::HitSensor* other,
+                    al::HitSensor* self) override;
+
+    void exeWait();
+    void exeReactionCap();
+};
