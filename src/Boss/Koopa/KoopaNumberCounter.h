fb8ef8e75f1e819c77c1800a5ccdc07c9c2cb5d2
guymakinggames <66076221+guymakinggames@users.noreply.github.com>
2026-03-18 09:06:37 +0000

    Boss/Koopa: Implement KoopaStateHitPunch

---
+++ src/Boss/Koopa/KoopaNumberCounter.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <prim/seadSafeString.h>
+
+namespace al {
+class LiveActor;
+struct ActorInitInfo;
+}  // namespace al
+
+class KoopaCap;
+
+class KoopaNumberCounter {
+public:
+    KoopaNumberCounter(const al::ActorInitInfo&);
+    bool isValidFireBeam() const;
+    s32 getDamage() const;
+    bool isShowCapWide() const;
+    void startBeam();
+    bool isFirstBeam() const;
+    bool isLastBeam() const;
+    bool addBeamCountAndCheckEnd();
+    bool isEnableStartDamageBall() const;
+    bool tryStartDamageBall();
+    bool isBombDamageBall(s32) const;
+    bool addDamageBallCountAndCheckEnd();
+    bool addDamageBallContinueSkipCountAndCheckSkip();
+    bool isDamageBallContinueSkipped() const;
+    bool tryStartAttackTail(KoopaCap*);
+    bool isExecuteAttackTailTwice() const;
+    bool addAttackTailCountAndCheckEnd();
+    bool isGuardPunchHitAction() const;
+    f32* getPunchHitSeParamPtr() const;
+    void addPunchHitCountAndPowerUpCap(KoopaCap*);
+    void recoverPunchHitCount(KoopaCap*);
+    void startPunchHitAction(al::LiveActor*) const;
+    void makeDamageAnimSeName(sead::BufferedSafeStringBase<char>*) const;
+    bool receiveDamageAndCheckDead(KoopaCap*);
+};
---
+++ src/Boss/Koopa/KoopaStateHitPunch.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "Library/Nerve/NerveStateBase.h"
+
+namespace al {
+class LiveActor;
+}
+
+class KoopaCap;
+class KoopaFlag;
+class KoopaNumberCounter;
+
+class KoopaStateHitPunch : public al::ActorStateBase {
+public:
+    KoopaStateHitPunch(al::LiveActor* actor, KoopaCap* cap, KoopaFlag* flag,
+                       KoopaNumberCounter* counter);
+    void appear() override;
+    void exeHitFirst();
+    void exeHit();
+    void exeHitEnd();
+
+private:
+    KoopaCap* mCap = nullptr;
+    KoopaFlag* mFlag = nullptr;
+    KoopaNumberCounter* mCounter = nullptr;
+};
