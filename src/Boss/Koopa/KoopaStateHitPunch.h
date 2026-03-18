0aafcadc658c38ca703f6d54ab1c2df4b96567a8
guymakinggames <66076221+guymakinggames@users.noreply.github.com>
2026-03-18 09:06:37 +0000

    Boss/Koopa: Implement KoopaStateHitPunch

---
+++ src/Boss/Koopa/KoopaStateHitPunch.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "Boss/Koopa/KoopaFlag.h"
+#include "Boss/Koopa/KoopaFunction.h"
+#include "Boss/Koopa/KoopaNumberCounter.h"
+
+#include "Library/Nerve/NerveStateBase.h"
+
+namespace al {
+class LiveActor;
+}
+
+class KoopaCap;
+class KoopaFlag;
+class KoopaNumberCounter;
+
+class KoopaStateHitPunch : public al::ActorStateBase {
+public:
+    KoopaStateHitPunch(al::LiveActor* actor, KoopaCap* cap, KoopaFlag* flag,
+                       KoopaNumberCounter* counter);
+    void appear() override;
+    void exeHitFirst();
+    void exeHit();
+    void exeHitEnd();
+
+private:
+    KoopaCap* mCap = nullptr;
+    KoopaFlag* mFlag = nullptr;
+    KoopaNumberCounter* mCounter = nullptr;
+};
