689e12ce86d2e6bda5184857221ecaa4c18d2ae3
guymakinggames <66076221+guymakinggames@users.noreply.github.com>
2026-03-18 08:56:54 +0000

    Boss/GolemClimb: Implement GolemStandBreakState

---
+++ src/Boss/GolemClimb/GolemShoutState.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "Library/Nerve/NerveStateBase.h"
+
+namespace al {
+class SensorMsg;
+class HitSensor;
+}  // namespace al
+
+class IUseGolemState;
+
+class GolemShoutState : public al::HostStateBase<IUseGolemState> {
+public:
+    GolemShoutState(const char* name, IUseGolemState* golemState);
+    void appear() override;
+    void kill() override;
+    void control() override;
+    void receiveMsg(const al::SensorMsg*, al::HitSensor* self, al::HitSensor* other);
+    void attackSensor(al::HitSensor* self, al::HitSensor* other);
+};
