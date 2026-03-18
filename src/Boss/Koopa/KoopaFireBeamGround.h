725c1e2e29a8aab21d2265ff55d9f3c19f2bd8c1
guymakinggames <66076221+guymakinggames@users.noreply.github.com>
2026-03-18 06:28:35 +0000

    Boss/Koopa: Implement KoopaFireBeamGround

---
+++ src/Boss/Koopa/KoopaFireBeamGround.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "Library/LiveActor/LiveActor.h"
+
+class KoopaFireBeamGround : public al::LiveActor {
+public:
+    KoopaFireBeamGround(const char* name);
+
+    void init(const al::ActorInitInfo& info) override;
+    void appear() override;
+    void attackSensor(al::HitSensor* self, al::HitSensor* other) override;
+    bool receiveMsg(const al::SensorMsg* message, al::HitSensor* other,
+                    al::HitSensor* self) override;
+
+    void appearSign();
+
+    void exeAppearSign();
+    void exeWait();
+};
+
+static_assert(sizeof(KoopaFireBeamGround) == 0x108);
