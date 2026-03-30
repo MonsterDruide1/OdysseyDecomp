e95a370468d4ddc9e364291a99b30ca80c8e9eae
guymakinggames <66076221+guymakinggames@users.noreply.github.com>
2026-03-30 16:49:09 +0100

    Boss/BossKnuckle: Implement `BossKnuckleHandPrint` (#985)

---
+++ src/Boss/BossKnuckle/BossKnuckleHandPrint.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "Library/LiveActor/LiveActor.h"
+
+class BossKnuckleHandPrint : public al::LiveActor {
+public:
+    BossKnuckleHandPrint(const char* name);
+    void initWithArchiveName(const al::ActorInitInfo& initInfo, const char* archiveName);
+    void appear() override;
+    void startDisappearIfAppeard();
+    void exeAppear();
+    void exeDisappear();
+
+private:
+    bool mIsLeft = false;
+};
+
+static_assert(sizeof(BossKnuckleHandPrint) == 0x110);
