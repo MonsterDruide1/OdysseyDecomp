f63c31bf6f46c76ebccca8ea3ba06b2c2d36d02c
guymakinggames <66076221+guymakinggames@users.noreply.github.com>
2026-04-09 21:57:11 +0100

    Boss/Koopa: Implement `KoopaStateHitPunch` (#988)

---
+++ src/Boss/Koopa/KoopaFlag.h
@@ -0,0 +1,26 @@
+#pragma once
+
+namespace al {
+class LiveActor;
+}
+
+class KoopaFlag {
+public:
+    KoopaFlag();
+    bool tryStartPunchHitFirst();
+    bool trySkipJumpSign();
+
+    bool hasHitEnd() const { return mHasHitEnd; }
+
+    void setHasHitEnd() { mHasHitEnd = true; }
+
+private:
+    bool _0 = false;
+    bool _1 = false;
+    bool _2 = false;
+    bool mHasHitEnd = false;
+    bool _4 = false;
+    bool _5 = false;
+    bool _6 = false;
+    bool _7 = true;
+};
