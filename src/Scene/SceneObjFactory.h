d49a564d25b134f476e177a5fe2f17aefb14e5e9
guymakinggames <66076221+guymakinggames@users.noreply.github.com>
2026-04-11 09:01:42 +0100

    Boss/Koopa: Implement KoopaHackStopCtrl

--- src/Scene/SceneObjFactory.h
+++ src/Scene/SceneObjFactory.h
@@ -37,7 +37,7 @@ enum SceneObjID : s32 {
     SceneObjID_InformationWindowDirector,
     SceneObjID__1c,
     SceneObjID_KidsModeLayoutAccessor,
-    SceneObjID_KoopaStopHackCtrl,
+    SceneObjID_KoopaHackStopCtrl,
     SceneObjID_KoopaShipDemoRequester,
     SceneObjID_alLayoutTextureRenderer,
     SceneObjID_LifeUpItemGenerateWatcher,
