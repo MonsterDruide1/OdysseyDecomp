c279f8bdce3387915da503bd7c4179ac4b25f87f
guymakinggames <66076221+guymakinggames@users.noreply.github.com>
2026-04-11 09:01:42 +0100

    Boss/Koopa: Implement KoopaHackStopCtrl

---
+++ src/Boss/Koopa/KoopaHackStopCtrl.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <basis/seadTypes.h>
+#include <math/seadQuat.h>
+#include <math/seadVector.h>
+
+#include "Library/Scene/ISceneObj.h"
+
+#include "Scene/SceneObjFactory.h"
+
+namespace al {
+class LiveActor;
+}
+
+class KoopaHackStopCtrl : public al::ISceneObj {
+public:
+    static constexpr s32 sSceneObjId = SceneObjID_KoopaHackStopCtrl;
+
+    KoopaHackStopCtrl();
+
+    void startStop(const al::LiveActor* actor);
+    void endStop(const al::LiveActor* actor);
+    void resetPosture(const al::LiveActor* actor, const sead::Quatf& quat,
+                      const sead::Vector3f& trans);
+    bool tryResetPosture(al::LiveActor* actor);
+
+    const char* getSceneObjName() const override { return "崩落クッパの停止制御"; }
+
+    bool isStop() const { return mStopActor != nullptr; }
+
+    bool isStatusDemoForSceneKoopaHack() const { return mIsStatusDemoForSceneKoopaHack; }
+
+    void setStatusDemoForSceneKoopaHack(bool status) { mIsStatusDemoForSceneKoopaHack = status; }
+
+private:
+    const al::LiveActor* mStopActor = nullptr;
+    bool mIsNeedResetPosture = false;
+    sead::Quatf mResetQuat = sead::Quatf::unit;
+    sead::Vector3f mResetTrans = {0.0f, 0.0f, 0.0f};
+    bool mIsStatusDemoForSceneKoopaHack = false;
+};
+
+static_assert(sizeof(KoopaHackStopCtrl) == 0x38);
+
+namespace KoopaHackFunction {
+void startStopKoopaHack(al::LiveActor* actor);
+void endStopKoopaHack(al::LiveActor* actor);
+void resetPostureKoopaHack(al::LiveActor* actor, const sead::Quatf& quat,
+                           const sead::Vector3f& trans);
+bool isStopKoopaHack(const al::LiveActor* actor);
+bool isStatusDemoForSceneKoopaHack(const al::LiveActor* actor);
+void setStatusDemoForSceneKoopaHack(const al::LiveActor* actor);
+void resetStatusDemoForSceneKoopaHack(const al::LiveActor* actor);
+}  // namespace KoopaHackFunction
