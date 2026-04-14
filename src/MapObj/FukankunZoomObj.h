5f8e410386f9a232cf67b3f8bb3f8f6b30d1e197
guymakinggames <66076221+guymakinggames@users.noreply.github.com>
2026-04-14 22:20:21 +0100

    MapObj: Implement FukankunZoomObj

---
+++ src/MapObj/FukankunZoomObj.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "Library/LiveActor/LiveActor.h"
+
+class FukankunZoomObj : public al::LiveActor {
+public:
+    FukankunZoomObj(const char* name);
+
+    void init(const al::ActorInitInfo& info) override;
+    void onAppear();
+    void onKill();
+    void attackSensor(al::HitSensor* self, al::HitSensor* other) override;
+    bool receiveMsg(const al::SensorMsg* message, al::HitSensor* other,
+                    al::HitSensor* self) override;
+    void exeWait();
+
+private:
+    f32 mRailMoveSpeed = 20.0f;
+    s32 mMoveType = 0;
+};
+
+static_assert(sizeof(FukankunZoomObj) == 0x110);
