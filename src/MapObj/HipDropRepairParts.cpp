a09245095f82348bec3513f5833607cfed0372b3
guymakinggames <66076221+guymakinggames@users.noreply.github.com>
2026-04-15 13:14:57 +0100

    MapObj: Implement HipDropRepairParts

---
+++ src/MapObj/HipDropRepairParts.cpp
@@ -0,0 +1,102 @@
+#include "MapObj/HipDropRepairParts.h"
+
+#include "Library/LiveActor/ActorActionFunction.h"
+#include "Library/LiveActor/ActorInitUtil.h"
+#include "Library/LiveActor/ActorModelFunction.h"
+#include "Library/LiveActor/ActorPoseUtil.h"
+#include "Library/LiveActor/LiveActorFunction.h"
+#include "Library/Nerve/NerveSetupUtil.h"
+#include "Library/Nerve/NerveUtil.h"
+#include "Library/Placement/PlacementFunction.h"
+#include "Library/Stage/StageSwitchUtil.h"
+#include "Library/Thread/FunctorV0M.h"
+
+#include "Util/ItemGenerator.h"
+#include "Util/SensorMsgFunction.h"
+
+namespace {
+NERVE_IMPL(HipDropRepairParts, Wait);
+NERVE_IMPL(HipDropRepairParts, On);
+NERVE_IMPL(HipDropRepairParts, OnWait);
+
+NERVES_MAKE_NOSTRUCT(HipDropRepairParts, OnWait);
+NERVES_MAKE_STRUCT(HipDropRepairParts, Wait, On);
+}  // namespace
+
+HipDropRepairParts::HipDropRepairParts(const char* name) : al::LiveActor(name) {}
+
+void HipDropRepairParts::init(const al::ActorInitInfo& info) {
+    using HipDropRepairPartsFunctor =
+        al::FunctorV0M<HipDropRepairParts*, void (HipDropRepairParts::*)()>;
+
+    const char* suffixName = nullptr;
+    al::tryGetStringArg(&suffixName, info, "SuffixName");
+    al::initMapPartsActor(this, info, suffixName);
+    al::initNerve(this, &NrvHipDropRepairParts.Wait, 0);
+
+    mInitTrans = al::getTrans(this);
+    al::calcTransLocalOffset(&mItemGeneratePos, this, sead::Vector3f::ey * 100.0f);
+
+    mItemGenerator = new ItemGenerator(this, info);
+    mSubActor = al::tryGetSubActor(this, "跡モデル");
+    if (mSubActor)
+        mSubActor->makeActorDead();
+
+    if (al::isValidStageSwitch(this, "SwitchHide")) {
+        al::listenStageSwitchOnOff(this, "SwitchHide",
+                                   HipDropRepairPartsFunctor(this, &HipDropRepairParts::startHide),
+                                   HipDropRepairPartsFunctor(this, &HipDropRepairParts::startShow));
+    } else if (al::isValidStageSwitch(this, "SwitchShow")) {
+        al::listenStageSwitchOnOff(this, "SwitchShow",
+                                   HipDropRepairPartsFunctor(this, &HipDropRepairParts::startShow),
+                                   HipDropRepairPartsFunctor(this, &HipDropRepairParts::startHide));
+        al::hideModel(this);
+        if (mSubActor)
+            al::hideModel(mSubActor);
+    }
+
+    makeActorAlive();
+}
+
+void HipDropRepairParts::startHide() {
+    al::hideModelIfShow(this);
+    if (mSubActor)
+        al::hideModelIfShow(mSubActor);
+}
+
+void HipDropRepairParts::startShow() {
+    al::showModelIfHide(this);
+    if (mSubActor)
+        al::showModelIfHide(mSubActor);
+}
+
+bool HipDropRepairParts::receiveMsg(const al::SensorMsg* message, al::HitSensor* other,
+                                    al::HitSensor* self) {
+    if (rs::isMsgPlayerAndCapHipDropAll(message) &&
+        al::isNerve(this, &NrvHipDropRepairParts.Wait)) {
+        al::startHitReaction(this, "ヒップドロップ開始");
+        al::setNerve(this, &NrvHipDropRepairParts.On);
+        if (mSubActor)
+            mSubActor->makeActorAlive();
+    }
+
+    return false;
+}
+
+void HipDropRepairParts::exeWait() {}
+
+void HipDropRepairParts::exeOn() {
+    f32 moveOffset = (al::getNerveStep(this) + 1) * -25.0f;
+    sead::Vector3f trans = sead::Vector3f::ey * moveOffset + mInitTrans;
+    al::setTrans(this, trans);
+
+    if (al::isGreaterEqualStep(this, 4)) {
+        mItemGenerator->generate(mItemGeneratePos, al::getQuat(this));
+        al::startHitReaction(this, "ヒップドロップ完了");
+        al::tryOnStageSwitch(this, "SwitchHipDropOn");
+        al::setNerve(this, &OnWait);
+        kill();
+    }
+}
+
+void HipDropRepairParts::exeOnWait() {}
