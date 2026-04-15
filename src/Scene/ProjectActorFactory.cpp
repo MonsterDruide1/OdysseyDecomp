d802a128352f51a7f2614d660a604d19b33b7164
guymakinggames <66076221+guymakinggames@users.noreply.github.com>
2026-04-15 10:12:19 +0100

    MapObj: Implement StatueSnapMark

--- src/Scene/ProjectActorFactory.cpp
+++ src/Scene/ProjectActorFactory.cpp
@@ -98,6 +98,7 @@
 #include "MapObj/SignBoardDanger.h"
 #include "MapObj/Souvenir.h"
 #include "MapObj/StageSwitchSelector.h"
+#include "MapObj/StatueSnapMark.h"
 #include "MapObj/TrampleBush.h"
 #include "MapObj/TrampleSwitch.h"
 #include "MapObj/TrampleSwitchTimer.h"
@@ -570,7 +571,7 @@ const al::NameToCreator<al::ActorCreatorFunction> sProjectActorFactoryEntries[] 
     {"StageTalkDemoNpcCapMoonRock", nullptr},
     {"Stake", nullptr},
     {"Statue", nullptr},
-    {"StatueSnapMark", nullptr},
+    {"StatueSnapMark", al::createActorFunction<StatueSnapMark>},
     {"SubActorLodFixPartsScenarioAction", nullptr},
     {"SwitchAnd", nullptr},
     {"SwitchKeyMoveMapParts", nullptr},
