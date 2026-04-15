a71d381d7d4a63b1232816f02af1175268e87bc5
guymakinggames <66076221+guymakinggames@users.noreply.github.com>
2026-04-15 15:59:57 +0100

    MapObj: Implement SignBoard

--- src/Scene/ProjectActorFactory.cpp
+++ src/Scene/ProjectActorFactory.cpp
@@ -95,6 +95,7 @@
 #include "MapObj/RouletteSwitch.h"
 #include "MapObj/SaveFlagCheckObj.h"
 #include "MapObj/ShineTowerRocket.h"
+#include "MapObj/SignBoard.h"
 #include "MapObj/SignBoardDanger.h"
 #include "MapObj/Souvenir.h"
 #include "MapObj/StageSwitchSelector.h"
@@ -545,7 +546,7 @@ const al::NameToCreator<al::ActorCreatorFunction> sProjectActorFactoryEntries[] 
     {"SkyWorldKoopaFire", nullptr},
     {"SkyWorldKoopaFrame", nullptr},
     {"SkyWorldMiddleViewCloud", nullptr},
-    {"SignBoard", nullptr},
+    {"SignBoard", al::createActorFunction<SignBoard>},
     {"SnowWorldBigIcicle", nullptr},
     {"SnowWorldSequenceFlagCheckObj", nullptr},
     {"Sky", nullptr},
