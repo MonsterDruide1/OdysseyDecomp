7db51f96d2441ac87320d6ac34376659cf63f190
guymakinggames <66076221+guymakinggames@users.noreply.github.com>
2026-04-17 21:26:28 +0100

    MapObj: Implement BlockStateNoItem

---
+++ src/MapObj/BlockStateNoItem.cpp
@@ -0,0 +1,74 @@
+#include "MapObj/BlockStateNoItem.h"
+
+#include "Library/LiveActor/ActorActionFunction.h"
+#include "Library/LiveActor/ActorCollisionFunction.h"
+#include "Library/LiveActor/ActorModelFunction.h"
+#include "Library/LiveActor/LiveActorFunction.h"
+#include "Library/Nerve/NerveSetupUtil.h"
+#include "Library/Nerve/NerveUtil.h"
+#include "Library/Obj/PartsFunction.h"
+
+#include "Util/SensorMsgFunction.h"
+
+namespace {
+NERVE_IMPL(BlockStateNoItem, Wait);
+NERVE_IMPL(BlockStateNoItem, Reaction);
+
+NERVES_MAKE_STRUCT(BlockStateNoItem, Wait, Reaction);
+}  // namespace
+
+BlockStateNoItem::BlockStateNoItem(al::LiveActor* actor)
+    : al::ActorStateBase("アイテムなし状態", actor) {}
+
+void BlockStateNoItem::init() {
+    // NOTE: initializes storage for one sub-state, but never adds it
+    initNerve(&NrvBlockStateNoItem.Wait, 1);
+}
+
+void BlockStateNoItem::appear() {
+    al::ActorStateBase::appear();
+    al::validateCollisionParts(mActor);
+    al::startAction(mActor, "Wait");
+    al::setNerve(this, &NrvBlockStateNoItem.Wait);
+}
+
+bool BlockStateNoItem::receiveMsg(const al::SensorMsg* message, al::HitSensor* other,
+                                  al::HitSensor* self) {
+    if (al::isNerve(this, &NrvBlockStateNoItem.Wait) && rs::isMsgBlockReactionAll(message)) {
+        doReaction();
+        return true;
+    }
+    return false;
+}
+
+void BlockStateNoItem::doReaction() {
+    al::LiveActor* subActor = al::tryGetSubActor(mActor, "壊れモデル");
+    if (subActor) {
+        al::appearBreakModelRandomRotateY(subActor);
+        al::startHitReaction(mActor, "破壊");
+        al::hideModelIfShow(mActor);
+        al::invalidateCollisionParts(mActor);
+    }
+
+    al::setNerve(this, &NrvBlockStateNoItem.Reaction);
+}
+
+bool BlockStateNoItem::isReaction() const {
+    return al::isNerve(this, &NrvBlockStateNoItem.Reaction);
+}
+
+void BlockStateNoItem::exeWait() {}
+
+void BlockStateNoItem::exeReaction() {
+    if (!al::isExistAction(mActor, "Reaction")) {
+        if (al::isGreaterEqualStep(this, 5))
+            kill();
+        return;
+    }
+
+    if (al::isFirstStep(this))
+        al::startAction(mActor, "Reaction");
+
+    if (al::isActionEnd(mActor))
+        kill();
+}
