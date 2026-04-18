2b8702704b59666650ca5cb4fc9ad6541d88f560
guymakinggames <66076221+guymakinggames@users.noreply.github.com>
2026-04-18 15:50:04 +0100

    MapObj: Implement FixMapPartsAppearKillAsync

---
+++ src/MapObj/FixMapPartsAppearKillAsync.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "Library/LiveActor/LiveActor.h"
+
+class FixMapPartsAppearKillAsync : public al::LiveActor {
+public:
+    FixMapPartsAppearKillAsync(const char* actorName);
+
+    void init(const al::ActorInitInfo& info) override;
+    void appear() override;
+    void kill() override;
+    void movement() override;
+    void calcAnim() override;
+    bool receiveMsg(const al::SensorMsg* message, al::HitSensor* other,
+                    al::HitSensor* self) override;
+
+private:
+    bool mIsCalcViewModel = false;
+};
+
+static_assert(sizeof(FixMapPartsAppearKillAsync) == 0x110);
