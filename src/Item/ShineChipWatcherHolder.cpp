19847cccc892db49b4db0e3f56286c61fed296bc
guymakinggames <66076221+guymakinggames@users.noreply.github.com>
2026-04-19 15:19:27 +0100

    Item: Implement ShineChipWatcherHolder

---
+++ src/Item/ShineChipWatcherHolder.cpp
@@ -0,0 +1,142 @@
+#include "Item/ShineChipWatcherHolder.h"
+
+#include "Library/Scene/SceneObjUtil.h"
+
+#include "Item/Shine.h"
+#include "Item/ShineChipWatcher.h"
+
+ShineChipWatcherHolder::ShineChipWatcherHolder() {
+    mCurrentWatcherIndex = 0;
+    mWatchers.allocBuffer(8, nullptr, 8);
+}
+
+void ShineChipWatcherHolder::entry(ShineChipWatcher* watcher) {
+    mWatchers.pushBack(watcher);
+}
+
+void ShineChipWatcherHolder::notify(ShineChipWatcher* watcher) {
+    mCurrentWatcherIndex = mWatchers.indexOf(watcher);
+}
+
+bool ShineChipWatcherHolder::tryStartAppearShine() {
+    return getCurrentWatcher()->tryStartAppearShine();
+}
+
+ShineChipWatcher* ShineChipWatcherHolder::getCurrentWatcher() const {
+    s32 currentWatcherIndex = mCurrentWatcherIndex;
+    u32 watcherCount = mWatchers.size();
+    u32 currentWatcherIndexU = currentWatcherIndex;
+
+    if (watcherCount <= currentWatcherIndexU)
+        return nullptr;
+
+    return mWatchers[currentWatcherIndex];
+}
+
+bool ShineChipWatcherHolder::tryUpdateCurrentWatcher(const al::LiveActor* actor) {
+    s32 watcherCountMax = mWatchers.size();
+    if (watcherCountMax < 1)
+        return false;
+
+    for (s32 i = 0; i < watcherCountMax; i++) {
+        u32 currentCount = mWatchers.size();
+        ShineChipWatcher* watcher = currentCount <= u32(i) ? nullptr : mWatchers[i];
+        if (watcher->isWatch(actor)) {
+            watcher->updateCount();
+            mCurrentWatcherIndex = i;
+            return true;
+        }
+    }
+    return false;
+}
+
+namespace ShineChipLocalFunction {
+
+bool tryCreateShineChipWatcherHolder(const al::IUseSceneObjHolder* objHolder) {
+    if (al::isExistSceneObj<ShineChipWatcherHolder>(objHolder))
+        return false;
+
+    al::setSceneObj(objHolder, new ShineChipWatcherHolder());
+    return true;
+}
+
+void entryShineChipWatcher(ShineChipWatcher* watcher) {
+    al::tryGetSceneObj<ShineChipWatcherHolder>(watcher)->entry(watcher);
+}
+
+void notifyShineChipGet(ShineChipWatcher* watcher) {
+    al::tryGetSceneObj<ShineChipWatcherHolder>(watcher)->notify(watcher);
+}
+
+void notifyShineChipGet(const al::LiveActor* actor) {
+    al::tryGetSceneObj<ShineChipWatcherHolder>(actor)->tryUpdateCurrentWatcher(actor);
+}
+
+}  // namespace ShineChipLocalFunction
+
+namespace rs {
+
+bool isExistShineChipWatcher(const al::IUseSceneObjHolder* objHolder) {
+    return al::tryGetSceneObj<ShineChipWatcherHolder>(objHolder) != nullptr;
+}
+
+bool isCompleteShineChip(const al::IUseSceneObjHolder* objHolder) {
+    s32 completeNum = ShineChipWatcher::getCompleteNum();
+
+    return completeNum <=
+           al::tryGetSceneObj<ShineChipWatcherHolder>(objHolder)->getCurrentWatcher()->getCount();
+}
+
+s32 getShineChipCount(const al::IUseSceneObjHolder* objHolder) {
+    return al::tryGetSceneObj<ShineChipWatcherHolder>(objHolder)->getCurrentWatcher()->getCount();
+}
+
+s32 getCurrentShineChipWatcherIndex(const al::IUseSceneObjHolder* objHolder) {
+    return al::tryGetSceneObj<ShineChipWatcherHolder>(objHolder)->getCurrentWatcherIndex();
+}
+
+bool isCurrentShineChipWatcherTypeEmpty(const al::IUseSceneObjHolder* objHolder) {
+    return al::tryGetSceneObj<ShineChipWatcherHolder>(objHolder)
+        ->getCurrentWatcher()
+        ->getShine()
+        ->isGot();
+}
+
+bool isAppearedShineChipShine(const al::IUseSceneObjHolder* objHolder) {
+    return al::tryGetSceneObj<ShineChipWatcherHolder>(objHolder)
+        ->getCurrentWatcher()
+        ->isAppearedShine();
+}
+
+void addDemoActorShineChipWatcher(const al::IUseSceneObjHolder* objHolder) {
+    al::tryGetSceneObj<ShineChipWatcherHolder>(objHolder)
+        ->getCurrentWatcher()
+        ->addDemoActorWithChips();
+}
+
+bool isEnableStartShineChipCompleteDemo(const al::IUseSceneObjHolder* objHolder) {
+    if (al::tryGetSceneObj<ShineChipWatcherHolder>(objHolder) == nullptr)
+        return false;
+
+    s32 completeNum = ShineChipWatcher::getCompleteNum();
+
+    if (completeNum >
+        al::tryGetSceneObj<ShineChipWatcherHolder>(objHolder)->getCurrentWatcher()->getCount())
+        return false;
+
+    return !al::tryGetSceneObj<ShineChipWatcherHolder>(objHolder)
+                ->getCurrentWatcher()
+                ->isAppearedShine();
+}
+
+bool tryStartAppearShineChipShine(const al::IUseSceneObjHolder* objHolder) {
+    return al::tryGetSceneObj<ShineChipWatcherHolder>(objHolder)
+        ->getCurrentWatcher()
+        ->tryStartAppearShine();
+}
+
+bool isInAreaCurrentShineChipWatcher(const al::IUseSceneObjHolder* objHolder) {
+    return al::tryGetSceneObj<ShineChipWatcherHolder>(objHolder)->getCurrentWatcher()->isInArea();
+}
+
+}  // namespace rs
