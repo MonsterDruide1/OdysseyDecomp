3ddf3b558b921fad678d174f728bdfe6856baa1a
guymakinggames <66076221+guymakinggames@users.noreply.github.com>
2026-04-22 20:16:49 +0100

    MapObj: Implement CapMessageDirector

---
+++ src/MapObj/CapMessageGameDataChecker.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <basis/seadTypes.h>
+
+namespace al {
+struct ActorInitInfo;
+class IUseSceneObjHolder;
+}  // namespace al
+
+class CapMessageGameDataChecker {
+public:
+    CapMessageGameDataChecker();
+
+    void initByPlacementInfo(const al::ActorInitInfo& info);
+    void invalidateKidsMode();
+    void invalidatePlayerSeparate();
+    void invalidateGameClear();
+    void enableNoCap();
+    void enableGiantWanderBoss();
+    bool check(const al::IUseSceneObjHolder* sceneObjHolder) const;
+
+private:
+    bool mIsInvalidateKidsMode = false;
+    bool mIsInvalidatePlayerSeparate = false;
+    bool mIsInvalidateGameClear = false;
+    bool mIsEnableNoCap = false;
+    bool mIsEnableGiantWanderBoss = false;
+};
+
+static_assert(sizeof(CapMessageGameDataChecker) == 0x5, "CapMessageGameDataChecker");
