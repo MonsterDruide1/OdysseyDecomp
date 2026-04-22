9090655294b12dccaf5bc3bed8a82378f1afc43d
guymakinggames <66076221+guymakinggames@users.noreply.github.com>
2026-04-22 22:27:25 +0100

    MapObj: Implement `BossKnuckleFix` (#1021)

--- src/Scene/ProjectActorFactory.cpp
+++ src/Scene/ProjectActorFactory.cpp
@@ -69,6 +69,7 @@
 #include "MapObj/AllDeadWatcherWithShine.h"
 #include "MapObj/AnagramAlphabet.h"
 #include "MapObj/BlockEmpty2D.h"
+#include "MapObj/BossKnuckleFix.h"
 #include "MapObj/CapBomb.h"
 #include "MapObj/CapHanger.h"
 #include "MapObj/CapSwitch.h"
@@ -164,7 +165,7 @@ const al::NameToCreator<al::ActorCreatorFunction> sProjectActorFactoryEntries[] 
     {"BossForestWander", al::createActorFunction<BossForestWander>},
     {"BossKnuckle", nullptr},
     {"BossKnuckleCounterGround", nullptr},
-    {"BossKnuckleFix", nullptr},
+    {"BossKnuckleFix", al::createActorFunction<BossKnuckleFix>},
     {"BossMagma", nullptr},
     {"BossRaid", nullptr},
     {"BossRaidNpc", nullptr},
