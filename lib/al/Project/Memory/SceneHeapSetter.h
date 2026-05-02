80d195bc759554f37d0b904347acba0658f11f1a
guymakinggames <66076221+guymakinggames@users.noreply.github.com>
2026-05-02 08:58:42 +0100

    Sequence: Implement HakoniwaStateDemoOpening

--- lib/al/Project/Memory/SceneHeapSetter.h
+++ lib/al/Project/Memory/SceneHeapSetter.h
@@ -1,10 +1,20 @@
 #pragma once
 
+#include <heap/seadHeap.h>
+#include <heap/seadHeapMgr.h>
+
 namespace al {
 
-class SceneHeapSetter {
+class SceneHeapSetter : public sead::ScopedCurrentHeapSetter {
 public:
     SceneHeapSetter();
+
+    sead::Heap* getSceneHeap() const { return mSceneHeap; }
+
+private:
+    sead::Heap* mSceneHeap = nullptr;
 };
 
 }  // namespace al
+
+static_assert(sizeof(al::SceneHeapSetter) == 0x10);
