4c667ff02c7dcb4e4937bd32f36a2c4c7ca76ea3
guymakinggames <66076221+guymakinggames@users.noreply.github.com>
2026-05-02 11:14:42 +0100

    Enemy: Implement GemaneBullet

--- src/Enemy/GamaneBullet.h
+++ src/Enemy/GamaneBullet.h
@@ -14,6 +14,7 @@ class SensorMsg;
 class GamaneBullet : public al::LiveActor {
 public:
     GamaneBullet(const char* name, const al::LiveActor* parent);
+
     void init(const al::ActorInitInfo& info) override;
     void attackSensor(al::HitSensor* self, al::HitSensor* other) override;
     bool receiveMsg(const al::SensorMsg* message, al::HitSensor* other,
@@ -21,17 +22,19 @@ public:
     void appear() override;
     void control() override;
 
-    void shoot(f32, const sead::Vector3f&);
-    void rotate(f32);
+    void shoot(f32 speed, const sead::Vector3f& direction);
+    void rotate(f32 degree);
 
     void exeStraight();
     void exeBrake();
     void exeFall();
     void exeHit();
 
-    al::LiveActor* getParent() { return mParentActor; }
+    const al::LiveActor* getParent() const { return mParentActor; }
 
 private:
-    f32 _108;
-    al::LiveActor* mParentActor;
+    f32 mRotateDegree = 60.0f;
+    const al::LiveActor* mParentActor = nullptr;
 };
+
+static_assert(sizeof(GamaneBullet) == 0x118);
