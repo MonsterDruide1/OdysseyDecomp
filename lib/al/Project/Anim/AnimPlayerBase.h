7ecf43d831c745d79c4441dc13ffaaeafcc0d1e2
Narr the Reg <5944268+german77@users.noreply.github.com>
2026-05-05 09:32:16 -0600

    cleanup

--- lib/al/Project/Anim/AnimPlayerBase.h
+++ lib/al/Project/Anim/AnimPlayerBase.h
@@ -15,7 +15,15 @@ public:
 
     AnimInfoTable* getAnimInfoTable() const { return mInfoTable; }
 
-protected:
+    bool is10() const { return _10; };
+
+    void set10(bool value) { _10 = value; }
+
+    bool is11() const { return _11; };
+
+    void set11(bool value) { _11 = value; }
+
+private:
     AnimInfoTable* mInfoTable = nullptr;
     bool _10 = false;
     bool _11 = false;
