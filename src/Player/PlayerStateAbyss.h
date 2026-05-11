ea0e5e952304ccb51f8399563eb4a20e9cfb59f9
Aubrey <aubrey@hall.ly>
2026-05-11 16:18:07 -0600

    Player: Implement `PlayerStateAbyss` (#1123)
    Co-authored-by: stellar_estrogen <stellarestrogen@gmail.com>

---
+++ src/Player/PlayerStateAbyss.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "Library/Nerve/NerveStateBase.h"
+
+namespace al {
+class LiveActor;
+}
+
+class PlayerConst;
+class PlayerRecoverySafetyPoint;
+class PlayerColliderHakoniwa;
+class PlayerAnimator;
+class PlayerStateRecoveryDead;
+
+class PlayerStateAbyss : public al::ActorStateBase {
+public:
+    PlayerStateAbyss(al::LiveActor* player, const PlayerConst* playerConst,
+                     PlayerRecoverySafetyPoint* recoverySafetyPoint,
+                     PlayerColliderHakoniwa* playerCollider, PlayerAnimator* playerAnimator,
+                     al::LiveActor* player2dModel);
+
+    void appear() override;
+    void kill() override;
+
+    void exeFall();
+    void exeRecovery();
+    bool isRecovery() const;
+    bool isRecoveryLandFall() const;
+    void prepareRecovery();
+
+private:
+    const PlayerConst* mConst = nullptr;
+    PlayerRecoverySafetyPoint* mRecoverySafetyPoint = nullptr;
+    PlayerAnimator* mAnimator = nullptr;
+    PlayerStateRecoveryDead* mStateRecoveryDead = nullptr;
+};
+
+static_assert(sizeof(PlayerStateAbyss) == 0x40);
---
+++ src/Player/PlayerStateRecoveryDead.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <math/seadQuat.h>
+
+#include "Library/Nerve/NerveStateBase.h"
+
+namespace al {
+class LiveActor;
+}
+
+class PlayerRecoverySafetyPoint;
+class PlayerColliderHakoniwa;
+class PlayerAnimator;
+class PlayerConst;
+class ActorDimensionKeeper;
+
+class PlayerStateRecoveryDead : public al::ActorStateBase {
+public:
+    PlayerStateRecoveryDead(al::LiveActor* player, PlayerRecoverySafetyPoint* recoverySafetyPoint,
+                            PlayerColliderHakoniwa* playerCollider, PlayerAnimator* playerAnimator,
+                            const PlayerConst* playerConst, al::LiveActor* player2dModel);
+
+    void appear() override;
+    void kill() override;
+
+    void exeFall();
+    void exeRecovery();
+    void exeStart();
+    bool isLandFall() const;
+
+private:
+    PlayerRecoverySafetyPoint* mRecoverySafetyPoint;
+    PlayerColliderHakoniwa* mColliderHakoniwa;
+    PlayerAnimator* mAnimator;
+    const PlayerConst* mConst;
+    al::LiveActor* mModelHolder;
+    f32 _48;
+    f32 _4c;
+    sead::Vector3f mTrans;
+    sead::Vector3f mNegativeGravity;
+    sead::Vector3f _68;
+    s32 _74;
+    s32 _78;
+    f32 _7c;
+    sead::Quatf _80;
+    sead::Vector3f _90;
+    sead::Vector3f _9c;
+    ActorDimensionKeeper* mDimensionKeeper;
+    sead::Vector3f _b0;
+    sead::Vector3f _bc;
+};
+
+static_assert(sizeof(PlayerStateRecoveryDead) == 0xc8);
