bdecdf9cc4ea95a2ffd7c77a70c35f99dc4a75d1
Fuzzy2319 <alexandre.verbrigghe@orange.fr>
2026-05-16 23:24:29 +0200

    add:Killer implementation

--- lib/al/Library/Light/LightKeeper.h
+++ lib/al/Library/Light/LightKeeper.h
@@ -6,4 +6,5 @@ class LiveActor;
 bool isActivePrePassLight(const LiveActor*, const char*);
 bool isExistPrePassLight(const LiveActor*, const char*);
 void killPrePassLight(const LiveActor*, const char*, s32);
+void appearPrePassLight(const LiveActor*, const char*, s32);
 }  // namespace al
