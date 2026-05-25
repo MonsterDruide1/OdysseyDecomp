c1e0a0be223e907f66327741e238087210d54283
Nicholas Yoannou <nicholas@nicholas.dev>
2026-05-25 20:39:45 +0100

    Library/Event: Implement `EventFlowEventData`

---
+++ lib/al/Library/Event/EventFlowEventData.cpp
@@ -0,0 +1,14 @@
+#include "Library/Event/EventFlowEventData.h"
+
+#include "Library/Event/EventFlowFunction.h"
+#include "Library/Yaml/ByamlIter.h"
+
+namespace al {
+
+EventFlowEventData::EventFlowEventData(const char* name, const EventFlowNodeInitInfo& info) {
+    mName = name;
+    mParamIter = new ByamlIter();
+    tryGetParamIter(mParamIter, info);
+}
+
+}  // namespace al
