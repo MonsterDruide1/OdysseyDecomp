0dc626a1b8e5b6fe19d6a92c2a8db576f8414582
Spletz47 <spletz47@gmail.com>
2026-08-04 12:01:22 +1000

    Npc: Implement `SessionMusicianLocalFunction` and `SessionMusicianManager`

---
+++ src/Npc/SessionMusicianManager.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <container/seadPtrArray.h>
+
+#include "Library/LiveActor/LiveActor.h"
+#include "Library/Scene/ISceneObj.h"
+
+#include "Npc/SessionMusicianNpc.h"
+#include "Scene/SceneObjFactory.h"
+
+namespace al {
+class ISceneObj;
+class LiveActor;
+class PlacementInfo;
+}  // namespace al
+
+class SessionMayorNpc;
+class SessionMusicianBgmController;
+class SessionMusicianType;
+
+class SessionMusicianManager : public al::LiveActor, public al::ISceneObj {
+public:
+    static constexpr s32 sSceneObjId = SceneObjID_SessionMusicianManager;
+
+    SessionMusicianManager(const char* name);
+
+    void initAfterPlacementSceneObj(const al::ActorInitInfo& info);
+
+    void entryMayor(SessionMayorNpc* mayor) { mMayorNpc = mayor; };
+
+    void entryMusician(SessionMusicianNpc* musician);
+    bool isJoinedMusician() const;
+    SessionMusicianNpc* getJoinedMusician() const;
+    bool isSubscribed(SessionMusicianType type) const;
+
+    bool tryAppearPowerPlant();
+    SessionMusicianNpc* findPowerPlant() const;
+
+    bool tryStartWarp(al::PlacementInfo* info);
+
+    void addDemoAllMusicians();
+
+    void exeWait();
+    void exeComplete();
+
+    sead::PtrArray<SessionMusicianNpc> getMusicians() const { return mMusicianPtrArray; }
+
+private:
+    sead::PtrArray<SessionMusicianNpc> mMusicianPtrArray;
+    SessionMusicianBgmController* mBgmController = nullptr;
+    SessionMayorNpc* mMayorNpc = nullptr;
+};
