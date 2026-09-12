fbc46daa02cbdf67b08981c40f08a61f8d3b71d0
Arayaphong Traisopon <arme.tr@gmail.com>
2026-09-12 23:02:18 +0700

    Item: Implement RandomItemSelector

--- src/Item/RandomItemSelector.h
+++ src/Item/RandomItemSelector.h
@@ -14,14 +14,20 @@ public:
     static constexpr s32 sSceneObjId = SceneObjID_RandomItemSelector;
 
     RandomItemSelector();
-    rs::ItemType::ValueType getRandomItemType(const al::IUseSceneObjHolder*);
+    rs::ItemType::ValueType getRandomItemType(const al::IUseSceneObjHolder* user);
 
 private:
-    char filler[0x10];
+    struct ItemList {
+        s32 itemCount = 0;
+        rs::ItemType::ValueType* itemTypes = nullptr;
+    };
+
+    ItemList* mItemLists = nullptr;
+    s32 mItemIndex = 0;
 };
 
 static_assert(sizeof(RandomItemSelector) == 0x18);
 
 namespace rs {
-void createRandomItemSelector(const al::IUseSceneObjHolder*);
-}
+RandomItemSelector* createRandomItemSelector(const al::IUseSceneObjHolder* user);
+}  // namespace rs
