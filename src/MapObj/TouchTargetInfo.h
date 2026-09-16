7482ab522172c8a3f1e2a4e41caddece077ec237
arayaphong <arme.tr@gmail.com>
2026-09-16 19:54:40 +0700

    MapObj: Implement `TouchTargetInfo` (#1362)

--- src/MapObj/TouchTargetInfo.h
+++ src/MapObj/TouchTargetInfo.h
@@ -9,8 +9,30 @@ class MtxConnector;
 
 class TouchTargetInfo {
 public:
-    void setInfoBySensor(const al::HitSensor*, const sead::Vector3f&, const sead::Vector3f&);
-    void setInfoByConnector(const al::MtxConnector*, const sead::Vector3f&, const sead::Vector3f&,
-                            bool);
-    void setInfoByPosAndNrm(const sead::Vector3f&, const sead::Vector3f&);
+    TouchTargetInfo();
+
+    void setInfoBySensor(const al::HitSensor* sensor, const sead::Vector3f& offset,
+                         const sead::Vector3f& normal);
+    void reset();
+    void setInfoByConnector(const al::MtxConnector* connector, const sead::Vector3f& pos,
+                            const sead::Vector3f& normal, bool isKeepTarget);
+    void setInfoByPosAndNrm(const sead::Vector3f& pos, const sead::Vector3f& normal);
+    void calcCurrentPosAndNrm(sead::Vector3f* pos, sead::Vector3f* normal) const;
+
+private:
+    enum class Type {
+        PosAndNrm,
+        Sensor,
+        Connector,
+    };
+
+    Type mType;
+    bool mIsKeepTarget;
+    const al::HitSensor* mSensor;
+    const al::MtxConnector* mConnector;
+    sead::Vector3f mSensorOffset;
+    sead::Vector3f mPos;
+    sead::Vector3f mNormal;
 };
+
+static_assert(sizeof(TouchTargetInfo) == 0x40);
